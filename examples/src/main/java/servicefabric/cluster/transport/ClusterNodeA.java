6ffcff6cd5e76e16b9818803aec1450f3622a6a4
Ronen Hamias <ronen.hamias@gmail.com>
2015-08-23 11:06:46 +0300

    add transport example

---
+++ examples/src/main/java/servicefabric/cluster/transport/ClusterNodeA.java
@@ -0,0 +1,30 @@
+package servicefabric.cluster.transport;
+
+import io.servicefabric.cluster.Cluster;
+import io.servicefabric.cluster.ICluster;
+import io.servicefabric.transport.TransportMessage;
+import rx.functions.Action1;
+import rx.functions.Func1;
+
+public class ClusterNodeA {
+
+	public static void main(String[] args) {
+	
+		// start cluster node that listen on port 3000
+		ICluster clusterA = Cluster.newInstance(3000).join();
+		
+		// subscribe to all gossip messages:
+		clusterA.transport().listen().filter(new Func1<TransportMessage, Boolean>() {
+			@Override
+			public Boolean call(TransportMessage t1) {
+				return t1.message().qualifier().equals("hello/world");
+			}
+		}).subscribe(new Action1<TransportMessage>() {
+			@Override
+			public void call(TransportMessage t1) {
+				System.out.println(t1.message());
+			}
+		});
+	}
+	
+}
---
+++ examples/src/main/java/servicefabric/cluster/transport/ClusterNodeB.java
@@ -0,0 +1,21 @@
+package servicefabric.cluster.transport;
+
+import io.servicefabric.cluster.Cluster;
+import io.servicefabric.cluster.ICluster;
+import io.servicefabric.transport.TransportEndpoint;
+import io.servicefabric.transport.protocol.Message;
+
+public class ClusterNodeB {
+
+	public static void main(String[] args) {
+		
+		// start cluster node that listen on port 3001 and point to node A as seed node
+		ICluster clusterB =  Cluster.newInstance(3001,"localhost:3000").join();
+		
+		// spread gossip
+		TransportEndpoint endpoint = TransportEndpoint.from("tcp://A@localhost:3000");
+		clusterB.transport().to(endpoint).send(new Message("hello/world"));
+		
+	}
+
+}
