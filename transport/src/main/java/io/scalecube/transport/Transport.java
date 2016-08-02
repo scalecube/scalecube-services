bigclouds/scalecube

Set inet addr resolution at connection point

transport/src/main/java/io/scalecube/transport/Transport.java
@@ -2,13 +2,16 @@ package io.scalecube.transport;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
-import static io.scalecube.transport.utils.ChannelFutureUtils.setPromise;
-import static io.scalecube.transport.utils.ChannelFutureUtils.wrap;
 
+import io.scalecube.transport.utils.FutureUtils;
 import io.scalecube.transport.utils.memoization.Computable;
 import io.scalecube.transport.utils.memoization.Memoizer;
 
 import com.google.common.base.Function;
+import com.google.common.net.InetAddresses;
+import com.google.common.util.concurrent.AsyncFunction;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.FutureFallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.SettableFuture;
@@ -22,6 +25,7 @@ import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.ChannelPromise;
+import io.netty.channel.EventLoop;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.ServerChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
@@ -40,7 +44,9 @@ import rx.schedulers.Schedulers;
 import rx.subjects.PublishSubject;
 import rx.subjects.Subject;
 
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ThreadFactory;
@@ -177,19 +183,21 @@ public final class Transport implements ITransportSpi, ITransport {
       }
     });
 
-    ChannelFuture bindFuture = server.bind(localEndpoint.socketAddress());
+    InetAddress inetAddress = InetAddresses.forString(localEndpoint.hostname()); // get inetAddress w/o DNS lookup
+    ChannelFuture bindFuture = server.bind(new InetSocketAddress(inetAddress, localEndpoint.port())); // bind
     final SettableFuture<Void> result = SettableFuture.create();
     bindFuture.addListener(new ChannelFutureListener() {
       @Override
       public void operationComplete(ChannelFuture channelFuture) throws Exception {
         if (channelFuture.isSuccess()) {
           serverChannel = (ServerChannel) channelFuture.channel();
-          LOGGER.info("Transport endpoint '{}' bound to: {}", localEndpoint.id(), localEndpoint.socketAddress());
+          LOGGER.info("Bound transport endpoint: {} at {}:{}",
+              localEndpoint.id(), localEndpoint.hostname(), localEndpoint.port());
           result.set(null);
         } else {
-          Throwable ex = channelFuture.cause();
-          result.setException(ex);
-          LOGGER.error("Failed to bind to: " + localEndpoint.socketAddress() + ", caught " + ex, ex);
+          LOGGER.error("Failed to bind transport endpoint: {} at {}:{}, cause: {}",
+              localEndpoint.id(), localEndpoint.hostname(), localEndpoint.port(), channelFuture.cause());
+          result.setException(channelFuture.cause());
         }
       }
     });
@@ -209,7 +217,7 @@ public final class Transport implements ITransportSpi, ITransport {
       public void run() {
         ChannelPromise promise = channel.newPromise();
         channel.connect(address, promise);
-        promise.addListener(wrap(new ChannelFutureListener() {
+        promise.addListener(FutureUtils.wrap(new ChannelFutureListener() {
           @Override
           public void operationComplete(ChannelFuture future) {
             if (!future.isSuccess()) {
@@ -283,7 +291,7 @@ public final class Transport implements ITransportSpi, ITransport {
       }
     }
     if (serverChannel != null) {
-      setPromise(serverChannel.close(), promise);
+      FutureUtils.listenableFuture(serverChannel.close(), promise);
     }
   }
 
@@ -308,8 +316,8 @@ public final class Transport implements ITransportSpi, ITransport {
     checkNotNull(remoteEndpoint.socketAddress());
     TransportChannel prev = acceptedChannels.putIfAbsent(remoteEndpoint.socketAddress(), transportChannel);
     if (prev != null) {
-      String err = String.format("Detected duplicate %s for key=%s in accepted_map", prev, remoteEndpoint);
-      throw new TransportBrokenException(err);
+      throw new TransportBrokenException(
+          String.format("Detected duplicate %s for key=%s in accepted_map", prev, remoteEndpoint));
     }
   }
 
@@ -323,28 +331,59 @@ public final class Transport implements ITransportSpi, ITransport {
     incomingMessagesSubject.onNext(message);
   }
 
-  private TransportChannel getOrConnect(@CheckForNull InetSocketAddress address) {
-    checkArgument(address != null);
-    return connectedChannels.get(address, new Computable<InetSocketAddress, TransportChannel>() {
+  private TransportChannel getOrConnect(@CheckForNull InetSocketAddress socketAddress) {
+    checkArgument(socketAddress != null);
+    checkArgument(socketAddress.isUnresolved());
+
+    return connectedChannels.get(socketAddress, new Computable<InetSocketAddress, TransportChannel>() {
       @Override
-      public TransportChannel compute(final InetSocketAddress address) {
+      public TransportChannel compute(final InetSocketAddress socketAddress) {
         final Channel channel = createConnectorChannel();
-        final TransportChannel transportChannel = createConnectorTransportChannel(channel, address);
+        final TransportChannel transportChannel = createConnectorTransportChannel(channel, socketAddress);
 
-        LOGGER.info("Registered connector: {}", transportChannel);
+        // register channel
+        final ListenableFuture<Void> registerFuture =
+            Futures.withFallback(FutureUtils.listenableFuture(eventLoop.register(channel)), new FutureFallback<Void>() {
+              @Override
+              public ListenableFuture<Void> create(Throwable cause) {
+                LOGGER.error("Failed to register channel, cause: {}", new Object[] {cause});
+                return Futures.immediateFailedFuture(cause);
+              }
+            });
 
-        final ChannelFuture registerChannelFuture = eventLoop.register(channel);
-        registerChannelFuture.addListener(new ChannelFutureListener() {
+        // resolve inet address from unresolved socketAddress
+        ListenableFuture<InetSocketAddress> resolveAddressFuture = Futures.withFallback(
+            Futures.transform(registerFuture, new AsyncFunction<Void, InetSocketAddress>() {
+              @Override
+              public ListenableFuture<InetSocketAddress> apply(Void input) {
+                LOGGER.info("Registered connector: {}", transportChannel);
+                return resolveAddress(socketAddress, channel.eventLoop());
+              }
+            }),
+            new FutureFallback<InetSocketAddress>() {
+              @Override
+              public ListenableFuture<InetSocketAddress> create(Throwable cause) throws Exception {
+                LOGGER.error("Failed to resolve inet address for hostname: {}, cause: {}",
+                    socketAddress.getHostName(), cause);
+                return Futures.immediateFailedFuture(cause);
+              }
+            });
+
+        // issue connect if previous two steps succeed
+        Futures.addCallback(resolveAddressFuture, new FutureCallback<InetSocketAddress>() {
           @Override
-          public void operationComplete(ChannelFuture future) throws Exception {
-            if (future.isSuccess()) {
-              connect(channel, address, transportChannel);
-            } else {
-              channel.unsafe().closeForcibly();
-              transportChannel.close();
-            }
+          public void onSuccess(InetSocketAddress address) {
+            LOGGER.info("Resolved inet address: {} -> {}", socketAddress.getHostName(), address.getAddress());
+            connect(channel, address, transportChannel);
+          }
+
+          @Override
+          public void onFailure(Throwable throwable) {
+            channel.unsafe().closeForcibly();
+            transportChannel.close();
           }
         });
+
         return transportChannel;
       }
     });
@@ -369,5 +408,15 @@ public final class Transport implements ITransportSpi, ITransport {
         return null;
       }
     });
+  }
+
+  private ListenableFuture<InetSocketAddress> resolveAddress(final InetSocketAddress socketAddress,
+      EventLoop eventLoop) {
+    return FutureUtils.listenableFuture(eventLoop.submit(new Callable<InetSocketAddress>() {
+      @Override
+      public InetSocketAddress call() throws Exception {
+        return new InetSocketAddress(InetAddress.getByName(socketAddress.getHostName()), socketAddress.getPort());
+      }
+    }));
   }
 }

transport/src/main/java/io/scalecube/transport/TransportChannel.java
@@ -5,7 +5,8 @@ import static com.google.common.collect.Iterables.transform;
 import static io.scalecube.transport.TransportChannel.Status.CLOSED;
 import static io.scalecube.transport.TransportChannel.Status.CONNECTED;
 import static io.scalecube.transport.TransportChannel.Status.CONNECT_IN_PROGRESS;
-import static io.scalecube.transport.utils.ChannelFutureUtils.setPromise;
+
+import io.scalecube.transport.utils.FutureUtils;
 
 import com.google.common.base.Function;
 import com.google.common.base.Joiner;
@@ -124,7 +125,7 @@ final class TransportChannel implements ITransportChannel {
     if (promise != null && getCause() != null) {
       promise.setException(getCause());
     } else {
-      setPromise(channel.writeAndFlush(message), promise);
+      FutureUtils.listenableFuture(channel.writeAndFlush(message), promise);
     }
   }
 
@@ -145,7 +146,7 @@ final class TransportChannel implements ITransportChannel {
     this.cause.compareAndSet(null, cause != null ? cause : new TransportClosedException());
     status.set(CLOSED);
     closeCallback.call(this);
-    setPromise(channel.close(), promise);
+    FutureUtils.listenableFuture(channel.close(), promise);
     LOGGER.info("Closed {}", this);
   }
 

transport/src/main/java/io/scalecube/transport/utils/FutureUtils.java
@@ -1,0 +1,77 @@
+package io.scalecube.transport.utils;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.SettableFuture;
+
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
+
+import javax.annotation.Nullable;
+
+public class FutureUtils {
+
+  private FutureUtils() {}
+
+  /**
+   * Converts netty {@link Future} to the given guava {@link ListenableFuture}.
+   * 
+   * @param future netty future
+   * @param promise guava future; can be null
+   * @return same {@code promise} passed as parameter; just for convenience
+   */
+  public static <T> ListenableFuture<T> listenableFuture(Future<T> future, @Nullable final SettableFuture<T> promise) {
+    if (promise != null) {
+      future.addListener(new GenericFutureListener<Future<T>>() {
+        @Override
+        public void operationComplete(Future<T> future) throws Exception {
+          if (future.isSuccess()) {
+            promise.set(future.get());
+          } else {
+            promise.setException(future.cause());
+          }
+        }
+      });
+    }
+    return promise;
+  }
+
+  /**
+   * Converts netty {@link Future} to the newly created guava {@link ListenableFuture}.
+   *
+   * @param future netty future
+   * @return newly created {@code listenableFuture}
+   */
+  public static <T> ListenableFuture<T> listenableFuture(Future<T> future) {
+    final SettableFuture<T> promise = SettableFuture.create();
+    future.addListener(new GenericFutureListener<Future<T>>() {
+      @Override
+      public void operationComplete(Future<T> future) throws Exception {
+        if (future.isSuccess()) {
+          promise.set(future.get());
+        } else {
+          promise.setException(future.cause());
+        }
+      }
+    });
+    return promise;
+  }
+
+  /**
+   * @return wrapped {@link ChannelFutureListener} that forwards a throwable thrown out of given
+   *         {@code channelFutureListener} into the {@code ChannelPipeline}.
+   */
+  public static ChannelFutureListener wrap(final ChannelFutureListener channelFutureListener) {
+    return new ChannelFutureListener() {
+      @Override
+      public void operationComplete(ChannelFuture future) throws Exception {
+        try {
+          channelFutureListener.operationComplete(future);
+        } catch (Exception e) {
+          future.channel().pipeline().fireExceptionCaught(e);
+        }
+      }
+    };
+  }
+}

transport/src/test/java/io/scalecube/transport/TransportEndpointTest.java
@@ -1,0 +1,42 @@
+package io.scalecube.transport;
+
+import static org.junit.Assert.assertEquals;
+
+import io.scalecube.transport.utils.IpAddressResolver;
+
+import org.junit.Test;
+
+import java.net.InetSocketAddress;
+
+public class TransportEndpointTest {
+
+  @Test
+  public void testParseHostPortEndpointId() throws Exception {
+    TransportEndpoint te1 = TransportEndpoint.from("localhost:5810:0A1B2C3");
+    assertEquals("0A1B2C3", te1.id());
+    assertEquals(IpAddressResolver.resolveIpAddress().getHostAddress() + ":5810" + ":0A1B2C3", te1.getString());
+
+    TransportEndpoint te2 = TransportEndpoint.from("127.0.0.1:5810:0A1B2C3");
+    assertEquals("0A1B2C3", te2.id());
+    assertEquals(IpAddressResolver.resolveIpAddress().getHostAddress() + ":5810" + ":0A1B2C3", te2.getString());
+
+    assertEquals(te1, te2);
+    assertEquals(te1.hostname(), te2.hostname());
+    assertEquals(te1.port(), te2.port());
+    assertEquals(te1.socketAddress(), te2.socketAddress());
+  }
+
+  @Test
+  public void testParseUnknownHostPortEndpoitId() throws Exception {
+    TransportEndpoint te = TransportEndpoint.from("host:1111:0A1B2C3");
+    assertEquals("0A1B2C3", te.id());
+    assertEquals("host:1111:0A1B2C3", te.getString());
+  }
+
+  @Test
+  public void testParseSocketAddress() throws Exception {
+    InetSocketAddress sa = TransportEndpoint.parseSocketAddress("host:1111");
+    assertEquals("host", sa.getHostName());
+    assertEquals(1111, sa.getPort());
+  }
+}

scalecube/scalecube-services

transport/src/main/java/io/scalecube/transport/Transport.java
@@ -2,13 +2,15 @@ package io.scalecube.transport;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
-import static io.scalecube.transport.utils.ChannelFutureUtils.setPromise;
-import static io.scalecube.transport.utils.ChannelFutureUtils.wrap;
 
 import io.scalecube.transport.memoizer.Computable;
 import io.scalecube.transport.memoizer.Memoizer;
+import io.scalecube.transport.utils.FutureUtils;
 
 import com.google.common.base.Function;
+import com.google.common.util.concurrent.AsyncFunction;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.FutureFallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.SettableFuture;
@@ -22,6 +24,7 @@ import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.ChannelPromise;
+import io.netty.channel.EventLoop;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.ServerChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
@@ -40,7 +43,9 @@ import rx.schedulers.Schedulers;
 import rx.subjects.PublishSubject;
 import rx.subjects.Subject;
 
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ThreadFactory;
@@ -177,19 +182,20 @@ public final class Transport implements ITransportSpi, ITransport {
       }
     });
 
-    ChannelFuture bindFuture = server.bind(localEndpoint.socketAddress());
+    ChannelFuture bindFuture = server.bind(localEndpoint.port());
     final SettableFuture<Void> result = SettableFuture.create();
     bindFuture.addListener(new ChannelFutureListener() {
       @Override
       public void operationComplete(ChannelFuture channelFuture) throws Exception {
         if (channelFuture.isSuccess()) {
           serverChannel = (ServerChannel) channelFuture.channel();
-          LOGGER.info("Transport endpoint '{}' bound to: {}", localEndpoint.id(), localEndpoint.socketAddress());
+          LOGGER.info("Bound transport endpoint: {} at {}:{}",
+              localEndpoint.id(), localEndpoint.host(), localEndpoint.port());
           result.set(null);
         } else {
-          Throwable ex = channelFuture.cause();
-          result.setException(ex);
-          LOGGER.error("Failed to bind to: " + localEndpoint.socketAddress() + ", caught " + ex, ex);
+          LOGGER.error("Failed to bind transport endpoint: {} at {}:{}, cause: {}",
+              localEndpoint.id(), localEndpoint.host(), localEndpoint.port(), channelFuture.cause());
+          result.setException(channelFuture.cause());
         }
       }
     });
@@ -209,7 +215,7 @@ public final class Transport implements ITransportSpi, ITransport {
       public void run() {
         ChannelPromise promise = channel.newPromise();
         channel.connect(address, promise);
-        promise.addListener(wrap(new ChannelFutureListener() {
+        promise.addListener(FutureUtils.wrap(new ChannelFutureListener() {
           @Override
           public void operationComplete(ChannelFuture future) {
             if (!future.isSuccess()) {
@@ -283,7 +289,7 @@ public final class Transport implements ITransportSpi, ITransport {
       }
     }
     if (serverChannel != null) {
-      setPromise(serverChannel.close(), promise);
+      FutureUtils.compose(serverChannel.close(), promise);
     }
   }
 
@@ -308,8 +314,8 @@ public final class Transport implements ITransportSpi, ITransport {
     checkNotNull(remoteEndpoint.socketAddress());
     TransportChannel prev = acceptedChannels.putIfAbsent(remoteEndpoint.socketAddress(), transportChannel);
     if (prev != null) {
-      String err = String.format("Detected duplicate %s for key=%s in accepted_map", prev, remoteEndpoint);
-      throw new TransportBrokenException(err);
+      throw new TransportBrokenException(
+          String.format("Detected duplicate %s for key=%s in accepted_map", prev, remoteEndpoint));
     }
   }
 
@@ -323,26 +329,54 @@ public final class Transport implements ITransportSpi, ITransport {
     incomingMessagesSubject.onNext(message);
   }
 
-  private TransportChannel getOrConnect(@CheckForNull InetSocketAddress address) {
-    checkArgument(address != null);
-    return connectedChannels.get(address, new Computable<InetSocketAddress, TransportChannel>() {
+  private TransportChannel getOrConnect(@CheckForNull InetSocketAddress socketAddress) {
+    checkArgument(socketAddress != null);
+    return connectedChannels.get(socketAddress, new Computable<InetSocketAddress, TransportChannel>() {
       @Override
-      public TransportChannel compute(final InetSocketAddress address) {
+      public TransportChannel compute(final InetSocketAddress socketAddress) {
         final Channel channel = createConnectorChannel();
-        final TransportChannel transportChannel = createConnectorTransportChannel(channel, address);
+        final TransportChannel transportChannel = createConnectorTransportChannel(channel, socketAddress);
 
-        LOGGER.info("Registered connector: {}", transportChannel);
+        // register channel
+        final ListenableFuture<Void> registerFuture =
+            Futures.withFallback(FutureUtils.compose(eventLoop.register(channel)), new FutureFallback<Void>() {
+              @Override
+              public ListenableFuture<Void> create(Throwable cause) {
+                LOGGER.error("Failed to register channel, cause: {}", new Object[] {cause});
+                return Futures.immediateFailedFuture(cause);
+              }
+            });
 
-        final ChannelFuture registerChannelFuture = eventLoop.register(channel);
-        registerChannelFuture.addListener(new ChannelFutureListener() {
+        // resolve inet address from unresolved socketAddress
+        ListenableFuture<InetSocketAddress> resolveAddressFuture = Futures.withFallback(
+            Futures.transform(registerFuture, new AsyncFunction<Void, InetSocketAddress>() {
+              @Override
+              public ListenableFuture<InetSocketAddress> apply(Void input) {
+                LOGGER.info("Registered connector: {}", transportChannel);
+                return resolveAddress(socketAddress, channel.eventLoop());
+              }
+            }),
+            new FutureFallback<InetSocketAddress>() {
+              @Override
+              public ListenableFuture<InetSocketAddress> create(Throwable cause) throws Exception {
+                LOGGER.error("Failed to resolve inet address for host: {}, cause: {}",
+                    socketAddress.getHostName(), cause);
+                return Futures.immediateFailedFuture(cause);
+              }
+            });
+
+        // issue connect if previous two steps succeed
+        Futures.addCallback(resolveAddressFuture, new FutureCallback<InetSocketAddress>() {
           @Override
-          public void operationComplete(ChannelFuture future) throws Exception {
-            if (future.isSuccess()) {
-              connect(channel, address, transportChannel);
-            } else {
-              channel.unsafe().closeForcibly();
-              transportChannel.close();
-            }
+          public void onSuccess(InetSocketAddress address) {
+            LOGGER.info("Resolved inet address: {} -> {}", socketAddress.getHostName(), address.getAddress());
+            connect(channel, address, transportChannel);
+          }
+
+          @Override
+          public void onFailure(Throwable throwable) {
+            channel.unsafe().closeForcibly();
+            transportChannel.close();
           }
         });
         return transportChannel;
@@ -369,5 +403,15 @@ public final class Transport implements ITransportSpi, ITransport {
         return null;
       }
     });
+  }
+
+  private ListenableFuture<InetSocketAddress> resolveAddress(final InetSocketAddress socketAddress,
+      EventLoop eventLoop) {
+    return FutureUtils.compose(eventLoop.submit(new Callable<InetSocketAddress>() {
+      @Override
+      public InetSocketAddress call() throws Exception {
+        return new InetSocketAddress(InetAddress.getByName(socketAddress.getHostName()), socketAddress.getPort());
+      }
+    }));
   }
 }

transport/src/main/java/io/scalecube/transport/TransportChannel.java
@@ -5,7 +5,8 @@ import static com.google.common.collect.Iterables.transform;
 import static io.scalecube.transport.TransportChannel.Status.CLOSED;
 import static io.scalecube.transport.TransportChannel.Status.CONNECTED;
 import static io.scalecube.transport.TransportChannel.Status.CONNECT_IN_PROGRESS;
-import static io.scalecube.transport.utils.ChannelFutureUtils.setPromise;
+
+import io.scalecube.transport.utils.FutureUtils;
 
 import com.google.common.base.Function;
 import com.google.common.base.Joiner;
@@ -124,7 +125,7 @@ final class TransportChannel implements ITransportChannel {
     if (promise != null && getCause() != null) {
       promise.setException(getCause());
     } else {
-      setPromise(channel.writeAndFlush(message), promise);
+      FutureUtils.compose(channel.writeAndFlush(message), promise);
     }
   }
 
@@ -145,7 +146,7 @@ final class TransportChannel implements ITransportChannel {
     this.cause.compareAndSet(null, cause != null ? cause : new TransportClosedException());
     status.set(CLOSED);
     closeCallback.call(this);
-    setPromise(channel.close(), promise);
+    FutureUtils.compose(channel.close(), promise);
     LOGGER.info("Closed {}", this);
   }
 

transport/src/main/java/io/scalecube/transport/utils/FutureUtils.java
@@ -1,0 +1,77 @@
+package io.scalecube.transport.utils;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.SettableFuture;
+
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
+
+import javax.annotation.Nullable;
+
+public class FutureUtils {
+
+  private FutureUtils() {}
+
+  /**
+   * Converts netty {@link Future} to the given guava {@link ListenableFuture}.
+   * 
+   * @param future netty future
+   * @param promise guava future; can be null
+   * @return same {@code promise} passed as parameter; just for convenience
+   */
+  public static <T> ListenableFuture<T> compose(Future<T> future, @Nullable final SettableFuture<T> promise) {
+    if (promise != null) {
+      future.addListener(new GenericFutureListener<Future<T>>() {
+        @Override
+        public void operationComplete(Future<T> future) throws Exception {
+          if (future.isSuccess()) {
+            promise.set(future.get());
+          } else {
+            promise.setException(future.cause());
+          }
+        }
+      });
+    }
+    return promise;
+  }
+
+  /**
+   * Converts netty {@link Future} to the newly created guava {@link ListenableFuture}.
+   *
+   * @param future netty future
+   * @return newly created {@code listenableFuture}
+   */
+  public static <T> ListenableFuture<T> compose(Future<T> future) {
+    final SettableFuture<T> promise = SettableFuture.create();
+    future.addListener(new GenericFutureListener<Future<T>>() {
+      @Override
+      public void operationComplete(Future<T> future) throws Exception {
+        if (future.isSuccess()) {
+          promise.set(future.get());
+        } else {
+          promise.setException(future.cause());
+        }
+      }
+    });
+    return promise;
+  }
+
+  /**
+   * @return wrapped {@link ChannelFutureListener} that forwards a throwable thrown out of given
+   *         {@code channelFutureListener} into the {@code ChannelPipeline}.
+   */
+  public static ChannelFutureListener wrap(final ChannelFutureListener channelFutureListener) {
+    return new ChannelFutureListener() {
+      @Override
+      public void operationComplete(ChannelFuture future) throws Exception {
+        try {
+          channelFutureListener.operationComplete(future);
+        } catch (Exception e) {
+          future.channel().pipeline().fireExceptionCaught(e);
+        }
+      }
+    };
+  }
+}

transport/src/test/java/io/scalecube/transport/TransportEndpointTest.java
@@ -1,0 +1,45 @@
+package io.scalecube.transport;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import io.scalecube.transport.utils.IpAddressResolver;
+
+import org.junit.Test;
+
+import java.net.InetSocketAddress;
+
+public class TransportEndpointTest {
+
+  @Test
+  public void testParseHostPortEndpointId() throws Exception {
+    TransportEndpoint te1 = TransportEndpoint.from("localhost:5810:0A1B2C3");
+    assertEquals("0A1B2C3", te1.id());
+    assertEquals(IpAddressResolver.resolveIpAddress().getHostAddress() + ":5810" + ":0A1B2C3", te1.getString());
+
+    TransportEndpoint te2 = TransportEndpoint.from("127.0.0.1:5810:0A1B2C3");
+    assertEquals("0A1B2C3", te2.id());
+    assertEquals(IpAddressResolver.resolveIpAddress().getHostAddress() + ":5810" + ":0A1B2C3", te2.getString());
+
+    assertEquals(te1, te2);
+    assertEquals(te1.host(), te2.host());
+    assertEquals(te1.port(), te2.port());
+    assertEquals(te1.socketAddress(), te2.socketAddress());
+  }
+
+  @Test
+  public void testParseUnknownHostPortEndpoitId() throws Exception {
+    TransportEndpoint te = TransportEndpoint.from("host:1111:0A1B2C3");
+    assertEquals("0A1B2C3", te.id());
+    assertEquals("host:1111:0A1B2C3", te.getString());
+    assertTrue(te.socketAddress().isUnresolved());
+  }
+
+  @Test
+  public void testParseSocketAddress() throws Exception {
+    InetSocketAddress sa = TransportEndpoint.parseSocketAddress("host:1111");
+    assertEquals("host", sa.getHostName());
+    assertEquals(1111, sa.getPort());
+    assertTrue(sa.isUnresolved());
+  }
+}