aff3f4f8e71b157e7a250d308ff0434d830519fe
Ronen Nachmias <ronenn@perion.com>
2016-09-16 14:58:32 +0300

    create leader election maven module to support RAFT leader election protocol

---
+++ leaderelection/src/main/java/io/scalecube/leaderelection/HeartbeatScheduler.java
@@ -0,0 +1,42 @@
+package io.scalecube.leaderelection;
+
+import io.scalecube.cluster.ICluster;
+import io.scalecube.transport.Message;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by ronenn on 9/13/2016.
+ */
+public class HeartbeatScheduler {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(HeartbeatScheduler.class);
+  private final ScheduledThreadPoolExecutor scheduler = new ScheduledThreadPoolExecutor(1);
+  public ICluster cluster;
+  long heartbeatInterval;
+
+  public HeartbeatScheduler(ICluster cluster, long heartbeatInterval) {
+    this.cluster = cluster;
+    this.heartbeatInterval = heartbeatInterval;
+  }
+
+  public void schedule() {
+    scheduler.scheduleAtFixedRate(new Runnable() {
+      @Override
+      public void run() {
+        LOGGER.debug("Leader Node: {} maintain leadership spread {} gossip regards selected leader {}",
+            cluster.address(), RaftProtocol.RAFT_PROTOCOL_HEARTBEAT, cluster.address());
+        cluster.spreadGossip(
+            Message.builder().qualifier(RaftProtocol.RAFT_PROTOCOL_HEARTBEAT).data(cluster.address()).build());
+      }
+    }, heartbeatInterval, heartbeatInterval, TimeUnit.SECONDS);
+  }
+
+  public void stop() {
+    scheduler.getQueue().clear();
+  }
+}
---
+++ leaderelection/src/main/java/io/scalecube/leaderelection/LeaderElection.java
@@ -0,0 +1,19 @@
+package io.scalecube.leaderelection;
+
+import io.scalecube.cluster.Cluster;
+import io.scalecube.cluster.ICluster;
+import io.scalecube.transport.Address;
+
+/**
+ * Created by ronenn on 9/12/2016.
+ */
+public interface LeaderElection {
+
+    Address leader();
+
+    void addStateListener(IStateListener handler);
+
+    RaftLeaderElection start();
+
+    ICluster cluster();
+}
