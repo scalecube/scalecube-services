f47b4740453968287a07ff89923e3abc35315a99
Ronen Nachmias <ronen.hamias@gmail.com>
2016-12-21 23:31:33 +0200

    core / 2

--- services/src/test/java/io/scalecube/services/ServiceTest.java
+++ services/src/test/java/io/scalecube/services/ServiceTest.java
@@ -561,7 +561,7 @@ public class ServiceTest extends BaseTest {
         .services(new GreetingServiceImpl())
         .build();
     
-    int cores = Runtime.getRuntime().availableProcessors();
+    int cores = Runtime.getRuntime().availableProcessors() / 2;
     
     for (int i=0 ; i < cores ; i ++) {
       // Create microservices cluster member.
