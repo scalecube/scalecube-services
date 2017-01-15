82e0e4245a1eebe9341a48bb9f981eab6c50a03f
Ronen Nachmias <ronen.hamias@gmail.com>
2017-01-15 18:35:12 +0200

    add coverage to thread factory constractor to make the code coverage at peace.

--- cluster/src/test/java/io/scalecube/cluster/membership/MembersipProtocolTest.java
+++ cluster/src/test/java/io/scalecube/cluster/membership/MembersipProtocolTest.java
@@ -13,9 +13,11 @@ public class MembersipProtocolTest extends BaseTest{
   @Test
   public void test_ipv6_thread_factory_addressing(){
     
+    ThreadFactory factory = new ThreadFactory();
+    
     Address address = Address.create("/fe80:0:0:0:8cf6:f5c8:c946:2c30%eno1", 4001);
     String nameFormat = "sc-membership-" + address.toString();
-    ThreadFactory.newSingleScheduledExecutorService(nameFormat);
+    factory.newSingleScheduledExecutorService(nameFormat);
 
     assertTrue(true); // if we reached here no IllegalFormatConversionException is thrown.
   }
