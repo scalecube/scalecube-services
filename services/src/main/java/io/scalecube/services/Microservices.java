b427ae8df37501f3e3ac3cafee0f34c1d555860c
ronen <ronen@ronen-pc.pisoft.local>
2017-07-04 07:06:05 +0300

    fix coday redundent import

--- services/src/main/java/io/scalecube/services/Microservices.java
+++ services/src/main/java/io/scalecube/services/Microservices.java
@@ -21,7 +21,6 @@ import java.lang.reflect.Field;
 import java.time.Duration;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
