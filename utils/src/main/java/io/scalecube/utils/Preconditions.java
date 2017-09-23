71e27379d1cb324355ad2a29d931c721414275e3
Ronen Nachmias <ronen.hamias@gmail.com>
2017-09-23 23:55:27 +0300

    check sytle

--- utils/src/main/java/io/scalecube/utils/Preconditions.java
+++ utils/src/main/java/io/scalecube/utils/Preconditions.java
@@ -100,25 +100,25 @@ public class Preconditions {
     // start substituting the arguments into the '%s' placeholders
     StringBuilder builder = new StringBuilder(template.length() + 16 * args.length);
     int templateStart = 0;
-    int i = 0;
-    while (i < args.length) {
+    int index = 0;
+    while (index < args.length) {
       int placeholderStart = template.indexOf("%s", templateStart);
       if (placeholderStart == -1) {
         break;
       }
       builder.append(template.substring(templateStart, placeholderStart));
-      builder.append(args[i++]);
+      builder.append(args[index++]);
       templateStart = placeholderStart + 2;
     }
     builder.append(template.substring(templateStart));
 
     // if we run out of placeholders, append the extra args in square braces
-    if (i < args.length) {
+    if (index < args.length) {
       builder.append(" [");
-      builder.append(args[i++]);
-      while (i < args.length) {
+      builder.append(args[index++]);
+      while (index < args.length) {
         builder.append(", ");
-        builder.append(args[i++]);
+        builder.append(args[index++]);
       }
       builder.append(']');
     }
