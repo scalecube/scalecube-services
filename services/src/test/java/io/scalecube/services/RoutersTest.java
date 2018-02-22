0db48f9459d83fd4e18377b640a43eca8097b1f5
aharonha <aharon@marketspulse.com>
2018-02-22 14:43:24 +0200

    A test (that currently fails)

--- services/src/test/java/io/scalecube/services/RoutersTest.java
+++ services/src/test/java/io/scalecube/services/RoutersTest.java
@@ -1,13 +1,19 @@
 package io.scalecube.services;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import io.scalecube.services.annotations.Service;
 import io.scalecube.services.routing.RandomServiceRouter;
 import io.scalecube.services.routing.Router;
 import io.scalecube.services.routing.RouterFactory;
+import io.scalecube.services.routing.Routers;
 import io.scalecube.testlib.BaseTest;
 
 import org.junit.Test;
+
+import java.util.concurrent.ExecutionException;
+import java.util.function.UnaryOperator;
 
 public class RoutersTest extends BaseTest {
 
@@ -23,5 +29,36 @@ public class RoutersTest extends BaseTest {
 
   }
 
+  @Service
+  @FunctionalInterface
+  interface StringUnaryOperator extends UnaryOperator<String> {
+  };
+
+  @Test
+  public void test_tag_router() throws InterruptedException, ExecutionException {
+
+
+    StringUnaryOperator upperCase = String::toUpperCase;
+    StringUnaryOperator loerCase = String::toLowerCase;
+    Microservices ms = Microservices.builder()
+        .services()
+        .service(upperCase).tag("to", "upper").add()
+        .service(loerCase).tag("to", "lower").add()
+        .build()
+        .build();
+
+
+
+    Microservices ms2 = Microservices.builder().seeds(ms.cluster().address()).build();
+    StringUnaryOperator uppercaseProxy =
+        ms2.proxy().router(Routers.withTag("to", "upper")).api(StringUnaryOperator.class).create();
+    StringUnaryOperator lowercaseProxy =
+        ms2.proxy().router(Routers.withTag("to", "lower")).api(StringUnaryOperator.class).create();
+
+    assertEquals("ABC", uppercaseProxy.apply("abC"));
+    assertEquals("abc", lowercaseProxy.apply("abC"));
+    ms.shutdown().get();
+
+  }
 
 }
