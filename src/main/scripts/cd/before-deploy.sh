cdba648a1b1b2ed315510488667f912561f09248
Aharon Hacmon <aharonha@users.noreply.github.com>
2018-03-12 13:46:55 +0200

    fix CD  (#236)
    * Test deployment (#234)
    
    Deployment fix
    
    * CD for travis.
    
    * CD for travis
    
    * add code-signing
    
    * CD for travis
    
    * restore default install and script

---
+++ src/main/scripts/cd/before-deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+	openssl aes-256-cbc -K $encrypted_d19fb18b4b9d_key -iv $encrypted_d19fb18b4b9d_iv -in   codesigning.asc.enc -out    codesigning.asc -d
+    gpg --fast-import codesigning.asc
+fi
