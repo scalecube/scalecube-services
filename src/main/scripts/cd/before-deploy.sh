95ce33b003ff948b9663132fc57f271a041bb8ae
aharonha <aharon@marketspulse.com>
2018-03-13 11:28:57 +0200

    fix dir path

--- src/main/scripts/cd/before-deploy.sh
+++ src/main/scripts/cd/before-deploy.sh
@@ -2,8 +2,10 @@
 
 
 # if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+	pushd src/main/scripts/cd
 	mkdir ~/tmp
 	openssl aes-256-cbc -K $encrypted_d19fb18b4b9d_key -iv $encrypted_d19fb18b4b9d_iv -in codesigning.asc.enc -out  ~/tmp/codesigning.asc -d
     gpg --fast-import ~/tmp/codesigning.asc
-    shred -z -u ~/tmp/codesigning.asc    
+    shred -z -u ~/tmp/codesigning.asc
+    popd    
 # fi
