brunocrt/scalecube

Renamed ExchangeStream to SubscriberStream, made safer subscribeOnNext method

examples/src/main/java/io/scalecube/examples/gateway/ServiceZipEchoRunner.java
@@ -2,9 +2,9 @@ package io.scalecube.examples.gateway;
 
 import static io.scalecube.ipc.ServiceMessage.copyFrom;
 
-import io.scalecube.ipc.ExchangeStream;
 import io.scalecube.ipc.ListeningServerStream;
 import io.scalecube.ipc.ServiceMessage;
+import io.scalecube.ipc.SubscriberStream;
 import io.scalecube.transport.Address;
 
 import rx.Observable;
@@ -22,17 +22,17 @@ public class ServiceZipEchoRunner {
    * Main method.
    */
   public static void main(String[] args) throws InterruptedException {
-    ExchangeStream exchangeStream = ExchangeStream.newExchangeStream();
+    SubscriberStream subscriberStream = SubscriberStream.newSubscriberStream();
 
     ListeningServerStream serverStream = ListeningServerStream.newServerStream().withPort(5801).bind();
     serverStream.listenMessageReadSuccess().subscribe(message -> {
 
       Observable<ServiceMessage> listen1 =
-          exchangeStream.send(ADDRESS1, copyFrom(message).qualifier("aaa").build());
+          subscriberStream.subscribeOnNext(ADDRESS1, copyFrom(message).qualifier("aaa").build());
       Observable<ServiceMessage> listen2 =
-          exchangeStream.send(ADDRESS2, copyFrom(message).qualifier("bbb").build());
+          subscriberStream.subscribeOnNext(ADDRESS2, copyFrom(message).qualifier("bbb").build());
       Observable<ServiceMessage> listen3 =
-          exchangeStream.send(ADDRESS3, copyFrom(message).qualifier("ccc").build());
+          subscriberStream.subscribeOnNext(ADDRESS3, copyFrom(message).qualifier("ccc").build());
 
       Observable.zip(listen1, listen2, listen3,
           (message1, message2, message3) -> {

scalecube/scalecube-services

examples/src/main/java/io/scalecube/examples/gateway/ServiceZipEchoRunner.java
@@ -2,9 +2,9 @@ package io.scalecube.examples.gateway;
 
 import static io.scalecube.ipc.ServiceMessage.copyFrom;
 
-import io.scalecube.ipc.ExchangeStream;
 import io.scalecube.ipc.ListeningServerStream;
 import io.scalecube.ipc.ServiceMessage;
+import io.scalecube.ipc.SubscriberStream;
 import io.scalecube.transport.Address;
 
 import rx.Observable;
@@ -22,17 +22,17 @@ public class ServiceZipEchoRunner {
    * Main method.
    */
   public static void main(String[] args) throws InterruptedException {
-    ExchangeStream exchangeStream = ExchangeStream.newExchangeStream();
+    SubscriberStream subscriberStream = SubscriberStream.newSubscriberStream();
 
     ListeningServerStream serverStream = ListeningServerStream.newServerStream().withPort(5801).bind();
     serverStream.listenMessageReadSuccess().subscribe(message -> {
 
       Observable<ServiceMessage> listen1 =
-          exchangeStream.send(ADDRESS1, copyFrom(message).qualifier("aaa").build());
+          subscriberStream.listenOnNext(ADDRESS1, copyFrom(message).qualifier("aaa").build());
       Observable<ServiceMessage> listen2 =
-          exchangeStream.send(ADDRESS2, copyFrom(message).qualifier("bbb").build());
+          subscriberStream.listenOnNext(ADDRESS2, copyFrom(message).qualifier("bbb").build());
       Observable<ServiceMessage> listen3 =
-          exchangeStream.send(ADDRESS3, copyFrom(message).qualifier("ccc").build());
+          subscriberStream.listenOnNext(ADDRESS3, copyFrom(message).qualifier("ccc").build());
 
       Observable.zip(listen1, listen2, listen3,
           (message1, message2, message3) -> {