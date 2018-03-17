brunocrt/scalecube

Created StreamProcessor, StreamProcessorFactory; WIP WIP

ipc/src/main/java/io/scalecube/ipc/DefaultEventStream.java
@@ -2,10 +2,15 @@ package io.scalecube.ipc;
 
 import static io.scalecube.ipc.Event.Topic;
 
+import io.scalecube.transport.Address;
+
 import rx.Observable;
+import rx.Subscription;
 import rx.subjects.PublishSubject;
 import rx.subjects.Subject;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
@@ -13,6 +18,8 @@ public class DefaultEventStream implements EventStream {
 
   private final Subject<Event, Event> subject = PublishSubject.<Event>create().toSerialized();
   private final Subject<Event, Event> closeSubject = PublishSubject.<Event>create().toSerialized();
+
+  private final ConcurrentMap<ChannelContext, Subscription> subscriptions = new ConcurrentHashMap<>();
 
   private final Function<Event, Event> eventMapper;
 
@@ -26,8 +33,26 @@ public class DefaultEventStream implements EventStream {
 
   @Override
   public final void subscribe(ChannelContext channelContext) {
-    channelContext.listen().subscribe(this::onNext, error -> onChannelContextInactiveDueError(channelContext, error));
+    // register cleanup process upfront
     channelContext.listenClose(this::onChannelContextClosed);
+
+    // bind channelContext to this event stream
+    Subscription subscription = channelContext.listen()
+        .doOnUnsubscribe(() -> onChannelContextUnsubscribed(channelContext))
+        .subscribe(this::onNext);
+
+    // save subscription
+    subscriptions.put(channelContext, subscription);
+  }
+
+  @Override
+  public final void unsubscribe(Address address) {
+    // full scan and filter then unsubscribe
+    subscriptions.keySet().forEach(channelContext -> {
+      if (address.equals(channelContext.getAddress())) {
+        unsubscribe(channelContext);
+      }
+    });
   }
 
   @Override
@@ -39,6 +64,8 @@ public class DefaultEventStream implements EventStream {
   public final void close() {
     subject.onCompleted();
     closeSubject.onCompleted();
+    // full scan and unsubscribe
+    subscriptions.keySet().forEach(this::unsubscribe);
   }
 
   @Override
@@ -52,13 +79,18 @@ public class DefaultEventStream implements EventStream {
   }
 
   private void onChannelContextClosed(ChannelContext channelContext) {
-    subject.onNext(new Event.Builder(Topic.ChannelContextInactive, channelContext).build());
+    unsubscribe(channelContext);
+    subject.onNext(new Event.Builder(Topic.ChannelContextClosed, channelContext).build());
   }
 
-  // Hint: this method exists only for consistency reasons; it's unlikely that onError ever be called on the
-  // ChannelContext; at the moment of writing this comment there's no such flow that would call ChannelContext's
-  // subject.onError(throwable)
-  private void onChannelContextInactiveDueError(ChannelContext channelContext, Throwable throwable) {
-    subject.onNext(new Event.Builder(Topic.ChannelContextInactive, channelContext).error(throwable).build());
+  private void onChannelContextUnsubscribed(ChannelContext channelContext) {
+    subject.onNext(new Event.Builder(Topic.ChannelContextUnsubscribed, channelContext).build());
+  }
+
+  private void unsubscribe(ChannelContext channelContext) {
+    Subscription subscription = subscriptions.remove(channelContext);
+    if (subscription != null) {
+      subscription.unsubscribe();
+    }
   }
 }

ipc/src/test/java/io/scalecube/ipc/ClientStreamTest.java
@@ -21,29 +21,30 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ClientStreamTest {
 
+  private ClientStream clientStream;
   private ListeningServerStream serverStream;
-  private Address address;
+  private Address serverAddress;
 
   @Before
   public void setUp() throws Exception {
+    clientStream = ClientStream.newClientStream();
     serverStream = ListeningServerStream.newServerStream().bind();
-    address = serverStream.listenBind().toBlocking().toFuture().get(3, TimeUnit.SECONDS);
+    serverAddress = serverStream.listenBind().toBlocking().toFuture().get(3, TimeUnit.SECONDS);
   }
 
   @After
   public void cleanUp() throws Exception {
+    clientStream.close();
     serverStream.close();
-    address = serverStream.listenUnbind().toBlocking().toFuture().get(3, TimeUnit.SECONDS);
+    serverAddress = serverStream.listenUnbind().toBlocking().toFuture().get(3, TimeUnit.SECONDS);
   }
 
   @Test
   public void testClientStreamEmitsWriteEvents() throws Exception {
-    ClientStream clientStream = ClientStream.newClientStream();
-
     Subject<Event, Event> emittedEventsSubject = ReplaySubject.create();
     clientStream.listen().subscribe(emittedEventsSubject);
 
-    clientStream.send(address, ServiceMessage.withQualifier("q/hello").build());
+    clientStream.send(serverAddress, ServiceMessage.withQualifier("q/hello").build());
 
     List<Event> events = emittedEventsSubject.buffer(2).timeout(3, TimeUnit.SECONDS).toBlocking().first();
     assertEquals(2, events.size());
@@ -56,8 +57,7 @@ public class ClientStreamTest {
     Subject<Event, Event> requestSubject = ReplaySubject.create();
     serverStream.listen().subscribe(requestSubject);
 
-    ClientStream clientStream = ClientStream.newClientStream();
-    clientStream.send(address, ServiceMessage.withQualifier("hola").build());
+    clientStream.send(serverAddress, ServiceMessage.withQualifier("hola").build());
 
     List<Event> events = requestSubject.buffer(1).timeout(3, TimeUnit.SECONDS).toBlocking().first();
     assertEquals(1, events.size());
@@ -68,11 +68,10 @@ public class ClientStreamTest {
   public void testClientStreamReceivesFromServerStream() throws Exception {
     serverStream.listenMessageReadSuccess().subscribe(serverStream::send);
 
-    ClientStream clientStream = ClientStream.newClientStream();
     Subject<Event, Event> responseSubject = ReplaySubject.create();
     clientStream.listen().filter(Event::isReadSuccess).subscribe(responseSubject);
 
-    clientStream.send(address, ServiceMessage.withQualifier("echo").build());
+    clientStream.send(serverAddress, ServiceMessage.withQualifier("echo").build());
 
     List<Event> events = responseSubject.buffer(1).timeout(3, TimeUnit.SECONDS).toBlocking().first();
     assertEquals(1, events.size());
@@ -81,50 +80,49 @@ public class ClientStreamTest {
 
   @Test
   public void testClientStreamOnClose() throws Exception {
-    ClientStream clientStream = ClientStream.newClientStream();
     AtomicBoolean onCloseBoolean = new AtomicBoolean();
     clientStream.listenClose(aVoid -> onCloseBoolean.set(true));
-
-    clientStream.send(address, ServiceMessage.withQualifier("q/hello").build());
-
-    Subject<Event, Event> emittedEventsSubject = ReplaySubject.create();
-    clientStream.listen().subscribe(emittedEventsSubject);
-
-    List<Event> events = emittedEventsSubject.buffer(2).timeout(3, TimeUnit.SECONDS).toBlocking().first();
-    assertEquals(2, events.size());
-    assertEquals(Topic.MessageWrite, events.get(0).getTopic());
-    assertEquals(Topic.WriteSuccess, events.get(1).getTopic());
-
     clientStream.close();
-
     assertTrue(onCloseBoolean.get());
   }
 
   @Test
-  public void testClientStreamOnCloseRemotePartyClosed() throws Exception {
-    ClientStream clientStream = ClientStream.newClientStream();
+  public void testClientStreamConnectFailed() throws Exception {
+    BehaviorSubject<Address> connectFailedSubject = BehaviorSubject.create();
+    clientStream.listenConnectFailed().subscribe(connectFailedSubject);
 
-    clientStream.send(address, ServiceMessage.withQualifier("q/hello").build());
+    Address failedAddress = Address.from("localhost:0");
+    clientStream.send(failedAddress, ServiceMessage.withQualifier("q/hello").build());
 
-    Subject<Event, Event> emittedEventsSubject = ReplaySubject.create();
-    clientStream.listen().subscribe(emittedEventsSubject);
+    Address actualFailedAddress = connectFailedSubject.timeout(3, TimeUnit.SECONDS).toBlocking().getIterator().next();
+    assertEquals(failedAddress, actualFailedAddress);
+  }
 
-    List<Event> events = emittedEventsSubject.buffer(2).timeout(3, TimeUnit.SECONDS).toBlocking().first();
-    assertEquals(2, events.size());
-    assertEquals(Topic.MessageWrite, events.get(0).getTopic());
-    assertEquals(Topic.WriteSuccess, events.get(1).getTopic());
+  @Test
+  public void testClientStreamRemotePartyClosed() throws Exception {
+    Subject<Event, Event> requestSubject = ReplaySubject.create();
+    serverStream.listen().subscribe(requestSubject);
+
+    clientStream.send(serverAddress, ServiceMessage.withQualifier("q/hello").build());
+
+    List<Event> events = requestSubject.buffer(1).timeout(3, TimeUnit.SECONDS).toBlocking().first();
+    assertEquals(1, events.size());
+    assertEquals(Topic.ReadSuccess, events.get(0).getTopic());
 
     // close remote party and receive corresp events
     BehaviorSubject<Event> channelInactiveSubject = BehaviorSubject.create();
-    clientStream.listenChannelContextInactive().subscribe(channelInactiveSubject);
+    clientStream.listenChannelContextClosed().subscribe(channelInactiveSubject);
+
     // unbind server channel at serverStream
     serverStream.close();
     serverStream.listenUnbind().toBlocking().toFuture().get(3, TimeUnit.SECONDS);
+
     // await a bit
     TimeUnit.SECONDS.sleep(3);
+
     // assert that clientStream received events about closed channels corresp to serverStream channels
     Event event = channelInactiveSubject.test().getOnNextEvents().get(0);
-    assertEquals(Topic.ChannelContextInactive, event.getTopic());
+    assertEquals(Topic.ChannelContextClosed, event.getTopic());
     assertFalse(event.hasError());
   }
 }

scalecube/scalecube-services

ipc/src/main/java/io/scalecube/ipc/DefaultEventStream.java
@@ -2,10 +2,15 @@ package io.scalecube.ipc;
 
 import static io.scalecube.ipc.Event.Topic;
 
+import io.scalecube.transport.Address;
+
 import rx.Observable;
+import rx.Subscription;
 import rx.subjects.PublishSubject;
 import rx.subjects.Subject;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
@@ -13,6 +18,8 @@ public class DefaultEventStream implements EventStream {
 
   private final Subject<Event, Event> subject = PublishSubject.<Event>create().toSerialized();
   private final Subject<Event, Event> closeSubject = PublishSubject.<Event>create().toSerialized();
+
+  private final ConcurrentMap<ChannelContext, Subscription> subscriptions = new ConcurrentHashMap<>();
 
   private final Function<Event, Event> eventMapper;
 
@@ -26,8 +33,26 @@ public class DefaultEventStream implements EventStream {
 
   @Override
   public final void subscribe(ChannelContext channelContext) {
-    channelContext.listen().subscribe(this::onNext, error -> onChannelContextInactiveDueError(channelContext, error));
+    // register cleanup process upfront
     channelContext.listenClose(this::onChannelContextClosed);
+
+    // bind channelContext to this event stream
+    Subscription subscription = channelContext.listen()
+        .doOnUnsubscribe(() -> onChannelContextUnsubscribed(channelContext))
+        .subscribe(this::onNext);
+
+    // save subscription
+    subscriptions.put(channelContext, subscription);
+  }
+
+  @Override
+  public final void unsubscribe(Address address) {
+    // full scan and filter then unsubscribe
+    subscriptions.keySet().forEach(channelContext -> {
+      if (address.equals(channelContext.getAddress())) {
+        cleanupSubscription(channelContext);
+      }
+    });
   }
 
   @Override
@@ -37,6 +62,9 @@ public class DefaultEventStream implements EventStream {
 
   @Override
   public final void close() {
+    // full scan and unsubscribe
+    subscriptions.keySet().forEach(this::cleanupSubscription);
+    // complete observers
     subject.onCompleted();
     closeSubject.onCompleted();
   }
@@ -51,14 +79,19 @@ public class DefaultEventStream implements EventStream {
     subject.onNext(event);
   }
 
-  private void onChannelContextClosed(ChannelContext channelContext) {
-    subject.onNext(new Event.Builder(Topic.ChannelContextInactive, channelContext).build());
+  private void onChannelContextClosed(ChannelContext ctx) {
+    cleanupSubscription(ctx);
+    subject.onNext(new Event.Builder(Topic.ChannelContextClosed, ctx.getAddress(), ctx.getId()).build());
   }
 
-  // Hint: this method exists only for consistency reasons; it's unlikely that onError ever be called on the
-  // ChannelContext; at the moment of writing this comment there's no such flow that would call ChannelContext's
-  // subject.onError(throwable)
-  private void onChannelContextInactiveDueError(ChannelContext channelContext, Throwable throwable) {
-    subject.onNext(new Event.Builder(Topic.ChannelContextInactive, channelContext).error(throwable).build());
+  private void onChannelContextUnsubscribed(ChannelContext ctx) {
+    subject.onNext(new Event.Builder(Topic.ChannelContextUnsubscribed, ctx.getAddress(), ctx.getId()).build());
+  }
+
+  private void cleanupSubscription(ChannelContext channelContext) {
+    Subscription subscription = subscriptions.remove(channelContext);
+    if (subscription != null) {
+      subscription.unsubscribe();
+    }
   }
 }

ipc/src/test/java/io/scalecube/ipc/ClientStreamTest.java
@@ -21,29 +21,30 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ClientStreamTest {
 
+  private ClientStream clientStream;
   private ListeningServerStream serverStream;
-  private Address address;
+  private Address serverAddress;
 
   @Before
   public void setUp() throws Exception {
+    clientStream = ClientStream.newClientStream();
     serverStream = ListeningServerStream.newServerStream().bind();
-    address = serverStream.listenBind().toBlocking().toFuture().get(3, TimeUnit.SECONDS);
+    serverAddress = serverStream.listenBind().toBlocking().toFuture().get(3, TimeUnit.SECONDS);
   }
 
   @After
   public void cleanUp() throws Exception {
+    clientStream.close();
     serverStream.close();
-    address = serverStream.listenUnbind().toBlocking().toFuture().get(3, TimeUnit.SECONDS);
+    serverAddress = serverStream.listenUnbind().toBlocking().toFuture().get(3, TimeUnit.SECONDS);
   }
 
   @Test
   public void testClientStreamEmitsWriteEvents() throws Exception {
-    ClientStream clientStream = ClientStream.newClientStream();
-
     Subject<Event, Event> emittedEventsSubject = ReplaySubject.create();
     clientStream.listen().subscribe(emittedEventsSubject);
 
-    clientStream.send(address, ServiceMessage.withQualifier("q/hello").build());
+    clientStream.send(serverAddress, ServiceMessage.withQualifier("q/hello").build());
 
     List<Event> events = emittedEventsSubject.buffer(2).timeout(3, TimeUnit.SECONDS).toBlocking().first();
     assertEquals(2, events.size());
@@ -56,8 +57,7 @@ public class ClientStreamTest {
     Subject<Event, Event> requestSubject = ReplaySubject.create();
     serverStream.listen().subscribe(requestSubject);
 
-    ClientStream clientStream = ClientStream.newClientStream();
-    clientStream.send(address, ServiceMessage.withQualifier("hola").build());
+    clientStream.send(serverAddress, ServiceMessage.withQualifier("hola").build());
 
     List<Event> events = requestSubject.buffer(1).timeout(3, TimeUnit.SECONDS).toBlocking().first();
     assertEquals(1, events.size());
@@ -68,11 +68,10 @@ public class ClientStreamTest {
   public void testClientStreamReceivesFromServerStream() throws Exception {
     serverStream.listenMessageReadSuccess().subscribe(serverStream::send);
 
-    ClientStream clientStream = ClientStream.newClientStream();
     Subject<Event, Event> responseSubject = ReplaySubject.create();
     clientStream.listen().filter(Event::isReadSuccess).subscribe(responseSubject);
 
-    clientStream.send(address, ServiceMessage.withQualifier("echo").build());
+    clientStream.send(serverAddress, ServiceMessage.withQualifier("echo").build());
 
     List<Event> events = responseSubject.buffer(1).timeout(3, TimeUnit.SECONDS).toBlocking().first();
     assertEquals(1, events.size());
@@ -81,50 +80,51 @@ public class ClientStreamTest {
 
   @Test
   public void testClientStreamOnClose() throws Exception {
-    ClientStream clientStream = ClientStream.newClientStream();
     AtomicBoolean onCloseBoolean = new AtomicBoolean();
     clientStream.listenClose(aVoid -> onCloseBoolean.set(true));
-
-    clientStream.send(address, ServiceMessage.withQualifier("q/hello").build());
-
-    Subject<Event, Event> emittedEventsSubject = ReplaySubject.create();
-    clientStream.listen().subscribe(emittedEventsSubject);
-
-    List<Event> events = emittedEventsSubject.buffer(2).timeout(3, TimeUnit.SECONDS).toBlocking().first();
-    assertEquals(2, events.size());
-    assertEquals(Topic.MessageWrite, events.get(0).getTopic());
-    assertEquals(Topic.WriteSuccess, events.get(1).getTopic());
-
     clientStream.close();
-
     assertTrue(onCloseBoolean.get());
   }
 
   @Test
-  public void testClientStreamOnCloseRemotePartyClosed() throws Exception {
-    ClientStream clientStream = ClientStream.newClientStream();
+  public void testClientStreamSendAttemptFailed() throws Exception {
+    BehaviorSubject<Event> sendFailedSubject = BehaviorSubject.create();
+    clientStream.listen().filter(Event::isWriteError).subscribe(sendFailedSubject);
 
-    clientStream.send(address, ServiceMessage.withQualifier("q/hello").build());
+    Address address = Address.from("host:1234");
+    ServiceMessage message = ServiceMessage.withQualifier("q/hello").build();
+    clientStream.send(address, message);
 
-    Subject<Event, Event> emittedEventsSubject = ReplaySubject.create();
-    clientStream.listen().subscribe(emittedEventsSubject);
+    Event event = sendFailedSubject.timeout(3, TimeUnit.SECONDS).toBlocking().getIterator().next();
+    assertEquals(address, event.getAddress());
+    assertEquals(message, event.getMessageOrThrow());
+  }
 
-    List<Event> events = emittedEventsSubject.buffer(2).timeout(3, TimeUnit.SECONDS).toBlocking().first();
-    assertEquals(2, events.size());
-    assertEquals(Topic.MessageWrite, events.get(0).getTopic());
-    assertEquals(Topic.WriteSuccess, events.get(1).getTopic());
+  @Test
+  public void testClientStreamRemotePartyClosed() throws Exception {
+    Subject<Event, Event> requestSubject = ReplaySubject.create();
+    serverStream.listen().subscribe(requestSubject);
+
+    clientStream.send(serverAddress, ServiceMessage.withQualifier("q/hello").build());
+
+    List<Event> events = requestSubject.buffer(1).timeout(3, TimeUnit.SECONDS).toBlocking().first();
+    assertEquals(1, events.size());
+    assertEquals(Topic.ReadSuccess, events.get(0).getTopic());
 
     // close remote party and receive corresp events
     BehaviorSubject<Event> channelInactiveSubject = BehaviorSubject.create();
-    clientStream.listenChannelContextInactive().subscribe(channelInactiveSubject);
+    clientStream.listenChannelContextClosed().subscribe(channelInactiveSubject);
+
     // unbind server channel at serverStream
     serverStream.close();
     serverStream.listenUnbind().toBlocking().toFuture().get(3, TimeUnit.SECONDS);
+
     // await a bit
     TimeUnit.SECONDS.sleep(3);
+
     // assert that clientStream received events about closed channels corresp to serverStream channels
     Event event = channelInactiveSubject.test().getOnNextEvents().get(0);
-    assertEquals(Topic.ChannelContextInactive, event.getTopic());
+    assertEquals(Topic.ChannelContextClosed, event.getTopic());
     assertFalse(event.hasError());
   }
 }