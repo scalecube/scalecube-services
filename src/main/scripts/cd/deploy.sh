1c6c8c8fd1beaccba78e408f8595352c1f103551
aharonha <aharon@marketspulse.com>
2018-03-18 18:21:47 +0200

    push to develop after finishing release

--- src/main/scripts/cd/deploy.sh
+++ src/main/scripts/cd/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mvn -P release release:prepare release:perform -DautoVersionSubmodules=true -DscmCommentPrefix='[skip ci] [maven-release-plugin]' -DskipTests=true -Dmaven.javadoc.skip=true -B -V -s travis-settings.xml
-git status
+git push origin/develop
