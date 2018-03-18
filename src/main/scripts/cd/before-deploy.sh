bb2331795866de36e061e275f186adcd640a6d8d
aharonha <aharon@marketspulse.com>
2018-03-18 12:04:02 +0200

    Fix deployment scripts:
    + decrypt secrets
    + config git remote as ssh connection
    + config git user name and email
    + internal tests
    + release prepare and perform on one mvn execution

--- src/main/scripts/cd/before-deploy.sh
+++ src/main/scripts/cd/before-deploy.sh
@@ -2,10 +2,23 @@
 
 
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ] &&  [ "$TRAVIS_BRANCH" = 'master' ]  || [ "$TRAVIS_BRANCH" = 'develop' ]; then
+    git remote set-url origin git@github.com:$TRAVIS_REPO_SLUG.git
+	git config --global user.email "io.scalecube.ci@gmail.com"
+    git config --global user.name "io-scalecube-ci"
+    git config --global commit.gpgSign false
 	pushd src/main/scripts/cd
 	mkdir ~/tmp
-	openssl aes-256-cbc -K $encrypted_d19fb18b4b9d_key -iv $encrypted_d19fb18b4b9d_iv -in codesigning.asc.enc -out  ~/tmp/codesigning.asc -d
+	openssl aes-256-cbc -K $encrypted_d19fb18b4b9d_key -iv $encrypted_d19fb18b4b9d_iv -in secrets.tar.enc -out ~/tmp/secrets.tar -d
+	md5sum secrets.tar
+	tar -xvf secrets.tar -C  ~/tmp
+	md5sum ~/tmp/*
     gpg --fast-import ~/tmp/codesigning.asc
+    md5sum ~/tmp/id_rsa
+    chmod 600 ~/tmp/id_rsa
     shred -z -u ~/tmp/codesigning.asc
+	eval "$(ssh-agent -s)"
+	ssh-add ~/tmp/id_rsa
+	ssh -T git@github.com | true
+	git fetch
     popd    
 fi
