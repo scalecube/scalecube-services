0885af0186597eece14915b07acc5956f651614a
aharonha <aharon@marketspulse.com>
2018-03-18 12:45:28 +0200

    some more prompts

--- src/main/scripts/ci/before-install.sh
+++ src/main/scripts/ci/before-install.sh
@@ -5,6 +5,7 @@ sudo apt-get install libxml-xpath-perl
 # get latest version of codacy reporter from sonatype
 latest=$(curl "https://oss.sonatype.org/service/local/repositories/releases/content/com/codacy/codacy-coverage-reporter/maven-metadata.xml" | xpath -e "/metadata/versioning/release/text()")
 
+echo Downloading latest version ($latest) of codacy reporter from sonatype 
 # download laterst assembly jar 
 mvn dependency:get dependency:copy \
    -DoutputDirectory=~ \
