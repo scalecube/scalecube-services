036da992dde933bd9c4e402993bb7a0a813c3e55
aharonha <aharon@marketspulse.com>
2018-03-19 16:40:10 +0200

    fix quotes

--- src/main/scripts/cd/deploy.sh
+++ src/main/scripts/cd/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-mvn -P release release:prepare release:perform -DautoVersionSubmodules=true -DscmCommentPrefix='$TRAVIS_COMMIT_MESSAGE [skip ci]' -DskipTests=true -B -V -s travis-settings.xml
+mvn -P release release:prepare release:perform -DautoVersionSubmodules=true -DscmCommentPrefix="$TRAVIS_COMMIT_MESSAGE [skip ci]" -DskipTests=true -B -V -s travis-settings.xml
 
 git checkout develop
 git merge --ff-only master && git push origin develop
