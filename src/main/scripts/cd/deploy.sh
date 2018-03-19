b33853bce1120255956045482a40a6f3b345ef4b
aharonha <aharon@marketspulse.com>
2018-03-19 09:25:59 +0200

    Do not skip javadocs

--- src/main/scripts/cd/deploy.sh
+++ src/main/scripts/cd/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mvn -P release release:prepare release:perform -DautoVersionSubmodules=true -DscmCommentPrefix='[skip ci] [maven-release-plugin]' -DskipTests=true -Dmaven.javadoc.skip=true -B -V -s travis-settings.xml
+mvn -P release release:prepare release:perform -DautoVersionSubmodules=true -DscmCommentPrefix='[skip ci] [maven-release-plugin]' -DskipTests=true -B -V -s travis-settings.xml
 git push origin/develop
