37cf70b9f5a9476d4e593504f5c8841582fa81f3
ronen <ronen.hamias@gmail.com>
2018-03-26 09:14:15 +0300

    fix example make it work

--- examples/src/main/java/io/scalecube/examples/streams/ServiceStreamsRunner.java
+++ examples/src/main/java/io/scalecube/examples/streams/ServiceStreamsRunner.java
@@ -2,10 +2,20 @@ package io.scalecube.examples.streams;
 
 import io.scalecube.examples.services.GreetingServiceImpl;
 import io.scalecube.services.streams.ServiceStreams;
+import io.scalecube.streams.StreamProcessors;
+import io.scalecube.streams.StreamProcessors.ServerStreamProcessors;
+
+import rx.Subscription;
+
+import java.util.List;
 
 public class ServiceStreamsRunner {
   public static void main(String[] args) {
-    ServiceStreams.builder().build().from(new GreetingServiceImpl());
+    ServerStreamProcessors sp = StreamProcessors.server().port(8000).build();
+    ServiceStreams.builder().server(sp).build().from(new GreetingServiceImpl());
+    sp.bind().whenComplete((s,r)->{
+      System.out.println(s);
+    });
   }
 }
 
