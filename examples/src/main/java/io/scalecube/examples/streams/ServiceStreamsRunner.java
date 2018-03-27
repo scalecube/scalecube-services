17fe83aa0181b8044e0c4c473119669a0b72a38f
artem-v <artem.vysochyn@jivygroup.com>
2018-03-27 21:29:15 +0300

    WIP; minor

--- examples/src/main/java/io/scalecube/examples/streams/ServiceStreamsRunner.java
+++ examples/src/main/java/io/scalecube/examples/streams/ServiceStreamsRunner.java
@@ -1,22 +1,21 @@
 package io.scalecube.examples.streams;
 
 import io.scalecube.examples.services.GreetingServiceImpl;
+import io.scalecube.examples.services.stocks.SimpleQuoteService;
 import io.scalecube.services.streams.ServiceStreams;
 import io.scalecube.streams.StreamProcessors;
 import io.scalecube.streams.StreamProcessors.ServerStreamProcessors;
 
-import rx.Subscription;
-
-import java.util.List;
-
 public class ServiceStreamsRunner {
+
   public static void main(String[] args) {
-    ServerStreamProcessors sp = StreamProcessors.server().port(8000).build();
-    
-    ServiceStreams.builder().server(sp).build().from(new GreetingServiceImpl());
-    sp.bind().whenComplete((s,r)->{
-      System.out.println(s);
-    });
+    ServerStreamProcessors server = StreamProcessors.server().port(8000).build();
+
+    ServiceStreams serviceStreams = new ServiceStreams(server);
+
+    serviceStreams.createSubscriptions(new GreetingServiceImpl());
+    serviceStreams.createSubscriptions(new SimpleQuoteService());
+
+    server.bind().whenComplete((s, r) -> System.out.println(s));
   }
 }
-
