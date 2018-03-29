12d53bb2e86546fafb5c805e7f469e2443b39756
ronen <ronen.hamias@gmail.com>
2018-03-29 20:49:40 +0300

    refactor service call

--- services/src/test/java/io/scalecube/services/CoarseGrainedServiceImpl.java
+++ services/src/test/java/io/scalecube/services/CoarseGrainedServiceImpl.java
@@ -46,8 +46,7 @@ public class CoarseGrainedServiceImpl implements CoarseGrainedService {
   public CompletableFuture<String> callGreetingWithDispatcher(String request) {
     final CompletableFuture<String> future = new CompletableFuture<String>();
 
-    ServiceCall service = ms.call().create();
-    service.invoke(Messages.builder().request(SERVICE_NAME, "greeting")
+    ms.call().invoke(Messages.builder().request(SERVICE_NAME, "greeting")
         .data("joe").build()).whenComplete((message, error) -> {
           future.complete(message.data().toString());
         });
