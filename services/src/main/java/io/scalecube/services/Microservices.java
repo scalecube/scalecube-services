655a75e014023d6b90d3a58eb95637861474f963
ronen <ronen.hamias@gmail.com>
2018-03-30 07:35:31 +0300

    add metrics to default service call builder out of microservices instance

--- services/src/main/java/io/scalecube/services/Microservices.java
+++ services/src/main/java/io/scalecube/services/Microservices.java
@@ -369,8 +369,7 @@ public class Microservices {
   }
 
   public Call call() {
-   
     Router router = this.router(RoundRobinServiceRouter.class);
-    return ServiceCall.call().router(router);
+    return ServiceCall.call().metrics(metrics).router(router);
   }
 }
