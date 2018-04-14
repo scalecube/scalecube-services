7c59bde7d9946140dbe20055d7ebc170715031f3
ronen <ronen@ronen-pc.pisoft.local>
2018-04-14 17:54:44 +0300

    Add transport factory as service loader

--- src/main/java/io/scalecube/services/transport/client/api/ClientTransport.java
+++ src/main/java/io/scalecube/services/transport/client/api/ClientTransport.java
@@ -6,9 +6,4 @@ public interface ClientTransport {
 
   public ClientChannel create(Address address);
 
-  public static ClientTransport newClient() {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
 }
