f7dbebc464ebd20491a5d4392dd372d8dd3650dc
ronen <ronen.hamias@gmail.com>
2018-04-15 22:16:06 +0300

    add utils

---
+++ services/src/main/java/io/scalecube/concurrency/ThreadFactory.java
@@ -0,0 +1,33 @@
+package io.scalecube.concurrency;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+/**
+ * Used to create and cache shared thread pools with given name.
+ */
+public class ThreadFactory {
+
+  private static final ConcurrentMap<String, ScheduledExecutorService> schedulers = new ConcurrentHashMap<>();
+  public static String SC_SERVICES_TIMEOUT = "sc-services-timeout";
+
+  /**
+   * Used to create and cache shared thread pools with given name.
+   * 
+   * @param name the requested name of the single thread executor if not cached will be created.
+   * @return computed or existing scheduled executor
+   */
+  public static ScheduledExecutorService singleScheduledExecutorService(String name) {
+    return schedulers.computeIfAbsent(name, ThreadFactory::compute);
+  }
+
+  private static ScheduledExecutorService compute(String name) {
+    return Executors.newSingleThreadScheduledExecutor(
+        new ThreadFactoryBuilder().setNameFormat(name).setDaemon(true).build());
+  }
+
+}
