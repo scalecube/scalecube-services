b1f0a7ec62d9822eb4e174843004b483a049b5fd
ronen <ronen.hamias@gmail.com>
2018-04-17 23:12:41 +0300

    working on discovery

--- services/src/main/java/io/scalecube/services/discovery/ServiceDiscovery.java
+++ services/src/main/java/io/scalecube/services/discovery/ServiceDiscovery.java
@@ -1,5 +1,6 @@
 package io.scalecube.services.discovery;
 
+import io.scalecube.cluster.Cluster;
 import io.scalecube.cluster.ClusterConfig;
 import io.scalecube.cluster.Member;
 import io.scalecube.concurrency.ThreadFactory;
@@ -11,9 +12,13 @@ import io.scalecube.services.registry.ServiceRegistryImpl;
 import io.scalecube.services.registry.api.ServiceRegistry;
 import io.scalecube.transport.Address;
 
+import com.google.common.collect.Sets;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
@@ -25,6 +30,8 @@ public class ServiceDiscovery {
 
   private final ServiceRegistry serviceRegistry;
 
+  private Cluster cluster;
+
   private static final Logger LOGGER = LoggerFactory.getLogger(ServiceDiscovery.class);
 
   private enum DiscoveryType {
@@ -35,94 +42,11 @@ public class ServiceDiscovery {
     this.serviceRegistry = serviceRegistry;
   }
 
-  private void listenCluster() {
-    microservices.cluster().listenMembership().subscribe(event -> {
-      if (event.isAdded()) {
-        loadMemberServices(DiscoveryType.ADDED, event.member());
-      } else if (event.isRemoved()) {
-        loadMemberServices(DiscoveryType.REMOVED, event.member());
-      }
-    });
-    ThreadFactory.singleScheduledExecutorService("service-registry-discovery")
-        .scheduleAtFixedRate(this::loadClusterServices, 10, 10, TimeUnit.SECONDS);
-  }
-
-  private void loadClusterServices() {
-    this.microservices.cluster().otherMembers().forEach(member -> {
-      loadMemberServices(DiscoveryType.DISCOVERED, member);
-    });
-  }
-  
-  private void loadMemberServices(DiscoveryType type, Member member) {
-    member.metadata().entrySet().stream()
-        .filter(entry -> "service".equals(entry.getValue())) // filter service tags
-        .forEach(entry -> {
-          Address serviceAddress = getServiceAddress(member);
-          ServiceInfo info = ServiceInfo.from(entry.getKey());
-          ServiceReference serviceRef = new ServiceReference(
-              member.id(),
-              info.getServiceName(),
-              info.methods(),
-              serviceAddress);
-
-          LOGGER.debug("Member: {} is {} : {}", member, type, serviceRef);
-          if (type.equals(DiscoveryType.ADDED) || type.equals(DiscoveryType.DISCOVERED)) {
-            if (!serviceInstances.containsKey(serviceRef)) {
-              serviceInstances.putIfAbsent(serviceRef,
-                  new ServiceInstance(microservices.client(), serviceRef, info.getTags()));
-              LOGGER.info("Service Reference was ADDED since new Member has joined the cluster {} : {}", member,
-                  serviceRef);
-            }
-          } else if (type.equals(DiscoveryType.REMOVED)) {
-            serviceInstances.remove(serviceRef);
-            LOGGER.info("Service Reference was REMOVED since Member have left the cluster {} : {}", member,
-                serviceRef);
-          }
-        });
-  }
-
-  ClusterConfig cfg = getClusterConfig(clusterConfig, services, serviceAddress).build();
-  this.cluster = Cluster.joinAwait(cfg); 
-
-  private ClusterConfig.Builder getClusterConfig(ClusterConfig.Builder clusterConfig, Services services,
-      Address address) {
-    if (services != null && !services.list().isEmpty()) {
-      clusterConfig.addMetadata(Microservices.metadata(services));
-      if (address != null) {
-        clusterConfig.addMetadata("service-address", address.toString());
-      }
-    }
-    return clusterConfig;
-  }
-
-  private static Map<String, String> metadata(Services services) {
-    Map<String, String> servicesTags = new HashMap<>();
-
-
-    services.stream().forEach(service -> {
-      // constract info metadata object as json key
-      String key = new ServiceInfo(service.serviceName(),
-          service.methods(),
-          service.tags()).toMetadata();
-      
-      // add service metadata to map.
-      servicesTags.put(key, "service");
-    });
-
-    return servicesTags;
-  }
-  
-  private Address getServiceAddress(Member member) {
-    String serviceAddressAsString = member.metadata().get("service-address");
-    if (serviceAddressAsString != null) {
-      return Address.from(serviceAddressAsString);
-    } else {
-      return member.address();
-    }
-  }
-
-  @Override
-  public void start() {
+  public void start(ClusterConfig.Builder config) {
+    
+    Collection<ServiceInstance> services = serviceRegistry.services();
+    ClusterConfig cfg = getClusterConfig(config, services).build();
+    this.cluster = Cluster.joinAwait(cfg); 
     loadClusterServices();
     listenCluster();
   }
@@ -140,6 +64,82 @@ public class ServiceDiscovery {
     } else {
       result.completeExceptionally(new IllegalStateException("Cluster transport alredy stopped"));
       return result;
+    }
+  }
+  
+  private ClusterConfig.Builder getClusterConfig(
+      ClusterConfig.Builder clusterConfig, 
+      Collection<ServiceInstance> services) {
+    
+    if (services != null && !services.isEmpty()) {
+      clusterConfig.addMetadata(metadata(services));
+    }
+    return clusterConfig;
+  }
+  private static Map<String, String> metadata(Collection<ServiceInstance> services) {
+    Map<String, String> servicesTags = new HashMap<>();
+
+    services.stream().forEach(service -> {
+      // constract info metadata object as json key
+      ServiceInfo info = new ServiceInfo(
+          service.serviceName(),
+          service.methods(),
+          service.tags()
+          );
+      
+      // add service metadata to map.
+      servicesTags.put(info.toMetadata(), "service");
+    });
+
+    return servicesTags;
+  }
+  
+  private void listenCluster() {
+    cluster.listenMembership().subscribe(event -> {
+      if (event.isAdded()) {
+        loadMemberServices(DiscoveryType.ADDED, event.member());
+      } else if (event.isRemoved()) {
+        loadMemberServices(DiscoveryType.REMOVED, event.member());
+      }
+    });
+    ThreadFactory.singleScheduledExecutorService("service-registry-discovery")
+        .scheduleAtFixedRate(this::loadClusterServices, 10, 10, TimeUnit.SECONDS);
+  }
+
+  private void loadClusterServices() {
+    cluster.otherMembers().forEach(member -> {
+      loadMemberServices(DiscoveryType.DISCOVERED, member);
+    });
+  }
+  
+  private void loadMemberServices(DiscoveryType type, Member member) {
+    member.metadata().entrySet().stream()
+        .filter(entry -> "service".equals(entry.getValue())) // filter service tags
+        .forEach(entry -> {
+          Address serviceAddress = getServiceAddress(member);
+          ServiceInfo info = ServiceInfo.from(entry.getKey());
+          
+          LOGGER.debug("Member: {} is {} : {}", member, type, serviceRef);
+          if (type.equals(DiscoveryType.ADDED) || type.equals(DiscoveryType.DISCOVERED)) {
+            if (!containsService(info)) {
+              serviceRegistry.registerService(new ServiceInstance( , info.getTags()));
+              LOGGER.info("Service Reference was ADDED since new Member has joined the cluster {} : {}", member,
+                  serviceRef);
+            }
+          } else if (type.equals(DiscoveryType.REMOVED)) {
+            serviceRegistry.remove(serviceRef);
+            LOGGER.info("Service Reference was REMOVED since Member have left the cluster {} : {}", member,
+                serviceRef);
+          }
+        });
+  }
+
+  private Address getServiceAddress(Member member) {
+    String serviceAddressAsString = member.metadata().get("service-address");
+    if (serviceAddressAsString != null) {
+      return Address.from(serviceAddressAsString);
+    } else {
+      return member.address();
     }
   }
 }
