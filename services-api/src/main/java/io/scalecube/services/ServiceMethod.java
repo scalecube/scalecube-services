b546b3ce182024e7be4d4c08687d744a92584f73
artem-v <artem.vysochyn@jivygroup.com>
2018-04-18 10:40:27 +0300

    Sketching model for service registry

---
+++ services-api/src/main/java/io/scalecube/services/ServiceMethod.java
@@ -0,0 +1,24 @@
+package io.scalecube.services;
+
+import java.util.Map;
+
+public class ServiceMethod {
+
+  private final String name;
+  private final String contentType;
+  private final Map<String, String> tags;
+
+  public ServiceMethod(String name, String contentType, Map<String, String> tags) {
+    this.name = name;
+    this.contentType = contentType;
+    this.tags = tags;
+  }
+
+  public String name() {
+    return name;
+  }
+
+  public String contentType() {
+    return contentType;
+  }
+}
---
+++ services-api/src/main/java/io/scalecube/services/ServiceRegistration.java
@@ -0,0 +1,35 @@
+package io.scalecube.services;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class ServiceRegistration {
+
+  private final String serviceName;
+  private final String contentType;
+  private final Map<String, String> tags;
+  private final Collection<ServiceMethod> methods;
+
+  public ServiceRegistration(String serviceName, String contentType, Map<String, String> tags, Collection<ServiceMethod> methods) {
+    this.serviceName = serviceName;
+    this.contentType = contentType;
+    this.tags = tags;
+    this.methods = methods;
+  }
+
+  public String serviceName() {
+    return serviceName;
+  }
+
+  public String contentType() {
+    return contentType;
+  }
+
+  public Map<String, String> tags() {
+    return tags;
+  }
+
+  public Collection<ServiceMethod> methods() {
+    return methods;
+  }
+}
