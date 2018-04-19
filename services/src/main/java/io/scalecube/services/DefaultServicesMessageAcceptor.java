7bac8e777f5ac3fe9701de3bc06bfce02d1164a4
ronen <ronen.hamias@gmail.com>
2018-04-19 08:31:14 +0300

    route request to local service method invoke

--- services/src/main/java/io/scalecube/services/DefaultServicesMessageAcceptor.java
+++ services/src/main/java/io/scalecube/services/DefaultServicesMessageAcceptor.java
@@ -1,40 +1,44 @@
 package io.scalecube.services;
 
-import io.scalecube.services.ServiceCall.Call;
 import io.scalecube.services.api.ServiceMessage;
 import io.scalecube.services.transport.server.api.ServerMessageAcceptor;
 
 import org.reactivestreams.Publisher;
 
+import java.util.Map;
+
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-public class DefaultServicesMessageAcceptor implements ServerMessageAcceptor{
+public class DefaultServicesMessageAcceptor implements ServerMessageAcceptor {
 
-  private Call services;
+  private Map<String, LocalServiceMethodInvoke> endpoints;
 
-  public DefaultServicesMessageAcceptor(Call services) {
-    this.services = services;
+  public DefaultServicesMessageAcceptor(Map<String, LocalServiceMethodInvoke> services) {
+    this.endpoints = services;
   }
 
   @Override
   public Flux<ServiceMessage> requestChannel(Publisher<ServiceMessage> request) {
-    return services.channel(request);
+    Mono<ServiceMessage> flux = Mono.from(request);
+    Mono<LocalServiceMethodInvoke> handler = flux.map(message -> endpoints.get(message.qualifier()));
+    return Flux.from(handler.map(mapper -> mapper.requestChannel(Flux.from(request))));
   }
+
 
   @Override
   public Flux<ServiceMessage> requestStream(ServiceMessage request) {
-    return services.listen(request);
+    return endpoints.get(request.qualifier()).requestStream(request);
   }
 
   @Override
   public Mono<ServiceMessage> requestResponse(ServiceMessage request) {
-    return services.requestResponse(request);
+    return endpoints.get(request.qualifier()).requestResponse(request);
   }
 
   @Override
   public Mono<Void> fireAndForget(ServiceMessage request) {
-    return services.fireAndForget(request);
+    return endpoints.get(request.qualifier()).fireAndForget(request);
   }
 
 }
