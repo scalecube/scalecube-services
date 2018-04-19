40d1c3983942e48828964b58d1834943f68b7639
artem-v <artem.vysochyn@jivygroup.com>
2018-04-19 15:12:02 +0300

    Added RequestType, ResponseType

---
+++ services-api/src/main/java/io/scalecube/services/annotations/ResponseType.java
@@ -0,0 +1,14 @@
+package io.scalecube.services.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+public @interface ResponseType {
+
+  Class<?> value();
+
+}
