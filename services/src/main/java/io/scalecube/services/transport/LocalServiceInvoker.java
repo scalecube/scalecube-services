ab01ec043c0fd12b2fd23cb3424318cce38d2d2b
ronen <ronen.hamias@gmail.com>
2018-04-20 19:31:54 +0300

    register By qualifier

--- services/src/main/java/io/scalecube/services/transport/LocalServiceInvoker.java
+++ services/src/main/java/io/scalecube/services/transport/LocalServiceInvoker.java
@@ -10,6 +10,7 @@ import org.reactivestreams.Publisher;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
@@ -21,19 +22,22 @@ public class LocalServiceInvoker implements ServerMessageAcceptor {
   @SuppressWarnings("rawtypes")
   private ConcurrentMap<String, ServiceMethodInvoker> handlers = new ConcurrentHashMap<>();
   private Object[] services;
-  private ServiceMessageCodec codec;
+  private List<? extends ServiceMessageCodec> codec;
 
-  public static LocalServiceInvoker create(ServiceMessageCodec codec, Object... serviceObjects) {
-    return new LocalServiceInvoker(codec, serviceObjects);
+  public static LocalServiceInvoker create(List<? extends ServiceMessageCodec> codecs, Object... serviceObjects) {
+    return new LocalServiceInvoker(codecs, serviceObjects);
   }
 
   public Collection<Object> services() {
     return Collections.unmodifiableCollection(Arrays.asList(this.services));
   }
-  public ServiceMessageCodec codec() {
+
+  public List<? extends ServiceMessageCodec> codec() {
     return this.codec;
   }
+
   @Override
+  @SuppressWarnings("unchecked")
   public Publisher<ServiceMessage> requestChannel(final Publisher<ServiceMessage> request) {
     // FIXME: need to seek handler and invoke it.
     ServiceMethodInvoker<Publisher<ServiceMessage>> handler = null;
@@ -42,17 +46,20 @@ public class LocalServiceInvoker implements ServerMessageAcceptor {
 
 
   @Override
+  @SuppressWarnings("unchecked")
   public Publisher<ServiceMessage> requestStream(ServiceMessage request) {
     return handlers.get(request.qualifier()).invoke(request);
   }
 
   @Override
+  @SuppressWarnings("unchecked")
   public Publisher<ServiceMessage> requestResponse(ServiceMessage request) {
     return handlers.get(request.qualifier())
         .invoke(request);
   }
 
   @Override
+  @SuppressWarnings("unchecked")
   public Publisher<Void> fireAndForget(ServiceMessage request) {
     return Mono.from(handlers.get(request.qualifier())
         .invoke(request))
@@ -60,23 +67,27 @@ public class LocalServiceInvoker implements ServerMessageAcceptor {
   }
 
 
-  private LocalServiceInvoker(ServiceMessageCodec codec, Object... serviceObjects) {
-    this.codec = codec;
+  private LocalServiceInvoker(List<? extends ServiceMessageCodec> codecs, Object... serviceObjects) {
+    this.codec = codecs;
     this.services = serviceObjects;
 
     Arrays.asList(serviceObjects).forEach(service -> {
-      Reflect.serviceInterfaces(service).stream().map(serviceIinterface -> {
+      Reflect.serviceInterfaces(service).stream().map(ServiceInterface -> {
 
-        return Reflect.serviceMethods(serviceIinterface)
+        return Reflect.serviceMethods(ServiceInterface)
             .entrySet().stream().map(entry -> {
+              codecs.forEach(codec -> {
+                {
+                  if (Reflect.exchangeTypeOf(entry.getValue()).equals(ExchangeType.REQUEST_RESPONSE)) {
+                    this.register(Reflect.qualifier(ServiceInterface, entry.getValue()),
+                        new RequestResponseInvoker(service, entry.getValue(), codec));
 
-              if (Reflect.exchangeTypeOf(entry.getValue()).equals(ExchangeType.REQUEST_RESPONSE)) {
-                this.register(Reflect.methodName(entry.getValue()),
-                    new RequestResponseInvoker(service, entry.getValue(), codec));
-              } else if (Reflect.exchangeTypeOf(entry.getValue()).equals(ExchangeType.REQUEST_CHANNEL)) {
-                this.register(Reflect.methodName(entry.getValue()),
-                    new RequestChannelInvoker(service, entry.getValue(), codec));
-              }
+                  } else if (Reflect.exchangeTypeOf(entry.getValue()).equals(ExchangeType.REQUEST_CHANNEL)) {
+                    this.register(Reflect.qualifier(ServiceInterface, entry.getValue()),
+                        new RequestChannelInvoker(service, entry.getValue(), codec));
+                  }
+                }
+              });
               return null;
             });
       });
