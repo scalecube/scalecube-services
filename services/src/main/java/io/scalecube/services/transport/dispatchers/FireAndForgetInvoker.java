f27f6234a77237266ee2c8449a970025d06fb259
artem-v <artem.vysochyn@jivygroup.com>
2018-04-24 17:11:02 +0300

    Fixing ServiceCallTest

--- services/src/main/java/io/scalecube/services/transport/dispatchers/FireAndForgetInvoker.java
+++ services/src/main/java/io/scalecube/services/transport/dispatchers/FireAndForgetInvoker.java
@@ -11,18 +11,21 @@ import java.lang.reflect.Method;
 
 import reactor.core.publisher.Mono;
 
-public class FireAndForgetInvoker extends AbstractServiceMethodDispatcher<ServiceMessage, Publisher<Void>> {
-  
-  public FireAndForgetInvoker(Object serviceObject, Method method, ServiceMessageDataCodec payloadCodec) {
-    super(serviceObject, method, payloadCodec);
+public class FireAndForgetInvoker
+    extends AbstractServiceMethodDispatcher<ServiceMessage, Publisher<Void>> {
+
+  public FireAndForgetInvoker(String qualifier,
+      Object serviceObject,
+      Method method,
+      ServiceMessageDataCodec payloadCodec) {
+    super(qualifier, serviceObject, method, payloadCodec);
   }
 
   @Override
   public Publisher<ServiceMessage> invoke(ServiceMessage request) {
     ServiceMessage message = payloadCodec.decodeData(request, super.requestType);
     try {
-      return Mono.from(Reflect.invokeMessage(serviceObject, method, message))
-          .map(obj->toReturnMessage(obj));
+      return Mono.from(Reflect.invokeMessage(serviceObject, method, message)).map(this::toReturnMessage);
     } catch (Exception e) {
       return Mono.error(e);
     }
