5f3595f87f1d477e2753e9c9116f3fd91add8c2c
Sergii Ripa <sergey.ripa@jivygroup.com>
2018-04-25 14:01:06 +0300

    Implement Error handling (POC, don't look)

---
+++ services/src/main/java/io/scalecube/services/transport/ServiceException.java
@@ -0,0 +1,7 @@
+package io.scalecube.services.transport;
+
+/**
+ * Business logic error occurred on remote side
+ */
+public class ServiceException extends RuntimeException {
+}
--- services/src/main/java/io/scalecube/services/transport/dispatchers/RequestResponseDispatcher.java
+++ services/src/main/java/io/scalecube/services/transport/dispatchers/RequestResponseDispatcher.java
@@ -1,5 +1,7 @@
 package io.scalecube.services.transport.dispatchers;
 
+import static reactor.core.publisher.Mono.just;
+
 import io.scalecube.services.Reflect;
 import io.scalecube.services.api.ServiceMessage;
 import io.scalecube.services.codecs.api.ServiceMessageDataCodec;
@@ -8,6 +10,7 @@ import io.scalecube.services.transport.AbstractServiceMethodDispatcher;
 import org.reactivestreams.Publisher;
 
 import java.lang.reflect.Method;
+import java.util.function.Function;
 
 import reactor.core.publisher.Mono;
 
@@ -25,10 +28,19 @@ public class RequestResponseDispatcher
   public Publisher<ServiceMessage> invoke(ServiceMessage request) {
     ServiceMessage message = payloadCodec.decodeData(request, super.requestType);
     try {
-      return Mono.from(Reflect.invokeMessage(serviceObject, method, message)).map(this::toReturnMessage);
-    } catch (Exception e) {
+      Mono<ServiceMessage> map =
+          Mono.from(Reflect.invokeMessage(serviceObject, method, message)).map(this::toReturnMessage);
+      return map.onErrorResume(toServiceMsg());
+    } catch (Throwable e) {
       return Mono.error(e);
     }
+  }
+
+  private Function<Throwable, Mono<? extends ServiceMessage>> toServiceMsg() {
+    return t -> just(ServiceMessage.builder()
+        .qualifier("ERROR_FATAL")
+        .data(new ErrData().setErrCode(500).setErrMessage("Error Fatal occurred man..."))
+        .build());
   }
 }
 
