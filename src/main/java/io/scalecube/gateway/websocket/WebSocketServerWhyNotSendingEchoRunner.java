bc4c05e416603f93feff3df990b25c755706f94d
artem-v <artem.vysochyn@jivygroup.com>
2018-05-11 23:23:56 +0300

    Added WebSocketServerWhyDoesThisSendingEchoRunner.java; confuused why does this impl sends echoes back

--- src/main/java/io/scalecube/gateway/websocket/WebSocketServerWhyNotSendingEchoRunner.java
+++ src/main/java/io/scalecube/gateway/websocket/WebSocketServerWhyNotSendingEchoRunner.java
@@ -1,35 +1,15 @@
 package io.scalecube.gateway.websocket;
 
-import io.netty.handler.codec.http.websocketx.WebSocketFrame;
-
 import java.net.InetSocketAddress;
 
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.ipc.netty.NettyPipeline;
-import reactor.ipc.netty.http.websocket.WebsocketInbound;
-import reactor.ipc.netty.http.websocket.WebsocketOutbound;
 
 public class WebSocketServerWhyNotSendingEchoRunner {
   public static void main(String[] args) throws InterruptedException {
     WebSocketServer server = new WebSocketServer(new WebSocketAcceptor() {
       @Override
       public Mono<Void> onConnect(WebSocketSession session) {
-        WebsocketInbound inbound = session.getInbound();
-        WebsocketOutbound outbound = session.getOutbound();
-
-        Flux<WebSocketFrame> receiveFlux = inbound
-            .aggregateFrames()
-            .receiveFrames()
-            .doOnNext(WebSocketFrame::retain);
-
-        // Flux<ServiceMessage> receiveFlux = session.receive();
-
-        return outbound
-            .options(NettyPipeline.SendOptions::flushOnEach)
-            /* .sendObject(receiveFlux.map(message -> (ByteBuf) message.data()).map(BinaryWebSocketFrame::new).log()) */
-            .sendObject(receiveFlux.log())
-            .then();
+        return session.send(session.receive());
       }
 
       @Override
