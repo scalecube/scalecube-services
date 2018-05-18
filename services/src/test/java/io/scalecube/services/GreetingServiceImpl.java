f9d5556e91adeb177648cebcee756fc9bedc787a
Ronen Nachmias <ronen.hamias@gmail.com>
2018-05-18 19:13:10 +0300

    dont sysout on travis ci

--- services/src/test/java/io/scalecube/services/GreetingServiceImpl.java
+++ services/src/test/java/io/scalecube/services/GreetingServiceImpl.java
@@ -13,6 +13,8 @@ public final class GreetingServiceImpl implements GreetingService {
 
   private int instanceId;
 
+  private boolean ci = System.getenv("TRAVIS") != null;
+
   public GreetingServiceImpl() {}
 
   public GreetingServiceImpl(int id) {
@@ -41,58 +43,65 @@ public final class GreetingServiceImpl implements GreetingService {
 
   @Override
   public Mono<GreetingResponse> greetingRequestTimeout(GreetingRequest request) {
-    System.out.println("[greetingRequestTimeout] Hello... i am a service an just recived a message:" + request);
+    print("[greetingRequestTimeout] Hello... i am a service an just recived a message:" + request);
     return Mono.delay(request.getDuration()).flatMap(
         i -> Mono.just(new GreetingResponse(" hello to: " + request.getName(), String.valueOf(this.hashCode()))));
   }
 
   @Override
   public Mono<String> greetingNoParams() {
-    System.out.println("[greetingNoParams] Hello... i am a service an just recived a call bu i dont know from who.");
+    print("[greetingNoParams] Hello... i am a service an just recived a call bu i dont know from who.");
     return Mono.just("hello unknown");
   }
 
   @Override
   public Mono<GreetingResponse> greetingRequest(GreetingRequest request) {
-    System.out.println(instanceId + ":[greetingRequest] Hello... i am a service an just recived a message:" + request);
+    print(instanceId + ":[greetingRequest] Hello... i am a service an just recived a message:" + request);
     return Mono.just(new GreetingResponse(" hello to: " + request.getName(), "" + instanceId));
   }
 
   @Override
   public Mono<ServiceMessage> greetingMessage(ServiceMessage request) {
-    System.out.println("[greetingMessage] Hello... i am a service an just recived a message:" + request);
+    print("[greetingMessage] Hello... i am a service an just recived a message:" + request);
     GreetingResponse resp = new GreetingResponse(" hello to: " + request.data(), "1");
     return Mono.just(ServiceMessage.builder().data(resp).build());
   }
 
   @Override
   public Mono<Void> greetingVoid(GreetingRequest request) {
-    System.out.println("[greetingVoid] Hello... i am a service an just recived a message:" + request);
-    System.out.println(" hello to: " + request.getName());
+    print("[greetingVoid] Hello... i am a service an just recived a message:" + request);
+    print(" hello to: " + request.getName());
     return Mono.empty();
   }
 
   @Override
   public Mono<Void> failingVoid(GreetingRequest request) {
-    System.out.println("[failingVoid] Hello... i am a service an just recived a message:" + request);
+    print("[failingVoid] Hello... i am a service an just recived a message:" + request);
     return Mono.error(new IllegalArgumentException(request.toString()));
   }
 
   @Override
   public Mono<Void> throwingVoid(GreetingRequest request) {
-    System.out.println("[failingVoid] Hello... i am a service an just recived a message:" + request);
+    print("[failingVoid] Hello... i am a service an just recived a message:" + request);
     throw new IllegalArgumentException(request.toString());
   }
 
   @Override
   public Mono<GreetingResponse> failingRequest(GreetingRequest request) {
-    System.out.println("[failingRequest] Hello... i am a service an just recived a message:" + request);
+    print("[failingRequest] Hello... i am a service an just recived a message:" + request);
     return Mono.error(new IllegalArgumentException(request.toString()));
   }
 
   @Override
   public Mono<GreetingResponse> exceptionRequest(GreetingRequest request) {
-    System.out.println("[exceptionRequest] Hello... i am a service an just recived a message:" + request);
+    print("[exceptionRequest] Hello... i am a service an just recived a message:" + request);
     throw new IllegalArgumentException(request.toString());
+  }
+
+  private void print(String message) {
+    if (!ci) {
+      System.out.println(message);
+    }
+
   }
 }
