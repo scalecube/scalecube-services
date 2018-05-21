31e7abbcb4b00a8b63f34d46948eda96ef4223ab
artem-v <artem.vysochyn@jivygroup.com>
2018-05-21 17:35:56 +0300

    Fixed Mono.from(Flux) bug

--- services-api/src/main/java/io/scalecube/services/api/ServiceMessageHandler.java
+++ services-api/src/main/java/io/scalecube/services/api/ServiceMessageHandler.java
@@ -1,8 +1,10 @@
 package io.scalecube.services.api;
 
+import org.reactivestreams.Publisher;
+
 import reactor.core.publisher.Flux;
 
 public interface ServiceMessageHandler {
 
-  Flux<ServiceMessage> invoke(Flux<ServiceMessage> publisher);
+  Flux<ServiceMessage> invoke(Publisher<ServiceMessage> publisher);
 }
