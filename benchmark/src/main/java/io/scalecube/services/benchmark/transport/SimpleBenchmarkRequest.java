4a1eb3547bf628e06616fae91a6ba86f01a4a40c
segabriel <sergiy.gabriel@jivygroup.com>
2018-05-22 17:54:44 +0300

    Added simple benchmark on fireAndForget

---
+++ benchmark/src/main/java/io/scalecube/services/benchmark/transport/SimpleBenchmarkRequest.java
@@ -0,0 +1,11 @@
+package io.scalecube.services.benchmark.transport;
+
+public class SimpleBenchmarkRequest {
+  public String request;
+
+  public SimpleBenchmarkRequest() {}
+
+  public SimpleBenchmarkRequest(String request) {
+    this.request = request;
+  }
+}
