406be44b16cf370669b26be49d924a060b563a67
segabriel <sergiy.gabriel@jivygroup.com>
2018-05-24 18:29:08 +0300

    Added requestMany test

--- benchmarks/src/main/java/io/scalecube/services/benchmarks/metrics/ServicesBenchmarksRunner.java
+++ benchmarks/src/main/java/io/scalecube/services/benchmarks/metrics/ServicesBenchmarksRunner.java
@@ -11,17 +11,21 @@ public class ServicesBenchmarksRunner {
 
   private static final int nThreads = Runtime.getRuntime().availableProcessors();
 
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) {
     MetricRegistry registry = new MetricRegistry();
 
     Scheduler scheduler = Schedulers.fromExecutor(Executors.newFixedThreadPool(nThreads));
 
-    int n = 1_000_000;
+    int n = 1_00_000;
+    int responseCount = 10;
     ServicesBenchmarks servicesBenchmarks = new ServicesBenchmarks(scheduler, registry);
 
     servicesBenchmarks.startAndWarmup(n);
 
+    System.out.println("###### requestResponse, n=" + n);
     servicesBenchmarks.run(n, (size) -> servicesBenchmarks.requestResponse(size).blockLast());
+    System.out.println("###### requestMany, n=" + n + ", responseCount=" + responseCount);
+    servicesBenchmarks.run(n, (size) -> servicesBenchmarks.requestMany(size, responseCount).blockLast());
 
     servicesBenchmarks.tearDown();
   }
