0b3c794b11b37aff81c6cb2f6654071a08a6e72e
segabriel <sergiy.gabriel@jivygroup.com>
2018-05-24 14:41:36 +0300

    Refactoring and separate on block and subscribe cases

--- benchmarks/src/main/java/io/scalecube/services/benchmarks/BenchmarkService.java
+++ benchmarks/src/main/java/io/scalecube/services/benchmarks/BenchmarkService.java
@@ -2,38 +2,20 @@ package io.scalecube.services.benchmarks;
 
 import io.scalecube.services.annotations.Service;
 import io.scalecube.services.annotations.ServiceMethod;
-import io.scalecube.services.api.ServiceMessage;
 
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
 public interface BenchmarkService {
 
-  ServiceMessage REQUEST_ONE = ServiceMessage.builder()
-      .qualifier(BenchmarkService.class.getName(), "requestOne")
-      .data("test")
-      .build();
-
-  ServiceMessage ONE_WAY = ServiceMessage.builder()
-      .qualifier(BenchmarkService.class.getName(), "oneWay")
-      .data("test")
-      .build();
-
-  ServiceMessage REQUEST_BIDIRECTIONAL_ECHO = ServiceMessage.builder()
-      .qualifier(BenchmarkService.class.getName(), "requestBidirectionalEcho")
-      .data("test")
-      .build();
+  BenchmarkMessage MESSAGE = new BenchmarkMessage("benchmarkMessage");
 
   @ServiceMethod
-  Mono<Void> oneWay(String request);
+  Mono<Void> fireAndForget0();
 
   @ServiceMethod
-  Mono<String> requestOne(String request);
+  Mono<Void> fireAndForget(BenchmarkMessage request);
 
   @ServiceMethod
-  Flux<String> requestMany(int count);
-
-  @ServiceMethod
-  Flux<String> requestBidirectionalEcho(Flux<String> counts);
+  Mono<BenchmarkMessage> requestOne(BenchmarkMessage request);
 }
