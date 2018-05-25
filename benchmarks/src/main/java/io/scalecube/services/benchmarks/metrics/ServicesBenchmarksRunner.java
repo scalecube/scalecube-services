a5803192977ea694fef35196b03f2c2712b064fb
segabriel <sergiy.gabriel@jivygroup.com>
2018-05-25 12:38:38 +0300

    Refactor and using duration instead N count

--- benchmarks/src/main/java/io/scalecube/services/benchmarks/metrics/ServicesBenchmarksRunner.java
+++ benchmarks/src/main/java/io/scalecube/services/benchmarks/metrics/ServicesBenchmarksRunner.java
@@ -1,41 +1,26 @@
 package io.scalecube.services.benchmarks.metrics;
 
-import com.codahale.metrics.MetricRegistry;
-
-import java.util.concurrent.Executors;
-import java.util.stream.Stream;
-
-import reactor.core.scheduler.Scheduler;
-import reactor.core.scheduler.Schedulers;
+import java.time.Duration;
 
 public class ServicesBenchmarksRunner {
 
   private static final int nThreads = Runtime.getRuntime().availableProcessors();
 
   public static void main(String[] args) {
-    MetricRegistry registry = new MetricRegistry();
-    Scheduler scheduler = Schedulers.fromExecutor(Executors.newFixedThreadPool(nThreads));
-
-    ServicesBenchmarks servicesBenchmarks = new ServicesBenchmarks(scheduler, registry).start();
-
-    int n = 1_00_000;
     int responseCount = 10;
-    int warmup = 1;
-    int iteration = 3;
+    Duration reporterPeriodDuration = Duration.ofSeconds(10);
+    ServicesBenchmarks servicesBenchmarks = new ServicesBenchmarks(nThreads, reporterPeriodDuration).start();
 
-    Stream.<Runnable>of(
-        () -> servicesBenchmarks.run(n, (size) -> servicesBenchmarks.oneWay(size).blockLast()),
-        () -> servicesBenchmarks.run(n, (size) -> servicesBenchmarks.oneWayLatch(size, nThreads)),
-        () -> servicesBenchmarks.run(n, (size) -> servicesBenchmarks.requestOne(size).blockLast()),
-        () -> servicesBenchmarks.run(n, (size) -> servicesBenchmarks.requestOneLatch(size, nThreads)),
-        () -> servicesBenchmarks.run(n, (size) -> servicesBenchmarks.requestMany(size, responseCount).blockLast()),
-        () -> servicesBenchmarks.run(n, (size) -> servicesBenchmarks.requestManyLatch(size, responseCount, nThreads)))
-        .forEach(task -> {
-          for (int i = 0; i < warmup + iteration; i++) {
-            task.run();
-          }
-        });
+
+    servicesBenchmarks.oneWay().take(Duration.ofSeconds(20))
+        .blockLast();
+    servicesBenchmarks.requestOne().take(Duration.ofSeconds(20))
+        .blockLast();
+    servicesBenchmarks.requestMany(responseCount).take(Duration.ofSeconds(20))
+        .blockLast();
 
     servicesBenchmarks.tearDown();
   }
 }
+
+
