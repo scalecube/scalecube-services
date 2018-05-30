0cf860451c62a17450cd93d2d14cb5cab42b0f9f
segabriel <sergiy.gabriel@jivygroup.com>
2018-05-30 10:43:56 +0300

    Added other test scenarios

--- benchmarks/src/main/java/io/scalecube/services/benchmarks/OneWayBenchmarksRunner.java
+++ benchmarks/src/main/java/io/scalecube/services/benchmarks/OneWayBenchmarksRunner.java
@@ -8,7 +8,7 @@ import java.util.stream.LongStream;
 
 import reactor.core.publisher.Flux;
 
-public class RequestOneBenchmarksRunner {
+public class OneWayBenchmarksRunner {
 
   public static void main(String[] args) {
     ServicesBenchmarksSettings settings = ServicesBenchmarksSettings.from(args).build();
@@ -16,13 +16,13 @@ public class RequestOneBenchmarksRunner {
     state.setup();
 
     BenchmarkService benchmarkService = state.service();
-    Timer timer = state.registry().timer("requestOne" + "-timer");
+    Timer timer = state.registry().timer("oneWay" + "-timer");
 
     Flux.merge(Flux.fromStream(LongStream.range(0, Long.MAX_VALUE).boxed())
         .subscribeOn(state.scheduler())
         .map(i -> {
           Timer.Context timeContext = timer.time();
-          return benchmarkService.requestOne(MESSAGE).doOnSuccess(next -> timeContext.stop());
+          return benchmarkService.oneWay(MESSAGE).doOnSuccess(next -> timeContext.stop());
         }))
         .take(settings.executionTaskTime())
         .blockLast();
