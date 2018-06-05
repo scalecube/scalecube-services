81491ebb4aba122942ae9e8f9b376754a7b12d53
segabriel <sergiy.gabriel@jivygroup.com>
2018-06-05 22:13:04 +0300

    Improved tests

--- src/test/java/io/scalecube/gateway/websocket/WebSocketServerTest.java
+++ src/test/java/io/scalecube/gateway/websocket/WebSocketServerTest.java
@@ -2,15 +2,20 @@ package io.scalecube.gateway.websocket;
 
 import static org.junit.Assert.assertEquals;
 
+import io.scalecube.services.api.ErrorData;
+import io.scalecube.services.api.Qualifier;
 import io.scalecube.services.api.ServiceMessage;
 
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import org.junit.Rule;
 import org.junit.Test;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class WebSocketServerTest {
@@ -43,24 +48,35 @@ public class WebSocketServerTest {
     resource.startServer().startServices();
 
     int n = 10;
-    List<String> echoMessages =
-        resource.sendThenReceive(Mono.just(greetingMany), TIMEOUT)
-            .take(n)
-            .map(message -> (String) message.data())
-            .collectList().block(TIMEOUT);
+    List<String> expected = IntStream.range(0, n)
+        .mapToObj(i -> "Greeting (" + i + ") to: hello")
+        .collect(Collectors.toList());
 
-    assertEquals(n, echoMessages.size());
-    IntStream.range(0, n).forEach(i -> assertEquals("Greeting (" + i + ") to: hello", echoMessages.get(i)));
+    StepVerifier.create(resource.sendThenReceive(Mono.just(greetingMany), TIMEOUT)
+            .take(n)
+        .map(message -> (String) message.data()))
+        .expectNextSequence(expected)
+        .expectComplete()
+        .verify(TIMEOUT);
   }
 
   @Test
   public void testServicesNotStartedYet() {
     resource.startServer();
 
-    ServiceMessage error =
-        resource.sendThenReceive(Mono.just(greetingOne), TIMEOUT).blockFirst(TIMEOUT);
+    int expectedErrorCode = 503;
+    String expectedQualifier = Qualifier.asError(expectedErrorCode);
+    String expectedErrorMessage = "No reachable member with such service: " + greetingOne.qualifier();
 
-    assertEquals("Echo:hello", error);
+    StepVerifier.create(resource.sendThenReceive(Mono.just(greetingOne), TIMEOUT))
+        .expectNextMatches(msg -> {
+          ErrorData data = msg.data();
+          return Objects.equals(expectedQualifier, msg.qualifier()) &&
+              Objects.equals(expectedErrorCode, data.getErrorCode()) &&
+              Objects.equals(expectedErrorMessage, data.getErrorMessage());
+        })
+        .expectComplete()
+        .verify(TIMEOUT);
   }
 
   @Test
