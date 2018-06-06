eb12f78a88712e23d0d107e971b96ebe63520c46
segabriel <sergiy.gabriel@jivygroup.com>
2018-06-06 12:32:00 +0300

    Added test with wrappers, need to fix mapping!

--- src/test/java/io/scalecube/gateway/websocket/WebSocketServerTest.java
+++ src/test/java/io/scalecube/gateway/websocket/WebSocketServerTest.java
@@ -4,6 +4,7 @@ import io.scalecube.services.api.Qualifier;
 import io.scalecube.services.api.ServiceMessage;
 import org.junit.Rule;
 import org.junit.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -14,6 +15,8 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static io.scalecube.gateway.websocket.GreetingService.GREETING_DTO_MANY;
+import static io.scalecube.gateway.websocket.GreetingService.GREETING_DTO_ONE;
 import static io.scalecube.gateway.websocket.GreetingService.GREETING_FAILING_MANY;
 import static io.scalecube.gateway.websocket.GreetingService.GREETING_FAILING_ONE;
 import static io.scalecube.gateway.websocket.GreetingService.GREETING_MANY;
@@ -120,6 +123,65 @@ public class WebSocketServerTest {
         .verify(TIMEOUT);
   }
 
+  @Test // todo fix it
+  public void testGreetingDtoOne() {
+    resource.startServer().startServices();
+
+    ServiceMessage expected = serviceMessage(GREETING_DTO_ONE.qualifier(), new GreetingResponse("Echo:hello"));
+
+    resource.sendThenReceive(Mono.just(GREETING_DTO_ONE), TIMEOUT)
+        .subscribe(System.err::println, Throwable::printStackTrace, () -> System.err.println("FIN"));
+
+    // todo this shows like it must be, remove after a fix
+    StepVerifier.create(Mono.defer(() -> {
+      Map<String, Object> content = new HashMap<>();
+      content.put("text", "Echo:hello");
+      return Mono.just(ServiceMessage.builder()
+          .data(content)
+          .build());
+    }))
+        .expectNextMatches(msg -> expected.data().equals(msg.data()))
+        .expectComplete()
+        .verify(TIMEOUT);
+
+    StepVerifier.create(resource.sendThenReceive(Mono.just(GREETING_DTO_ONE), TIMEOUT))
+        .expectNextMatches(msg -> expected.data().equals(msg.data()))
+        .expectComplete()
+        .verify(TIMEOUT);
+  }
+
+  @Test // todo fix it
+  public void testGreetingDtoMany() {
+    resource.startServer().startServices();
+
+    int n = 10;
+    List<?> expected = IntStream.range(0, n)
+        .mapToObj(i -> "Greeting (" + i + ") to: hello")
+        .map(GreetingResponse::new)
+        .map(resp -> serviceMessage(GREETING_DTO_MANY.qualifier(), resp))
+        .map(ServiceMessage::data)
+        .collect(Collectors.toList());
+
+    // todo this shows like it must be, remove after a fix
+    StepVerifier.create(Flux.fromIterable(expected)
+        .map(content -> ServiceMessage.builder()
+            .qualifier(GREETING_DTO_MANY.qualifier())
+            .data(content)
+            .build())
+        .take(n)
+        .map(ServiceMessage::data))
+        .expectNextSequence(expected)
+        .expectComplete()
+        .verify(TIMEOUT);
+
+    StepVerifier.create(resource.sendThenReceive(Mono.just(GREETING_DTO_MANY), TIMEOUT)
+        .take(n)
+        .map(ServiceMessage::data))
+        .expectNextSequence(expected)
+        .expectComplete()
+        .verify(TIMEOUT);
+  }
+
   private ServiceMessage unreachableServiceMessage(String qualifier) {
     int errorCode = 503;
     String errorMessage = "No reachable member with such service: " + qualifier;
@@ -133,6 +195,15 @@ public class WebSocketServerTest {
     return ServiceMessage.builder()
         .qualifier(Qualifier.asError(errorCode))
         .data(errorData)
+        .build();
+  }
+
+  private ServiceMessage serviceMessage(String qualifier, GreetingResponse response) {
+    Map<String, Object> content = new HashMap<>();
+    content.put("text", response.getText());
+    return ServiceMessage.builder()
+        .qualifier(qualifier)
+        .data(content)
         .build();
   }
 }
