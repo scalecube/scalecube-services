9adc2e2470efe3052cc55fa9ee4ac6386ec30c7a
segabriel <sergiy.gabriel@jivygroup.com>
2018-06-06 16:25:07 +0300

    Fixed tests

--- src/main/java/io/scalecube/gateway/websocket/GreetingResponse.java
+++ src/main/java/io/scalecube/gateway/websocket/GreetingResponse.java
@@ -16,6 +16,21 @@ public class GreetingResponse {
   }
 
   @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GreetingResponse that = (GreetingResponse) o;
+
+    return text != null ? text.equals(that.text) : that.text == null;
+  }
+
+  @Override
+  public int hashCode() {
+    return text != null ? text.hashCode() : 0;
+  }
+
+  @Override
   public String toString() {
     return "GreetingResponse{" +
         "text='" + text + '\'' +
