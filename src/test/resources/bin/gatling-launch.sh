770668e6a57ddcd605c033eb12ed95c42af5a489
Sergii Ripa <sergey.ripa@jivygroup.com>
2018-06-07 15:18:31 +0300

    fix scenario

--- src/test/resources/bin/gatling-launch.sh
+++ src/test/resources/bin/gatling-launch.sh
@@ -1,2 +1,2 @@
 USER_ARGS="$@"
-JAVA_OPTS="$USER_ARGS ${JAVA_OPTS}" /opt/gatling/bin/gatling.sh
+JAVA_OPTS="$USER_ARGS ${JAVA_OPTS}" bin/gatling.sh
--- src/test/scala/RequestResponseTest.scala
+++ src/test/scala/RequestResponseTest.scala
@@ -4,33 +4,42 @@ import scala.concurrent.duration._
 
 class RequestResponseTest extends Simulation {
   // Base test parameters
-  val host = System.getProperty("host", "127.0.0.1")
+  val host = System.getProperty("host", "localhost")
   val port = Integer.getInteger("port", 8080)
   val users = Integer.getInteger("users", 5000)
-  val duration = Integer.getInteger("duration", 120)
+  val duration = Integer.getInteger("duration", 60)
   // Derived test parameters
   val rampUpDuration: Integer = duration.toInt / 10
-  val scenarioDuration: Integer = 2 * duration.toInt / 10
+  val scenarioDuration: Integer = 8 * duration.toInt / 10
 
-  // Scenario parameters
-  val echoRequest = """"echoName""""
-  val url = s"ws://$host:$port/greeting/one"
+  val url = s"ws://$host:$port"
+  val echoRequest = """{"headers":{"q":"/greeting/one"},"data":"echoName"}"""
   val httpConfig = http.baseURL(s"http://$host:$port")
+
+  System.out.println("Server address: " + url)
+  System.out.println("Users count: " + users)
+  System.out.println("Simulation duration: " + duration + " seconds")
+  System.out.println("Scenario rampup: " + rampUpDuration + " seconds")
+  System.out.println("Scenario duration: " + scenarioDuration + " seconds")
 
   // User scenario is a chain of requests and pauses
   val usersScn = {
     scenario("RequestResponseTest")
-      .exec(ws("socket").open(url)
-      )
-      .pause(2 seconds)
+      .exec(ws("socket").open(url))
+      .pause(2)
       .during(scenarioDuration seconds) {
-        exec(ws("Echo Request").sendText(echoRequest).check(wsAwait.within(3000 milliseconds).until(1).regex( """(.*)echoName""").exists)).pause(1000 milliseconds)
+        exec(ws("Echo Request")
+          .sendText(echoRequest)
+          .check(wsAwait.within(3 seconds).until(1).regex( """.*Echo.*""")))
+          .pause(1)
       }
+      .pause(1)
       .exec(ws("close").close)
   }
 
   // Start user scenarios
   setUp(usersScn.inject(
-    rampUsers(users) over (rampUpDuration seconds))
-  ).protocols(httpConfig)
+    rampUsers(users) over (rampUpDuration seconds)))
+    .protocols(httpConfig)
+    .maxDuration(duration)
 }
