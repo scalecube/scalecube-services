97220656b3ab33d4b33301e462714a342adf764a
artem-v <artem.vysochyn@jivygroup.com>
2018-06-08 12:30:00 +0300

    Added directory config source; fixed config filename pattern

--- src/main/java/io/scalecube/gateway/config/GatewayConfigRegistry.java
+++ src/main/java/io/scalecube/gateway/config/GatewayConfigRegistry.java
@@ -4,16 +4,20 @@ import io.scalecube.config.ConfigRegistry;
 import io.scalecube.config.ConfigRegistrySettings;
 import io.scalecube.config.audit.Slf4JConfigEventListener;
 import io.scalecube.config.source.ClassPathConfigSource;
+import io.scalecube.config.source.DirectoryConfigSource;
 import io.scalecube.config.source.SystemEnvironmentConfigSource;
 import io.scalecube.config.source.SystemPropertiesConfigSource;
 
+import java.nio.file.Path;
+import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
 public class GatewayConfigRegistry {
 
   public static final String JMX_MBEAN_NAME = "io.scalecube.gateway.config:name=ConfigRegistry";
-  public static final Pattern CONFIG_FILENAME_PATTERN = Pattern.compile(".*/config/(.*)config(.*)?\\.properties");
-  public static final int RELOAD_INTERVAL_SEC = 300;
+  public static final Pattern CONFIG_PATTERN = Pattern.compile(".*(/)?config/(.*)config(.*)?\\.properties");
+  public static final Predicate<Path> PATH_PREDICATE = path -> CONFIG_PATTERN.matcher(path.toString()).matches();
+  public static final int RELOAD_INTERVAL_SEC = 1;
 
   private final ConfigRegistry configRegistry;
 
@@ -22,8 +26,8 @@ public class GatewayConfigRegistry {
         .addListener(new Slf4JConfigEventListener())
         .addLastSource("sys_prop", new SystemPropertiesConfigSource())
         .addLastSource("env_var", new SystemEnvironmentConfigSource())
-        .addLastSource("cp",
-            new ClassPathConfigSource(path -> CONFIG_FILENAME_PATTERN.matcher(path.toString()).matches()))
+        .addLastSource("dir", new DirectoryConfigSource("config", PATH_PREDICATE))
+        .addLastSource("cp", new ClassPathConfigSource(PATH_PREDICATE))
         .jmxMBeanName(JMX_MBEAN_NAME)
         .reloadIntervalSec(RELOAD_INTERVAL_SEC)
         .build());
