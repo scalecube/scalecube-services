3f739aad066d50f410dd93fcca4f26f1524b9591
artem-v <artem.vysochyn@jivygroup.com>
2018-06-08 16:57:26 +0300

    Added more tests; changed module layout; removed TestLib

--- src/main/java/io/scalecube/gateway/examples/GreetingService.java
+++ src/main/java/io/scalecube/gateway/examples/GreetingService.java
@@ -2,26 +2,12 @@ package io.scalecube.gateway.examples;
 
 import io.scalecube.services.annotations.Service;
 import io.scalecube.services.annotations.ServiceMethod;
-import io.scalecube.services.api.ServiceMessage;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service("greeting")
 public interface GreetingService {
-
-  ServiceMessage GREETING_ONE =
-      ServiceMessage.builder().qualifier("/greeting/one").data("hello").build();
-  ServiceMessage GREETING_FAILING_ONE =
-      ServiceMessage.builder().qualifier("/greeting/failing/one").data("hello").build();
-  ServiceMessage GREETING_MANY =
-      ServiceMessage.builder().qualifier("/greeting/many").data("hello").build();
-  ServiceMessage GREETING_FAILING_MANY =
-      ServiceMessage.builder().qualifier("/greeting/failing/many").data("hello").build();
-  ServiceMessage GREETING_POJO_ONE =
-      ServiceMessage.builder().qualifier("/greeting/pojo/one").data(new GreetingRequest("hello")).build();
-  ServiceMessage GREETING_POJO_MANY =
-      ServiceMessage.builder().qualifier("/greeting/pojo/many").data(new GreetingRequest("hello")).build();
 
   @ServiceMethod("one")
   Mono<String> one(String name);
