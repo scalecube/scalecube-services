f93d21dfe7cb718b6044c386179d5e9e50f80fa2
segabriel <sergiy.gabriel@jivygroup.com>
2018-06-26 14:31:43 +0300

    MPA-5332 gateway message to have flat fields (plus encoder/decoder)(#27)
    * MPA-5332 GatewayMessage contains flat headers
    
    * MPA-5332 GatewayMessage encoder/decoder
    
    * MPA-5332 GatewayMessage adding tests...
    
    * Prepared websocket acceptor, added convert methods for gateway message
    
    * MPA-5332 Skip data field while decoding
    
    * some extra changes
    
    * Fixed decoding in gateway codec, improved tests
    
    * Added encoding tests for gateway codec
    
    * Fixed gateway message codec, updated scalecube-service version (but only snapshot works), improved gateway tests
    
    * Fixed gatling scripts
    
    * Fix decoding of field names, fix null values decoding, add tests
    
    * Updated after review
    
    * change signature of Codec.encode()
    
    * Fixed codacy issues
    
    * Minor
    
    * Minor

--- src/main/java/io/scalecube/gateway/websocket/WebSocketAcceptor.java
+++ src/main/java/io/scalecube/gateway/websocket/WebSocketAcceptor.java
@@ -1,38 +1,22 @@
 package io.scalecube.gateway.websocket;
 
-import static io.scalecube.gateway.websocket.WebSocketSession.DEFAULT_CONTENT_TYPE;
-
+import io.scalecube.gateway.core.GatewayMessage;
+import io.scalecube.gateway.core.GatewayMessageCodec;
 import io.scalecube.services.ServiceCall;
 import io.scalecube.services.api.ServiceMessage;
 import io.scalecube.services.exceptions.BadRequestException;
 import io.scalecube.services.exceptions.ExceptionProcessor;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufAllocator;
-import io.netty.buffer.ByteBufInputStream;
-import io.netty.buffer.ByteBufOutputStream;
 import io.netty.handler.codec.http.websocketx.WebSocketFrame;
-import io.netty.util.ReferenceCountUtil;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.nio.charset.Charset;
-import java.util.HashMap;
-import java.util.Map;
 
 public final class WebSocketAcceptor {
 
@@ -40,11 +24,7 @@ public final class WebSocketAcceptor {
 
   private final ServiceCall serviceCall;
 
-  private static final ObjectMapper objectMapper;
-
-  static {
-    objectMapper = initMapper();
-  }
+  private final GatewayMessageCodec gatewayMessageCodec = new GatewayMessageCodec();
 
   public WebSocketAcceptor(ServiceCall serviceCall) {
     this.serviceCall = serviceCall;
@@ -61,14 +41,14 @@ public final class WebSocketAcceptor {
 
     Flux<ByteBuf> messages = session.receive()
         .flatMap(frame -> {
-          ServiceMessage request;
           try {
-            request = toMessage(frame);
+            ServiceMessage request = GatewayMessage.toServiceMessage(toMessage(frame));
             return serviceCall.requestMany(request);
           } catch (Throwable ex) {
             return Flux.just(ExceptionProcessor.toMessage(ex));
           }
         })
+        .map(GatewayMessage::toGatewayMessage)
         .map(this::toByteBuf)
         .doOnError(throwable -> session.close());
 
@@ -90,67 +70,26 @@ public final class WebSocketAcceptor {
     return Mono.empty();
   }
 
-  private ByteBuf toByteBuf(ServiceMessage message) {
-    ServiceMessage resultMessage = message;
-
-    if (message.hasData(ByteBuf.class)) {
-      ByteBuf byteBuf = message.data();
-      try (InputStream stream = new ByteBufInputStream(byteBuf.slice())) {
-        resultMessage = ServiceMessage.from(message)
-            .data(objectMapper.readTree(stream))
-            .build();
-      } catch (Throwable ex) {
-        LOGGER.error("Failed to decode data on: {}, cause: {}, data buffer: {}",
-            message, ex, byteBuf.toString(Charset.defaultCharset()));
-        throw new BadRequestException("Failed to decode data on message q=" + message.qualifier());
-      } finally {
-        ReferenceCountUtil.release(byteBuf);
-      }
-    }
-
-    ByteBuf byteBuf = ByteBufAllocator.DEFAULT.buffer();
-    try (OutputStream stream = new ByteBufOutputStream(byteBuf)) {
-      objectMapper.writeValue(stream, resultMessage);
-      return byteBuf;
+  private ByteBuf toByteBuf(GatewayMessage message) {
+    try {
+      return gatewayMessageCodec.encode(message);
     } catch (Throwable ex) {
-      ReferenceCountUtil.release(byteBuf);
-      LOGGER.error("Failed to encode message: {}, cause: {}", resultMessage, ex);
-      throw new BadRequestException("Failed to encode message q=" + resultMessage.qualifier());
+      LOGGER.error("Failed to encode message: {}, cause: {}", message, ex);
+      throw new BadRequestException("Failed to encode message q=" + message.qualifier());
     }
   }
 
-  private ServiceMessage toMessage(WebSocketFrame frame) {
+  private GatewayMessage toMessage(WebSocketFrame frame) {
     ByteBuf content = frame.content().slice();
-    ServiceMessage.Builder builder = ServiceMessage.builder();
-
-    try (InputStream stream = new ByteBufInputStream(content)) {
-      Map decoded = objectMapper.readValue(stream, HashMap.class);
-      // noinspection unchecked
-      return builder
-          .headers((Map) decoded.get("headers"))
-          .data(decoded.get("data"))
-          .dataFormat(DEFAULT_CONTENT_TYPE)
-          .build();
+    try {
+      return gatewayMessageCodec.decode(content);
     } catch (Throwable ex) {
       LOGGER.error("Failed to decode message headers: {}, cause: {}",
           content.toString(Charset.defaultCharset()), ex);
       throw new BadRequestException("Failed to decode message headers {headers=" + content.readableBytes()
           + ", data=" + content.readableBytes() + "}");
     } finally {
-      ReferenceCountUtil.release(content);
+      // ReferenceCountUtil.release(content);
     }
-  }
-
-  private static ObjectMapper initMapper() {
-    ObjectMapper mapper = new ObjectMapper();
-    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-    mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-    mapper.configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true);
-    mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-    mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-    mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-    mapper.configure(SerializationFeature.WRITE_ENUMS_USING_TO_STRING, true);
-    mapper.registerModule(new JavaTimeModule());
-    return mapper;
   }
 }
