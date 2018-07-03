4898fceea07ab6639297535d10eb10a15354a711
Artem Vysochyn <artem-v@users.noreply.github.com>
2018-07-03 13:25:56 +0300

    Prepare new release (#32)
    * WIP on tests
    
    * Refactored web socket gateway
    
    * Improved tests
    
    * Added failing test, need to pass it
    
    * Added failing many test
    
    * Minor
    
    * Added test with wrappers, need to fix mapping!
    
    * Fixed gateway tests
    
    * Fixed tests
    
    * Renaming in tests
    
    * Fixed checkstyle
    
    * Fixed gateway tests; fixed checkstyle
    
    * Fixed gateway tests; fixed checkstyle
    
    * Fixed gateway tests; fixed checkstyle; formattingl removed unsued class
    
    * Add request-response and request-stream gatling scenarios
    
    * Introduce Runners for example services and gateway. Add packaging
    
    * fix scenario
    
    * Set version for config; formatter poms
    
    * Finished reviewing runners
    
    * Fixed typo in examples runner pom
    
    * Added directory config source; fixed config filename pattern
    
    * Added more tests; changed module layout; removed TestLib
    
    * fix gatling req-resp scneario
    
    * change defaults for request-stream gatling scenario
    
    * Performance testing: put vm options to init script, increase ramp-up duration of gatling script
    
    * Rename folders of modules, add Logger module and configurations
    
    * Put new line at the end of file (pom.xml)
    
    * Remove -Xmx vm option for perf tests
    
    * Use G1 GC for perf  tests
    
    * Cosmetics:  reformatted poms
    
    * Update gatling scenarios (#23)
    
    * Added tests for checking empty responses (#24)
    
    * Added tests to check empty responses
    
    * MPA-5332 gateway message to have flat fields (plus encoder/decoder)(#27)
    
    * MPA-5332 GatewayMessage contains flat headers
    
    * MPA-5332 GatewayMessage encoder/decoder
    
    * MPA-5332 GatewayMessage adding tests...
    
    * Prepared websocket acceptor, added convert methods for gateway message
    
    * MPA-5332 Skip data field while decoding
    
    * some extra changes
    
    * Fixed decoding in gateway codec, improved tests
    
    * Added encoding tests for gateway codec
    
    * Fixed gateway message codec, updated scalecube-service version (but only snapshot works), improved gateway tests
    
    * Fixed gatling scripts
    
    * Fix decoding of field names, fix null values decoding, add tests
    
    * Updated after review
    
    * change signature of Codec.encode()
    
    * Fixed codacy issues
    
    * Minor
    
    * Minor
    
    * Added signal support (#29)
    
    * Added SignalTypes, refactoring
    
    * Attempt to support signal in given error message
    
    * refactoring
    
    * final refactoring
    
    * Fixed tests
    
    * Refactoring tests
    
    * Added checking non optional fields
    
    * Minor
    
    * Fixed review issue
    
    * Fixed tests
    
    * Added required filed (sid) in requests of gatling scripts
    
    * Changed streamId type to Long (Nonce.js)
    
    * Changed scalecube-services version and jackson
    
    * Minor
    
    * gateway message codec benchmarks (#28)
    
    * gateway message codec benchmarks
    
    * Updated services version (#31)
    
    Updated services version

---
+++ src/main/java/io/scalecube/gateway/config/GatewayConfigRegistry.java
@@ -0,0 +1,39 @@
+package io.scalecube.gateway.config;
+
+import io.scalecube.config.ConfigRegistry;
+import io.scalecube.config.ConfigRegistrySettings;
+import io.scalecube.config.audit.Slf4JConfigEventListener;
+import io.scalecube.config.source.ClassPathConfigSource;
+import io.scalecube.config.source.DirectoryConfigSource;
+import io.scalecube.config.source.SystemEnvironmentConfigSource;
+import io.scalecube.config.source.SystemPropertiesConfigSource;
+
+import java.nio.file.Path;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
+public class GatewayConfigRegistry {
+
+  public static final String JMX_MBEAN_NAME = "io.scalecube.gateway.config:name=ConfigRegistry";
+  public static final Pattern CONFIG_PATTERN = Pattern.compile(".*(/)?config/(.*)config(.*)?\\.properties");
+  public static final Predicate<Path> PATH_PREDICATE = path -> CONFIG_PATTERN.matcher(path.toString()).matches();
+  public static final int RELOAD_INTERVAL_SEC = 1;
+
+  private final ConfigRegistry configRegistry;
+
+  private GatewayConfigRegistry() {
+    this.configRegistry = ConfigRegistry.create(ConfigRegistrySettings.builder()
+        .addListener(new Slf4JConfigEventListener())
+        .addLastSource("sys_prop", new SystemPropertiesConfigSource())
+        .addLastSource("env_var", new SystemEnvironmentConfigSource())
+        .addLastSource("dir", new DirectoryConfigSource("config", PATH_PREDICATE))
+        .addLastSource("cp", new ClassPathConfigSource(PATH_PREDICATE))
+        .jmxMBeanName(JMX_MBEAN_NAME)
+        .reloadIntervalSec(RELOAD_INTERVAL_SEC)
+        .build());
+  }
+
+  public static ConfigRegistry configRegistry() {
+    return new GatewayConfigRegistry().configRegistry;
+  }
+}
