7375195f894e8fa131b52d95be6012d26144853c
segabriel <sergiy.gabriel@jivygroup.com>
2018-07-03 11:43:40 +0300

    gateway message codec benchmarks (#28)
    * gateway message codec benchmarks

---
+++ src/main/java/io/scalecube/gateway/benchmarks/EncodeGatewayMessageBenchmarkRunner.java
@@ -0,0 +1,32 @@
+package io.scalecube.gateway.benchmarks;
+
+import io.scalecube.benchmarks.BenchmarksSettings;
+import io.scalecube.gateway.core.GatewayMessage;
+import io.scalecube.gateway.core.GatewayMessageCodec;
+
+import com.codahale.metrics.Timer;
+
+import io.netty.buffer.ByteBuf;
+
+import java.util.concurrent.TimeUnit;
+
+public class EncodeGatewayMessageBenchmarkRunner {
+
+  public static void main(String[] args) {
+    BenchmarksSettings settings = BenchmarksSettings.from(args).durationUnit(TimeUnit.NANOSECONDS).build();
+    new GatewayMessageCodecBenchmarkState(settings).runForSync(state -> {
+
+      GatewayMessageCodec codec = state.codec();
+      GatewayMessage message = state.message();
+      Timer timer = state.timer("timer");
+
+      return i -> {
+        Timer.Context timerContext = timer.time();
+        ByteBuf bb = codec.encode(message);
+        timerContext.stop();
+        bb.release();
+        return bb;
+      };
+    });
+  }
+}
