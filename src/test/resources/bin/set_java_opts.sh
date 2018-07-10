1d7b74044784b90c706020d0942854e4b3d2996d
artem-v <artem.vysochyn@jivygroup.com>
2018-07-10 17:10:05 +0300

    Moved gatling artifacts from Examples to Tests

--- src/test/resources/bin/set_java_opts.sh
+++
@@ -1,5 +0,0 @@
-GC_OPTS="-XX:+UseG1GC -Xms3g"
-PROFILER_OPTS="-agentpath:/opt/gatling/bin/linux-x86-64/libyjpagent.so"
-ON_OOM_OPTS="-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp/`date "+%Y/%m/%d-%H:%M:%S"`.hprof"
-
-export JAVA_OPTS="-server $GC_OPTS $PROFILER_OPTS $ON_OOM_OPTS ${JAVA_OPTS}"
