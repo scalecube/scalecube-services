12f343b137122aefb30b33aaacc13ac22f7067fd
segabriel <sergiy.gabriel@jivygroup.com>
2018-08-17 16:57:52 +0300

    Websocket bootstrapping (#106)
    * Added websocket gateway implementations

--- src/main/java/io/scalecube/gateway/examples/ExampleGatewayRunner.java
+++ src/main/java/io/scalecube/gateway/examples/ExampleGatewayRunner.java
@@ -1,18 +1,17 @@
 package io.scalecube.gateway.examples;
 
-import io.scalecube.gateway.websocket.WebsocketServer;
+import io.scalecube.gateway.websocket.WebsocketGateway;
 import io.scalecube.services.Microservices;
+import io.scalecube.services.gateway.GatewayConfig;
 
 public class ExampleGatewayRunner {
 
   public static void main(String[] args) throws InterruptedException {
-    
+
     Microservices service = Microservices.builder()
+        .gateway(GatewayConfig.builder("ws", WebsocketGateway.class).port(8080).build())
         .services(new GreetingServiceImpl())
         .startAwait();
-    
-    WebsocketServer server = new WebsocketServer(service);
-    server.start(8080);
 
     Thread.currentThread().join();
   }
