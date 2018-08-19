813a1971f5afcba8446856a4319fe4b4a35d872d
io-scalecube-ci <io.scalecube.ci@gmail.com>
2018-08-19 11:23:09 +0000

    + script files

--- src/main/scripts/ci/after-success.sh
+++ src/main/scripts/ci/after-success.sh
@@ -5,8 +5,8 @@ echo       Running $0
 echo *-*-*-*-*-*-*-*-*-*-*-*-*-*
 
 if [ -z "$CODACY_PROJECT_TOKEN" ]; then
-    echo [WARNING] Please go to https://app.codacy.com/app/$TRAVIS_REPO_SLUG/settings/coverage and add CODACY_PROJECT_TOKEN to travis settings
+	echo [WARNING] Please go to https://app.codacy.com/app/$TRAVIS_REPO_SLUG/settings/coverage and add CODACY_PROJECT_TOKEN to travis settings
 else 
-    find -name jacoco.xml | xargs -i java -jar ~/codacy-coverage-reporter-assembly.jar report -l Java --partial -r {}
-    java -jar ~/codacy-coverage-reporter-assembly.jar final
+	find -name jacoco.xml -exec java -jar ~/codacy-coverage-reporter-assembly.jar report -l Java -r {} --partial\;
+	java -jar ~/codacy-coverage-reporter-assembly.jar final
 fi;
