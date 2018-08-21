50d89e6953922d068c800bbbc7613541d4789e7b
segabriel <sergiy.gabriel@jivygroup.com>
2018-08-21 22:51:52 +0300

    Added broadcast method to examples

--- src/main/java/io/scalecube/gateway/benchmarks/example/ExampleService.java
+++ src/main/java/io/scalecube/gateway/benchmarks/example/ExampleService.java
@@ -20,4 +20,7 @@ public interface ExampleService {
 
   @ServiceMethod("requestInfiniteStream")
   Flux<Long> requestInfiniteStream(StreamRequest request);
+
+  @ServiceMethod("broadcast")
+  Flux<Long> broadcast();
 }
