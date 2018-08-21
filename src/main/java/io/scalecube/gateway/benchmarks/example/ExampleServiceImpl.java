0fdfde8e8b3c303fd0627772dc77517327f38e56
segabriel <sergiy.gabriel@jivygroup.com>
2018-08-21 22:51:52 +0300

    Added broadcast method to examples

--- src/main/java/io/scalecube/gateway/benchmarks/example/ExampleServiceImpl.java
+++ src/main/java/io/scalecube/gateway/benchmarks/example/ExampleServiceImpl.java
@@ -4,6 +4,7 @@ import io.scalecube.gateway.examples.StreamRequest;
 import java.time.Duration;
 import java.util.stream.LongStream;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink.OverflowStrategy;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
@@ -45,5 +46,23 @@ public class ExampleServiceImpl implements ExampleService {
     return Flux.concat(fluxes)
         .publishOn(Schedulers.parallel(), Integer.MAX_VALUE)
         .onBackpressureDrop();
+  }
+
+  private Flux<Long> source =
+    Flux.<Long>create(
+      sink -> {
+        while (true) {
+          sink.next(1L);
+        }
+      },
+      OverflowStrategy.DROP)
+      .subscribeOn(Schedulers.newSingle("source"))
+      .publish()
+      .autoConnect()
+      .onBackpressureDrop();
+
+  @Override
+  public Flux<Long> broadcast() {
+    return source.map(i -> System.currentTimeMillis());
   }
 }
