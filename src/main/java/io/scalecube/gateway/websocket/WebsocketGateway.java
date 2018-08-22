6263c18cff5fafd44dc24fa025d05f6e8b8b3ad1
artem-v <artem.vysochyn@jivygroup.com>
2018-08-22 15:59:43 +0300

    Integration of gateway with event loop chooser interface

--- src/main/java/io/scalecube/gateway/websocket/WebsocketGateway.java
+++ src/main/java/io/scalecube/gateway/websocket/WebsocketGateway.java
@@ -1,10 +1,14 @@
 package io.scalecube.gateway.websocket;
 
+import io.netty.channel.EventLoopGroup;
 import io.scalecube.services.ServiceCall;
 import io.scalecube.services.gateway.Gateway;
 import io.scalecube.services.gateway.GatewayConfig;
 import io.scalecube.services.metrics.Metrics;
-
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executor;
+import java.util.function.BiFunction;
+import org.reactivestreams.Publisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
@@ -15,14 +19,6 @@ import reactor.ipc.netty.http.websocket.WebsocketInbound;
 import reactor.ipc.netty.http.websocket.WebsocketOutbound;
 import reactor.ipc.netty.tcp.BlockingNettyContext;
 
-import io.netty.channel.EventLoopGroup;
-
-import org.reactivestreams.Publisher;
-
-import java.net.InetSocketAddress;
-import java.util.concurrent.ExecutorService;
-import java.util.function.BiFunction;
-
 public class WebsocketGateway implements Gateway {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(WebsocketGateway.class);
@@ -30,43 +26,50 @@ public class WebsocketGateway implements Gateway {
   private BlockingNettyContext server;
 
   @Override
-  public Mono<InetSocketAddress> start(GatewayConfig config,
-      ExecutorService executorService,
+  public Mono<InetSocketAddress> start(
+    GatewayConfig config,
+    Executor selectorThreadPool,
+    Executor workerThreadPool,
       ServiceCall.Call call,
       Metrics metrics) {
 
-    return Mono.defer(() -> {
-      LOGGER.info("Starting gateway with {}", config);
+    return Mono.defer(
+      () -> {
+        LOGGER.info("Starting gateway with {}", config);
 
-      InetSocketAddress listenAddress = new InetSocketAddress(config.port());
-      WebsocketAcceptor acceptor = new WebsocketAcceptor(call.create(), metrics);
-      server = HttpServer.builder()
-          .options(opts -> {
-            opts.listenAddress(listenAddress);
-            if (config.executorService() != null) {
-              opts.eventLoopGroup((EventLoopGroup) config.executorService());
-            } else if (executorService != null) {
-              opts.eventLoopGroup((EventLoopGroup) executorService);
-            }
-          })
-          .build()
-          .start(new WebSocketServerBiFunction(acceptor));
-      server.installShutdownHook();
-      InetSocketAddress address = server.getContext().address();
+        InetSocketAddress listenAddress = new InetSocketAddress(config.port());
+        WebsocketAcceptor acceptor = new WebsocketAcceptor(call.create(), metrics);
+        server =
+          HttpServer.builder()
+            .options(
+              opts -> {
+                opts.listenAddress(listenAddress);
+                if (selectorThreadPool != null && workerThreadPool != null) {
+                  opts.loopResources(
+                    new WebsocketLoopResources(
+                      (EventLoopGroup) selectorThreadPool,
+                      (EventLoopGroup) workerThreadPool));
+                }
+              })
+            .build()
+            .start(new WebSocketServerBiFunction(acceptor));
+        server.installShutdownHook();
+        InetSocketAddress address = server.getContext().address();
 
-      LOGGER.info("Gateway has been started successfully on {}", address);
+        LOGGER.info("Gateway has been started successfully on {}", address);
 
-      return Mono.just(address);
-    });
+        return Mono.just(address);
+      });
   }
 
   @Override
   public Mono<Void> stop() {
-    return Mono.fromRunnable(() -> {
-      if (server != null) {
-        server.shutdown();
-      }
-    });
+    return Mono.fromRunnable(
+      () -> {
+        if (server != null) {
+          server.shutdown();
+        }
+      });
   }
 
   private static class WebSocketServerBiFunction
@@ -80,12 +83,13 @@ public class WebsocketGateway implements Gateway {
 
     @Override
     public Publisher<Void> apply(HttpServerRequest httpRequest, HttpServerResponse httpResponse) {
-      return httpResponse.sendWebsocket((WebsocketInbound inbound, WebsocketOutbound outbound) -> {
-        WebsocketSession session = new WebsocketSession(httpRequest, inbound, outbound);
-        Mono<Void> voidMono = acceptor.onConnect(session);
-        session.onClose(() -> acceptor.onDisconnect(session));
-        return voidMono;
-      });
+      return httpResponse.sendWebsocket(
+        (WebsocketInbound inbound, WebsocketOutbound outbound) -> {
+          WebsocketSession session = new WebsocketSession(httpRequest, inbound, outbound);
+          Mono<Void> voidMono = acceptor.onConnect(session);
+          session.onClose(() -> acceptor.onDisconnect(session));
+          return voidMono;
+        });
     }
   }
 }
