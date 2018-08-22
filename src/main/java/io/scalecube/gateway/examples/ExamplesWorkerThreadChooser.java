5b372f641d4f2d81c4493955a1565abbf3862279
segabriel <sergiy.gabriel@jivygroup.com>
2018-08-22 18:43:21 +0300

    Fixed CR comments

--- src/main/java/io/scalecube/gateway/examples/ExamplesWorkerThreadChooser.java
+++ src/main/java/io/scalecube/gateway/examples/ExamplesWorkerThreadChooser.java
@@ -1,7 +1,6 @@
 package io.scalecube.gateway.examples;
 
 import io.scalecube.services.transport.api.WorkerThreadChooser;
-import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -10,17 +9,13 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class ExamplesWorkerThreadChooser implements WorkerThreadChooser {
 
-  private final Map<String, AtomicInteger> counterByRemoteHost = new ConcurrentHashMap<>();
+  private final Map<String, AtomicInteger> counterByRemoteHostAndId = new ConcurrentHashMap<>();
 
   @Override
   public Executor getWorker(
     String id, SocketAddress localAddress, SocketAddress remoteAddress, Executor[] executors) {
-    if (!(remoteAddress instanceof InetSocketAddress)) {
-      return null;
-    }
-    String hostString = ((InetSocketAddress) remoteAddress).getHostString();
     AtomicInteger counter =
-      counterByRemoteHost.computeIfAbsent(hostString, key -> new AtomicInteger());
+      counterByRemoteHostAndId.computeIfAbsent(id, key -> new AtomicInteger());
     int index = (counter.incrementAndGet() & Integer.MAX_VALUE) % executors.length;
     return executors[index];
   }
