c20c2cb69158ade90a571ff5706750d83752c52a
artem-v <artem.vysochyn@jivygroup.com>
2018-08-26 15:10:04 +0300

    Fninished

--- src/main/java/io/scalecube/gateway/http/HttpGateway.java
+++ src/main/java/io/scalecube/gateway/http/HttpGateway.java
@@ -1,28 +1,41 @@
 package io.scalecube.gateway.http;
 
+import io.netty.util.concurrent.DefaultThreadFactory;
+import io.scalecube.gateway.GatewayTemplate;
 import io.scalecube.services.ServiceCall;
-import io.scalecube.services.gateway.Gateway;
 import io.scalecube.services.gateway.GatewayConfig;
 import io.scalecube.services.metrics.Metrics;
 import java.net.InetSocketAddress;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
 import reactor.ipc.netty.http.server.HttpServer;
+import reactor.ipc.netty.resources.LoopResources;
 import reactor.ipc.netty.tcp.BlockingNettyContext;
 
-public class HttpGateway implements Gateway {
+/**
+ * Gateway implementation on Http.
+ */
+public class HttpGateway extends GatewayTemplate {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(HttpGateway.class);
+
+  private static final DefaultThreadFactory BOSS_THREAD_FACTORY =
+    new DefaultThreadFactory("http-boss", true);
+
+  private static final Duration START_TIMEOUT = Duration.ofSeconds(30);
 
   private BlockingNettyContext server;
 
   @Override
   public Mono<InetSocketAddress> start(
     GatewayConfig config,
-    Executor selectorThreadPool,
     Executor workerThreadPool,
+    boolean preferNative,
     ServiceCall.Call call,
       Metrics metrics) {
 
@@ -31,36 +44,41 @@ public class HttpGateway implements Gateway {
         LOGGER.info("Starting gateway with {}", config);
 
         InetSocketAddress listenAddress = new InetSocketAddress(config.port());
-        GatewayHttpAcceptor acceptor = new GatewayHttpAcceptor(call.create());
+
+        LoopResources loopResources =
+          prepareLoopResources(preferNative, BOSS_THREAD_FACTORY, config, workerThreadPool);
+
+        GatewayHttpAcceptor httpAcceptor = new GatewayHttpAcceptor(call.create());
 
         server =
           HttpServer.builder()
             .options(
               opts -> {
                 opts.listenAddress(listenAddress);
-                if (selectorThreadPool != null && workerThreadPool != null) {
-                  opts.loopResources(
-                    new HttpLoopResources(true, selectorThreadPool, workerThreadPool));
+                if (loopResources != null) {
+                  opts.loopResources(loopResources);
                 }
               })
             .build()
-            .start(acceptor);
-        server.installShutdownHook();
+            .start(httpAcceptor, START_TIMEOUT);
+
         InetSocketAddress address = server.getContext().address();
-
         LOGGER.info("Gateway has been started successfully on {}", address);
-
         return Mono.just(address);
       });
   }
 
   @Override
   public Mono<Void> stop() {
-    return Mono.fromRunnable(
+    return Mono.defer(
       () -> {
+        List<Mono<Void>> stopList = new ArrayList<>();
+        stopList.add(shutdownBossGroup());
         if (server != null) {
-          server.shutdown();
+          server.getContext().dispose();
+          stopList.add(server.getContext().onClose());
         }
+        return Mono.when(stopList);
       });
   }
 }
