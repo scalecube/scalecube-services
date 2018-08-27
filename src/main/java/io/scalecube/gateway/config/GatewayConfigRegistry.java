b146eafe6fc272e8731b451a482c761d2af2c61e
artem-v <artem.vysochyn@jivygroup.com>
2018-08-27 19:40:00 +0300

    Formatted with google style

--- src/main/java/io/scalecube/gateway/config/GatewayConfigRegistry.java
+++ src/main/java/io/scalecube/gateway/config/GatewayConfigRegistry.java
@@ -7,7 +7,6 @@ import io.scalecube.config.source.ClassPathConfigSource;
 import io.scalecube.config.source.DirectoryConfigSource;
 import io.scalecube.config.source.SystemEnvironmentConfigSource;
 import io.scalecube.config.source.SystemPropertiesConfigSource;
-
 import java.nio.file.Path;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
@@ -15,20 +14,24 @@ import java.util.regex.Pattern;
 public class GatewayConfigRegistry {
 
   private static final String JMX_MBEAN_NAME = "io.scalecube.gateway.config:name=ConfigRegistry";
-  private static final Pattern CONFIG_PATTERN = Pattern.compile(".*[\\\\|/]?config[\\\\|/](.*)config(.*)?\\.properties");
-  private static final Predicate<Path> PATH_PREDICATE = path -> CONFIG_PATTERN.matcher(path.toString()).matches();
+  private static final Pattern CONFIG_PATTERN =
+    Pattern.compile(".*[\\\\|/]?config[\\\\|/](.*)config(.*)?\\.properties");
+  private static final Predicate<Path> PATH_PREDICATE =
+    path -> CONFIG_PATTERN.matcher(path.toString()).matches();
 
   private final ConfigRegistry configRegistry;
 
   private GatewayConfigRegistry() {
-    this.configRegistry = ConfigRegistry.create(ConfigRegistrySettings.builder()
-        .addListener(new Slf4JConfigEventListener())
-        .addLastSource("sys_prop", new SystemPropertiesConfigSource())
-        .addLastSource("env_var", new SystemEnvironmentConfigSource())
-        .addLastSource("dir", new DirectoryConfigSource("config", PATH_PREDICATE))
-        .addLastSource("cp", new ClassPathConfigSource(PATH_PREDICATE))
-        .jmxMBeanName(JMX_MBEAN_NAME)
-        .build());
+    this.configRegistry =
+      ConfigRegistry.create(
+        ConfigRegistrySettings.builder()
+          .addListener(new Slf4JConfigEventListener())
+          .addLastSource("sys_prop", new SystemPropertiesConfigSource())
+          .addLastSource("env_var", new SystemEnvironmentConfigSource())
+          .addLastSource("dir", new DirectoryConfigSource("config", PATH_PREDICATE))
+          .addLastSource("cp", new ClassPathConfigSource(PATH_PREDICATE))
+          .jmxMBeanName(JMX_MBEAN_NAME)
+          .build());
   }
 
   public static ConfigRegistry configRegistry() {
