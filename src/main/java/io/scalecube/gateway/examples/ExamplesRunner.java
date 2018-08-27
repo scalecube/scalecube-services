eaa06734b6feeea18a6ff5ab11b75f09233914bc
segabriel <sergiy.gabriel@jivygroup.com>
2018-08-27 17:24:03 +0300

    Changed to Schedulers.parallel

--- src/main/java/io/scalecube/gateway/examples/ExamplesRunner.java
+++ src/main/java/io/scalecube/gateway/examples/ExamplesRunner.java
@@ -7,14 +7,12 @@ import io.scalecube.transport.Address;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * Runner for example services.
- */
+/** Runner for example services. */
 public class ExamplesRunner {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(ExamplesRunner.class);
   private static final String DECORATOR =
-    "***********************************************************************";
+      "***********************************************************************";
 
   /**
    * Main method of runner for example services.
@@ -39,7 +37,6 @@ public class ExamplesRunner {
         .seeds(seeds)
         .servicePort(servicePort)
         .services(new GreetingServiceImpl())
-      .numOfThreads(1)
         .startAwait();
 
     Thread.currentThread().join();
