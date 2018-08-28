ef3c62e592887447d583563ab95eb01103959bbe
artem-v <artem.vysochyn@jivygroup.com>
2018-08-28 14:17:50 +0300

    Fixing checkstyle issues + google code format

--- src/main/java/io/scalecube/gateway/config/GatewayConfigRegistry.java
+++ src/main/java/io/scalecube/gateway/config/GatewayConfigRegistry.java
@@ -15,23 +15,23 @@ public class GatewayConfigRegistry {
 
   private static final String JMX_MBEAN_NAME = "io.scalecube.gateway.config:name=ConfigRegistry";
   private static final Pattern CONFIG_PATTERN =
-    Pattern.compile(".*[\\\\|/]?config[\\\\|/](.*)config(.*)?\\.properties");
+      Pattern.compile(".*[\\\\|/]?config[\\\\|/](.*)config(.*)?\\.properties");
   private static final Predicate<Path> PATH_PREDICATE =
-    path -> CONFIG_PATTERN.matcher(path.toString()).matches();
+      path -> CONFIG_PATTERN.matcher(path.toString()).matches();
 
   private final ConfigRegistry configRegistry;
 
   private GatewayConfigRegistry() {
     this.configRegistry =
-      ConfigRegistry.create(
-        ConfigRegistrySettings.builder()
-          .addListener(new Slf4JConfigEventListener())
-          .addLastSource("sys_prop", new SystemPropertiesConfigSource())
-          .addLastSource("env_var", new SystemEnvironmentConfigSource())
-          .addLastSource("dir", new DirectoryConfigSource("config", PATH_PREDICATE))
-          .addLastSource("cp", new ClassPathConfigSource(PATH_PREDICATE))
-          .jmxMBeanName(JMX_MBEAN_NAME)
-          .build());
+        ConfigRegistry.create(
+            ConfigRegistrySettings.builder()
+                .addListener(new Slf4JConfigEventListener())
+                .addLastSource("sys_prop", new SystemPropertiesConfigSource())
+                .addLastSource("env_var", new SystemEnvironmentConfigSource())
+                .addLastSource("dir", new DirectoryConfigSource("config", PATH_PREDICATE))
+                .addLastSource("cp", new ClassPathConfigSource(PATH_PREDICATE))
+                .jmxMBeanName(JMX_MBEAN_NAME)
+                .build());
   }
 
   public static ConfigRegistry configRegistry() {
