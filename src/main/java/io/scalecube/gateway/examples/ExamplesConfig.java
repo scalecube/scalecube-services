bbc2f76e9265ac3f6560e4a56d023a5afa2f7a3b
artem-v <artem.vysochyn@jivygroup.com>
2018-08-31 16:02:31 +0300

    Added examples runner module; removed examples api module

---
+++ src/main/java/io/scalecube/gateway/examples/ExamplesConfig.java
@@ -0,0 +1,37 @@
+package io.scalecube.gateway.examples;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ExamplesConfig {
+
+  private int servicePort = 5801;
+  private List<String> seeds = Arrays.asList("localhost:4801");
+
+  public ExamplesConfig() {}
+
+  public int getServicePort() {
+    return servicePort;
+  }
+
+  public void setServicePort(int servicePort) {
+    this.servicePort = servicePort;
+  }
+
+  public List<String> getSeedAddress() {
+    return seeds;
+  }
+
+  public void setSeedAddress(List<String> seedAddress) {
+    this.seeds = seedAddress;
+  }
+
+  @Override
+  public String toString() {
+    final StringBuilder sb = new StringBuilder("ExamplesConfig{");
+    sb.append("servicePort=").append(servicePort);
+    sb.append(", seeds='").append(seeds).append('\'');
+    sb.append('}');
+    return sb.toString();
+  }
+}
