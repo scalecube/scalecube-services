9aadd51c82974d25e19b0de11755a83316dc5e14
artem-v <artem.vysochyn@jivygroup.com>
2018-09-06 11:55:02 +0300

    WIP; changed session.onClose

--- src/test/java/io/scalecube/gateway/websocket/WebsocketClientTransportTest.java
+++ src/test/java/io/scalecube/gateway/websocket/WebsocketClientTransportTest.java
@@ -5,6 +5,8 @@ import io.scalecube.gateway.clientsdk.ClientSettings;
 import io.scalecube.gateway.clientsdk.codec.WebsocketGatewayMessageCodec;
 import io.scalecube.gateway.clientsdk.websocket.WebsocketClientTransport;
 import io.scalecube.services.codec.DataCodec;
+import java.time.Duration;
+import reactor.core.publisher.Mono;
 import reactor.ipc.netty.resources.LoopResources;
 
 public class WebsocketClientTransportTest {
@@ -22,15 +24,15 @@ public class WebsocketClientTransportTest {
             loopResources);
 
     ClientMessage request =
-        ClientMessage.builder().qualifier("/greeting/one").header("sid", "1").build();
+        ClientMessage.builder().qualifier("/greeting/one").header("sid", "1").data("hello").build();
 
-    //    Mono.delay(Duration.ofSeconds(15))
-    //        .doOnTerminate(
-    //            () -> {
-    //              System.err.println("### Closing ...");
-    //              transport.close().subscribe();
-    //            })
-    //        .subscribe();
+    Mono.delay(Duration.ofSeconds(5))
+        .doOnTerminate(
+            () -> {
+              System.err.println("### Closing ...");
+              transport.close().subscribe();
+            })
+        .subscribe();
 
     transport
         .requestResponse(request)
