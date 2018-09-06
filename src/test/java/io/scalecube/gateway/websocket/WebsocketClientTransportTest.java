3958a0bf6fe5a20ddd774b1b6645ed666551ea93
artem-v <artem.vysochyn@jivygroup.com>
2018-09-06 14:13:19 +0300

    WIP; changed session.onClose; made inbound processor

--- src/test/java/io/scalecube/gateway/websocket/WebsocketClientTransportTest.java
+++ src/test/java/io/scalecube/gateway/websocket/WebsocketClientTransportTest.java
@@ -1,12 +1,11 @@
 package io.scalecube.gateway.websocket;
 
-import io.scalecube.gateway.clientsdk.ClientMessage;
+import io.scalecube.gateway.clientsdk.Client;
 import io.scalecube.gateway.clientsdk.ClientSettings;
 import io.scalecube.gateway.clientsdk.codec.WebsocketGatewayMessageCodec;
 import io.scalecube.gateway.clientsdk.websocket.WebsocketClientTransport;
+import io.scalecube.gateway.examples.GreetingService;
 import io.scalecube.services.codec.DataCodec;
-import java.time.Duration;
-import reactor.core.publisher.Mono;
 import reactor.ipc.netty.resources.LoopResources;
 
 public class WebsocketClientTransportTest {
@@ -17,27 +16,25 @@ public class WebsocketClientTransportTest {
         ClientSettings.builder().contentType(contentType).host("localhost").port(7070).build();
     LoopResources loopResources = LoopResources.create("worker");
 
+    WebsocketGatewayMessageCodec codec =
+        new WebsocketGatewayMessageCodec(DataCodec.getInstance(contentType));
+
     WebsocketClientTransport transport =
-        new WebsocketClientTransport(
-            settings,
-            new WebsocketGatewayMessageCodec(DataCodec.getInstance(contentType)),
-            loopResources);
+        new WebsocketClientTransport(settings, codec, loopResources);
 
-    ClientMessage request =
-        ClientMessage.builder().qualifier("/greeting/one").header("sid", "1").data("hello").build();
-
-    Mono.delay(Duration.ofSeconds(5))
-        .doOnTerminate(
-            () -> {
-              System.err.println("### Closing ...");
-              transport.close().subscribe();
-            })
-        .subscribe();
-
-    transport
-        .requestResponse(request)
+    Client client = new Client(transport, codec);
+    client
+        .forService(GreetingService.class)
+        .neverOne("hello")
         .subscribe(
-            System.out::println, System.err::println, () -> System.out.println("### Complete"));
+            System.out::println,
+            System.err::println,
+            () -> System.err.println("neverOne complete"));
+    client
+        .forService(GreetingService.class)
+        .one("hello")
+        .subscribe(
+            System.out::println, System.err::println, () -> System.err.println("one complete"));
 
     Thread.currentThread().join();
   }
