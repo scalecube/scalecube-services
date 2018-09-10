bfc7fe787cad155767e3488bfe7ecaf7180c158c
artem-v <artem.vysochyn@jivygroup.com>
2018-09-10 12:11:10 +0300

    Fixed cR3

--- src/main/java/io/scalecube/gateway/benchmarks/AbstractBenchmarkState.java
+++ src/main/java/io/scalecube/gateway/benchmarks/AbstractBenchmarkState.java
@@ -4,9 +4,10 @@ import io.rsocket.Payload;
 import io.scalecube.benchmarks.BenchmarkSettings;
 import io.scalecube.benchmarks.BenchmarkState;
 import io.scalecube.gateway.clientsdk.Client;
+import io.scalecube.gateway.clientsdk.ClientCodec;
 import io.scalecube.gateway.clientsdk.ClientMessage;
 import io.scalecube.gateway.clientsdk.ClientSettings;
-import io.scalecube.gateway.clientsdk.ClientCodec;
+import io.scalecube.gateway.clientsdk.ClientTransport;
 import io.scalecube.gateway.clientsdk.rsocket.RSocketClientCodec;
 import io.scalecube.gateway.clientsdk.rsocket.RSocketClientTransport;
 import io.scalecube.services.codec.DataCodec;
@@ -42,11 +43,11 @@ public abstract class AbstractBenchmarkState<T extends AbstractBenchmarkState<T>
   protected final Mono<Client> createClient(ClientSettings settings) {
     HeadersCodec headersCodec = HeadersCodec.getInstance(settings.contentType());
     DataCodec dataCodec = DataCodec.getInstance(settings.contentType());
-    ClientCodec<Payload> messageCodec = new RSocketClientCodec(headersCodec, dataCodec);
+    ClientCodec<Payload> clientCodec = new RSocketClientCodec(headersCodec, dataCodec);
 
-    RSocketClientTransport transport =
-        new RSocketClientTransport(settings, messageCodec, loopResources);
-    Client client = new Client(transport, messageCodec);
+    ClientTransport clientTransport =
+        new RSocketClientTransport(settings, clientCodec, loopResources);
+    Client client = new Client(clientTransport, clientCodec);
 
     ClientMessage request = ClientMessage.builder().qualifier("/benchmarks/one").build();
     return client.requestResponse(request).then(Mono.just(client));
