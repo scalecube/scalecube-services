e2dcfafb8b24d36893d7c07108b1fa06cd339494
artem-v <artem.vysochyn@jivygroup.com>
2018-09-12 15:07:55 +0300

    Refactored receive

--- src/main/java/io/scalecube/gateway/clientsdk/websocket/WebsocketClientTransport.java
+++ src/main/java/io/scalecube/gateway/clientsdk/websocket/WebsocketClientTransport.java
@@ -1,20 +1,25 @@
 package io.scalecube.gateway.clientsdk.websocket;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
 import io.scalecube.gateway.clientsdk.ClientCodec;
 import io.scalecube.gateway.clientsdk.ClientMessage;
 import io.scalecube.gateway.clientsdk.ClientSettings;
 import io.scalecube.gateway.clientsdk.ClientTransport;
+import io.scalecube.gateway.clientsdk.ErrorData;
+import io.scalecube.gateway.clientsdk.exceptions.ExceptionProcessor;
 import java.net.InetSocketAddress;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+import java.util.function.Consumer;
+import java.util.logging.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.MonoSink;
+import reactor.core.scheduler.Schedulers;
 import reactor.ipc.netty.http.client.HttpClient;
 import reactor.ipc.netty.resources.LoopResources;
 
@@ -62,14 +67,14 @@ public final class WebsocketClientTransport implements ClientTransport {
     return Mono.defer(
         () -> {
           String sid = String.valueOf(sidCounter.incrementAndGet());
-          BinaryWebSocketFrame frame = toRequest(request, sid);
+          ByteBuf byteBuf = toRequest(request, sid);
 
           return getOrConnect()
               .flatMap(
                   session ->
                       session
-                          .send(frame)
-                          .then(session.receiveResponse(sid))
+                          .send(byteBuf)
+                          .then(receiveResponse(session.receive(), sid))
                           .map(this::enrichForRecv)
                           .doOnCancel(() -> handleCancel(sid, session)));
         });
@@ -80,14 +85,14 @@ public final class WebsocketClientTransport implements ClientTransport {
     return Flux.defer(
         () -> {
           String sid = String.valueOf(sidCounter.incrementAndGet());
-          BinaryWebSocketFrame frame = toRequest(request, sid);
+          ByteBuf byteBuf = toRequest(request, sid);
 
           return getOrConnect()
               .flatMapMany(
                   session ->
                       session
-                          .send(frame)
-                          .thenMany(session.receiveStream(sid))
+                          .send(byteBuf)
+                          .thenMany(receiveStream(session.receive(), sid))
                           .map(this::enrichForRecv)
                           .doOnCancel(() -> handleCancel(sid, session)));
         });
@@ -124,7 +129,7 @@ public final class WebsocketClientTransport implements ClientTransport {
                                 (in, out) -> {
                                   LOGGER.info("Connected successfully to {}", address);
 
-                                  WebsocketSession session = new WebsocketSession(in, out, codec);
+                                  WebsocketSession session = new WebsocketSession(in, out);
 
                                   sink.success(session);
 
@@ -145,14 +150,13 @@ public final class WebsocketClientTransport implements ClientTransport {
   }
 
   private Disposable handleCancel(String sid, WebsocketSession session) {
-    ClientMessage cancelMessage =
-        ClientMessage.builder()
-            .header("sid", sid)
-            .header("sig", Signal.CANCEL.codeAsString())
-            .build();
-    ByteBuf byteBuf = codec.encode(cancelMessage);
-    BinaryWebSocketFrame cancelFrame = new BinaryWebSocketFrame(byteBuf);
-    return session.send(cancelFrame).subscribe();
+    ByteBuf byteBuf =
+        codec.encode(
+            ClientMessage.builder()
+                .header("sid", sid)
+                .header("sig", Signal.CANCEL.codeAsString())
+                .build());
+    return session.send(byteBuf).subscribe();
   }
 
   private ClientMessage enrichForRecv(ClientMessage message) {
@@ -161,12 +165,70 @@ public final class WebsocketClientTransport implements ClientTransport {
         .build();
   }
 
-  private BinaryWebSocketFrame toRequest(ClientMessage message, String sid) {
-    ClientMessage request =
+  private ByteBuf toRequest(ClientMessage message, String sid) {
+    return codec.encode(
         ClientMessage.from(message)
             .header("client-send-time", String.valueOf(System.currentTimeMillis()))
             .header("sid", sid)
-            .build();
-    return new BinaryWebSocketFrame(codec.encode(request));
+            .build());
+  }
+
+  private Flux<ClientMessage> receiveForSid(Flux<ByteBuf> inbound, String sid) {
+    return inbound
+        .publishOn(Schedulers.single(), Integer.MAX_VALUE)
+        .map(codec::decode)
+        .filter(response -> sid.equals(response.header("sid")))
+        .log(">>> SID_RECEIVE", Level.FINE);
+  }
+
+  private Flux<ClientMessage> receiveStream(Flux<ByteBuf> inbound, String sid) {
+    return Flux.create(
+        sink ->
+            receiveForSid(inbound, sid)
+                .subscribe(
+                    response -> handleResponse(response, sink::next, sink::complete, sink::error)));
+  }
+
+  public Mono<ClientMessage> receiveResponse(Flux<ByteBuf> inbound, String sid) {
+    return Mono.create(
+        sink ->
+            receiveForSid(inbound, sid)
+                .subscribe(
+                    response ->
+                        handleResponse(response, sink::success, sink::success, sink::error)));
+  }
+
+  private void handleResponse(
+      ClientMessage response,
+      Consumer<ClientMessage> onNext,
+      Runnable onComplete,
+      Consumer<Throwable> onError) {
+    try {
+      Optional<Signal> signalOptional =
+          Optional.ofNullable(response.header("sig")).map(Signal::from);
+
+      if (signalOptional.isPresent()) {
+        // handle completion signal
+        Signal signal = signalOptional.get();
+        if (signal == Signal.COMPLETE) {
+          onComplete.run();
+        }
+        if (signal == Signal.ERROR) {
+          // decode error data to retrieve real error cause
+          ErrorData errorData = codec.decodeData(response, ErrorData.class).data();
+          Throwable e =
+              ExceptionProcessor.toException(
+                  response.qualifier(), errorData.getErrorCode(), errorData.getErrorMessage());
+          String sid = response.header("sid");
+          LOGGER.error("Received error response: sid={}, error={}", sid, e);
+          onError.accept(e);
+        }
+      } else {
+        // handle normal response
+        onNext.accept(response);
+      }
+    } catch (Exception e) {
+      onError.accept(e);
+    }
   }
 }
--- src/main/java/io/scalecube/gateway/clientsdk/websocket/WebsocketSession.java
+++ src/main/java/io/scalecube/gateway/clientsdk/websocket/WebsocketSession.java
@@ -1,71 +1,46 @@
 package io.scalecube.gateway.clientsdk.websocket;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.CloseWebSocketFrame;
-import io.netty.handler.codec.http.websocketx.WebSocketFrame;
-import io.scalecube.gateway.clientsdk.ClientCodec;
-import io.scalecube.gateway.clientsdk.ClientMessage;
-import io.scalecube.gateway.clientsdk.ErrorData;
-import io.scalecube.gateway.clientsdk.exceptions.ExceptionProcessor;
-import java.util.Optional;
 import java.util.logging.Level;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import reactor.core.publisher.DirectProcessor;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 import reactor.ipc.netty.NettyPipeline.SendOptions;
 import reactor.ipc.netty.http.websocket.WebsocketInbound;
 import reactor.ipc.netty.http.websocket.WebsocketOutbound;
 
 final class WebsocketSession {
 
-  static final Logger LOGGER = LoggerFactory.getLogger(WebsocketSession.class);
-
   private final WebsocketInbound inbound;
   private final WebsocketOutbound outbound;
-  private final ClientCodec<ByteBuf> codec;
 
-  private final DirectProcessor<ClientMessage> inboundProcessor = DirectProcessor.create();
-  private final FluxSink<ClientMessage> inboundSink = inboundProcessor.serialize().sink();
+  private final DirectProcessor<ByteBuf> inboundProcessor = DirectProcessor.create();
+  private final FluxSink<ByteBuf> inboundSink = inboundProcessor.serialize().sink();
 
-  WebsocketSession(
-      WebsocketInbound inbound, WebsocketOutbound outbound, ClientCodec<ByteBuf> codec) {
+  WebsocketSession(WebsocketInbound inbound, WebsocketOutbound outbound) {
     this.inbound = inbound;
     this.outbound = outbound;
-    this.codec = codec;
 
     this.inbound
         .aggregateFrames()
         .receive()
         .map(ByteBuf::retain)
-        .publishOn(Schedulers.single(), Integer.MAX_VALUE) // offload netty thread
-        .map(codec::decode)
         .log(">>> RECEIVE", Level.FINE)
         .subscribe(inboundSink::next, inboundSink::error, inboundSink::complete);
   }
 
-  public Mono<Void> send(WebSocketFrame frame) {
+  public Mono<Void> send(ByteBuf byteBuf) {
     return outbound
         .options(SendOptions::flushOnEach)
-        .sendObject(Mono.just(frame).log("<<< SEND", Level.FINE))
+        .sendObject(Mono.just(new BinaryWebSocketFrame(byteBuf)).log("<<< SEND", Level.FINE))
         .then();
   }
 
-  public Flux<ClientMessage> receiveStream(String sid) {
-    return Flux.create(
-        (FluxSink<ClientMessage> sink) ->
-            inboundProcessor
-                .filter(response -> sid.equals(response.header("sid")))
-                .log(">>> SID_RECEIVE", Level.FINE)
-                .subscribe(response -> handleResponse(sink, response)));
-  }
-
-  public Mono<ClientMessage> receiveResponse(String sid) {
-    return Mono.create(
-        sink -> receiveStream(sid).subscribe(sink::success, sink::error, sink::success));
+  public Flux<ByteBuf> receive() {
+    return inboundProcessor;
   }
 
   public Mono<Void> close() {
@@ -79,39 +54,5 @@ final class WebsocketSession {
 
   public Mono<Void> onClose(Runnable runnable) {
     return inbound.context().onClose(runnable).onClose();
-  }
-
-  private void handleResponse(FluxSink<ClientMessage> sink, ClientMessage response) {
-    try {
-      Optional<Signal> signal = Optional.ofNullable(response.header("sig")).map(Signal::from);
-      if (signal.isPresent()) {
-        handleSignal(sink, response, signal.get());
-      } else {
-        sink.next(response);
-      }
-    } catch (Exception e) {
-      sink.error(e);
-    }
-  }
-
-  private void handleSignal(FluxSink<ClientMessage> sink, ClientMessage response, Signal signal) {
-    if (signal == Signal.COMPLETE) {
-      sink.complete();
-    }
-    if (signal == Signal.ERROR) {
-      Throwable e = toError(response);
-      LOGGER.error(
-          "Received error response on session {}: sid={}, error={}",
-          this,
-          response.header("sid"),
-          e);
-      sink.error(e);
-    }
-  }
-
-  private Throwable toError(ClientMessage response) {
-    ErrorData errorData = codec.decodeData(response, ErrorData.class).data();
-    return ExceptionProcessor.toException(
-        response.qualifier(), errorData.getErrorCode(), errorData.getErrorMessage());
   }
 }
