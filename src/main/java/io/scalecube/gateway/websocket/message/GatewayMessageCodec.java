1028090774690b0a045eb7a1f39936952b16a8a2
artem-v <artem.vysochyn@jivygroup.com>
2018-09-21 14:49:36 +0300

    ++ safe code in GatewayMessageCodec

--- src/main/java/io/scalecube/gateway/websocket/message/GatewayMessageCodec.java
+++ src/main/java/io/scalecube/gateway/websocket/message/GatewayMessageCodec.java
@@ -67,12 +67,15 @@ public class GatewayMessageCodec {
       if (data != null) {
         if (data instanceof ByteBuf) {
           ByteBuf dataBin = (ByteBuf) data;
-          if (dataBin.readableBytes() > 0) {
-            generator.writeFieldName(DATA_FIELD);
-            generator.writeRaw(":");
-            generator.flush();
-            byteBuf.writeBytes(dataBin);
-            ReferenceCountUtil.safestRelease(dataBin);
+          if (dataBin.isReadable()) {
+            try {
+              generator.writeFieldName(DATA_FIELD);
+              generator.writeRaw(":");
+              generator.flush();
+              byteBuf.writeBytes(dataBin);
+            } finally {
+              ReferenceCountUtil.safestRelease(dataBin);
+            }
           }
         } else {
           generator.writeObjectField(DATA_FIELD, data);
