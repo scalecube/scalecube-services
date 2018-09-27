059366fb4a05d81d63810b26a671361477f52b05
artem-v <artem.vysochyn@jivygroup.com>
2018-09-27 14:56:47 +0300

    Done on GatewayWebsocketAcceptor.java; Done on refatrong server side of websocket

--- src/main/java/io/scalecube/gateway/GatewayMetrics.java
+++ src/main/java/io/scalecube/gateway/GatewayMetrics.java
@@ -6,13 +6,15 @@ import io.scalecube.services.metrics.Metrics;
 
 public class GatewayMetrics {
 
-  public static final String METRIC_CONNECTIONS = "connections";
-  public static final String METRIC_REQUESTS = "requests";
-  public static final String METRIC_RESPONSES = "responses";
+  public static final String METRIC_CONNECTION = "connection";
+  public static final String METRIC_REQ = "request";
+  public static final String METRIC_RESP = "response";
+  public static final String METRIC_SERVICE_RESP = "service-response";
 
-  private final Counter connectionCounter;
-  private final Meter requestMeter;
-  private final Meter responseMeter;
+  private final Counter connCounter;
+  private final Meter reqMeter;
+  private final Meter respMeter;
+  private final Meter serviceRespMeter;
 
   /**
    * Constructor for gateway metrics.
@@ -21,36 +23,44 @@ public class GatewayMetrics {
    * @param metrics microservices metrics
    */
   public GatewayMetrics(String prefix, Metrics metrics) {
-    connectionCounter = metrics != null ? metrics.getCounter(prefix, METRIC_CONNECTIONS) : null;
-    requestMeter = metrics != null ? metrics.getMeter(prefix, "", METRIC_REQUESTS) : null;
-    responseMeter = metrics != null ? metrics.getMeter(prefix, "", METRIC_RESPONSES) : null;
+    connCounter = metrics != null ? metrics.getCounter(prefix, METRIC_CONNECTION) : null;
+    reqMeter = metrics != null ? metrics.getMeter(prefix, "", METRIC_REQ) : null;
+    respMeter = metrics != null ? metrics.getMeter(prefix, "", METRIC_RESP) : null;
+    serviceRespMeter = metrics != null ? metrics.getMeter(prefix, "", METRIC_SERVICE_RESP) : null;
   }
 
   /** Increment connection counter. */
   public void incConnection() {
-    if (connectionCounter != null) {
-      connectionCounter.inc();
+    if (connCounter != null) {
+      connCounter.inc();
     }
   }
 
   /** Decrement connection counter. */
   public void decConnection() {
-    if (connectionCounter != null) {
-      connectionCounter.dec();
+    if (connCounter != null) {
+      connCounter.dec();
     }
   }
 
   /** Mark request for calls/sec measurement. */
   public void markRequest() {
-    if (requestMeter != null) {
-      requestMeter.mark();
+    if (reqMeter != null) {
+      reqMeter.mark();
     }
   }
 
   /** Mark response for calls/sec measurement. */
   public void markResponse() {
-    if (responseMeter != null) {
-      responseMeter.mark();
+    if (respMeter != null) {
+      respMeter.mark();
+    }
+  }
+
+  /** Mark service response for calls/sec measurement. */
+  public void markServiceResponse() {
+    if (serviceRespMeter != null) {
+      serviceRespMeter.mark();
     }
   }
 }
