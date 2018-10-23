4f407736ac7dc771be1abe990102e9a4f1db5634
artem-v <artem.vysochyn@jivygroup.com>
2018-10-23 17:07:37 +0300

    Updated test sscenarios

--- benchmarks/src/main/java/io/scalecube/services/benchmarks/service/BenchmarkServiceImpl.java
+++ benchmarks/src/main/java/io/scalecube/services/benchmarks/service/BenchmarkServiceImpl.java
@@ -10,7 +10,7 @@ public class BenchmarkServiceImpl implements BenchmarkService {
 
   @Override
   public Mono<Void> requestVoid(ServiceMessage request) {
-    return Mono.<Void>empty().subscribeOn(Schedulers.immediate());
+    return Mono.empty();
   }
 
   @Override
@@ -23,7 +23,7 @@ public class BenchmarkServiceImpl implements BenchmarkService {
               .header(SERVICE_SEND_TIME, value)
               .build();
         };
-    return Mono.fromCallable(callable).subscribeOn(Schedulers.immediate());
+    return Mono.fromCallable(callable);
   }
 
   @Override
