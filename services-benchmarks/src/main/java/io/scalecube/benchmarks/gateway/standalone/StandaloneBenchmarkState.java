4b35d72a82999cbe28cb8be5c27672aac4dfcaa1
Artem Vysochyn <artem.vysochyn@gmail.com>
2018-11-07 12:12:24 +0200

    Renamed modules

---
+++ services-benchmarks/src/main/java/io/scalecube/benchmarks/gateway/standalone/StandaloneBenchmarkState.java
@@ -0,0 +1,61 @@
+package io.scalecube.benchmarks.gateway.standalone;
+
+import io.scalecube.benchmarks.BenchmarkSettings;
+import io.scalecube.benchmarks.gateway.AbstractBenchmarkState;
+import io.scalecube.gateway.benchmarks.BenchmarksServiceImpl;
+import io.scalecube.gateway.clientsdk.Client;
+import io.scalecube.gateway.http.HttpGateway;
+import io.scalecube.gateway.rsocket.websocket.RSocketGateway;
+import io.scalecube.gateway.websocket.WebsocketGateway;
+import io.scalecube.services.Microservices;
+import io.scalecube.services.gateway.GatewayConfig;
+import java.net.InetSocketAddress;
+import java.util.function.BiFunction;
+import reactor.core.publisher.Mono;
+import reactor.netty.resources.LoopResources;
+
+public class StandaloneBenchmarkState extends AbstractBenchmarkState<StandaloneBenchmarkState> {
+
+  private final String gatewayName;
+
+  private Microservices microservices;
+
+  public StandaloneBenchmarkState(
+      BenchmarkSettings settings,
+      String gatewayName,
+      BiFunction<InetSocketAddress, LoopResources, Client> clientBuilder) {
+    super(settings, clientBuilder);
+    this.gatewayName = gatewayName;
+  }
+
+  @Override
+  protected void beforeAll() throws Exception {
+    super.beforeAll();
+
+    microservices =
+        Microservices.builder()
+            .services(new BenchmarksServiceImpl())
+            .gateway(GatewayConfig.builder("rsws", RSocketGateway.class).build())
+            .gateway(GatewayConfig.builder("ws", WebsocketGateway.class).build())
+            .gateway(GatewayConfig.builder("http", HttpGateway.class).build())
+            .metrics(registry())
+            .startAwait();
+  }
+
+  @Override
+  protected void afterAll() throws Exception {
+    super.afterAll();
+    if (microservices != null) {
+      microservices.shutdown().block();
+    }
+  }
+
+  /**
+   * Factory function for {@link Client}.
+   *
+   * @return client
+   */
+  public Mono<Client> createClient() {
+    return createClient(microservices, gatewayName, clientBuilder);
+  }
+}
