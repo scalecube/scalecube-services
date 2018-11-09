b9df7264b40b21102ea72b5aac8b0d0fbc797c56
Serhii Kolomiiets <serhii.kolomiiets@jivygroup.com>
2018-11-09 16:55:12 +0200

    Fix: deploy script

--- src/main/scripts/cd/push_docker_image.sh
+++ src/main/scripts/cd/push_docker_image.sh
@@ -2,5 +2,5 @@
 pip install --user -r requirements.txt
 label_exist=$(python scripts/check_pull_request_label.py)
 if [ ! "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$label_exist" == "exist" ]; then
-    mvn clean install
+    mvn clean install -DskipTests
 fi
