ef5ba305e2eb8994d3613e2c045f7f2402917827
Serhii Kolomiiets <serhii.kolomiiets@jivygroup.com>
2018-11-09 17:57:53 +0200

    Feature: docker push images

--- src/main/scripts/cd/push_docker_image.sh
+++ src/main/scripts/cd/push_docker_image.sh
@@ -3,4 +3,12 @@ pip install --user -r requirements.txt
 label_exist=$(python scripts/check_pull_request_label.py)
 if [ ! "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$label_exist" == "exist" ]; then
     mvn clean install -DskipTests
+    LABEL=$(echo $(git rev-parse HEAD) | cut -c1-7)
+    docker tag scalecube/scalecube-services-benchmarks:latest scalecube/scalecube-services-benchmarks:$LABEL
+    docker tag scalecube/scalecube-services-examples-runner:latest scalecube/scalecube-services-examples-runner:$LABEL
+    docker tag scalecube/scalecube-services-gateway-runner:latest scalecube/scalecube-services-gateway-runner:$LABEL
+    echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
+    docker push scalecube/scalecube-services-benchmarks:$LABEL
+    docker push scalecube/scalecube-services-examples-runner:$LABEL
+    docker push scalecube/scalecube-services-gateway-runner:$LABEL 
 fi
