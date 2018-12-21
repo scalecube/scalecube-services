209348ac4c3c06bb9a199960e918f81335dac9c2
artem-v <artem.vysochyn@gmail.com>
2018-12-22 01:07:31 +0200

    Updated RSocketAeronServiceTransport

--- services-transport-rsocket-aeron/src/main/java/io/scalecube/services/transport/rsocket/aeron/RSocketAeronServiceTransport.java
+++ services-transport-rsocket-aeron/src/main/java/io/scalecube/services/transport/rsocket/aeron/RSocketAeronServiceTransport.java
@@ -1,77 +1,69 @@
 package io.scalecube.services.transport.rsocket.aeron;
 
-import io.netty.util.concurrent.DefaultThreadFactory;
 import io.scalecube.services.transport.api.ClientTransport;
 import io.scalecube.services.transport.api.HeadersCodec;
 import io.scalecube.services.transport.api.ServerTransport;
 import io.scalecube.services.transport.api.ServiceMessageCodec;
 import io.scalecube.services.transport.api.ServiceTransport;
+import java.util.Optional;
 import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import reactor.aeron.AeronResources;
+import reactor.aeron.AeronResourcesConfig;
 import reactor.core.publisher.Mono;
 
 public class RSocketAeronServiceTransport implements ServiceTransport {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(RSocketAeronServiceTransport.class);
-
   private static final String DEFAULT_HEADERS_FORMAT = "application/json";
 
   @Override
-  public ClientTransport getClientTransport(Executor aeronRecourcesHolder) {
-    AeronResources aeronResources = ((AeronRecourcesHolder) aeronRecourcesHolder).aeronResources;
+  public ServiceTransport.Resources resources(int numOfWorkers) {
+    return null;
+  }
+
+  @Override
+  public ClientTransport clientTransport(ServiceTransport.Resources resources) {
     return new RSocketAeronClientTransport(
-        new ServiceMessageCodec(HeadersCodec.getInstance(DEFAULT_HEADERS_FORMAT)), aeronResources);
+        new ServiceMessageCodec(HeadersCodec.getInstance(DEFAULT_HEADERS_FORMAT)),
+        ((Resources) resources).aeronResources);
   }
 
   @Override
-  public ServerTransport getServerTransport(Executor aeronRecourcesHolder) {
-    AeronResources aeronResources = ((AeronRecourcesHolder) aeronRecourcesHolder).aeronResources;
+  public ServerTransport serverTransport(ServiceTransport.Resources resources) {
     return new RSocketAeronServerTransport(
-        new ServiceMessageCodec(HeadersCodec.getInstance(DEFAULT_HEADERS_FORMAT)), aeronResources);
+        new ServiceMessageCodec(HeadersCodec.getInstance(DEFAULT_HEADERS_FORMAT)),
+        ((Resources) resources).aeronResources);
   }
 
-  @Override
-  public Executor getWorkerThreadPool(int numOfThreads) {
-    return new AeronRecourcesHolder(numOfThreads);
-  }
+  /**
+   * Aeron RSocket service transport Resources implementation. Contains internally {@link
+   * AeronResources} instance.
+   */
+  private static class Resources implements ServiceTransport.Resources {
 
-  @Override
-  public Mono shutdown(Executor aeronRecourcesHolder) {
-    return Mono.fromRunnable(
-        () -> {
-          if (aeronRecourcesHolder != null) {
-            ((AeronRecourcesHolder) aeronRecourcesHolder).shutdown();
-          }
-        });
-  }
-
-  private static class AeronRecourcesHolder implements Executor {
-
-    private static final ThreadFactory WORKER_THREAD_FACTORY =
-        new DefaultThreadFactory("rsocket-aeron-worker", true);
-
-    private final ExecutorService workerThreadPool;
     private final AeronResources aeronResources;
 
-    private AeronRecourcesHolder(int numOfThreads) {
-      this.workerThreadPool = Executors.newFixedThreadPool(numOfThreads, WORKER_THREAD_FACTORY);
-      this.aeronResources = AeronResources.start();
+    private Resources(int numOfWorkers) {
+      aeronResources =
+          AeronResources.start(
+              AeronResourcesConfig //
+                  .builder()
+                  .numOfWorkers(numOfWorkers)
+                  .build());
     }
 
     @Override
-    public void execute(Runnable command) {
-      workerThreadPool.execute(command);
+    public Optional<Executor> workerPool() {
+      // worker pool is not exposed in aeron
+      return Optional.empty();
     }
 
-    private void shutdown() {
-      workerThreadPool.shutdown();
-      aeronResources.dispose();
-      aeronResources.onDispose().block();
+    @Override
+    public Mono<Void> shutdown() {
+      return Mono.defer(
+          () -> {
+            aeronResources.dispose();
+            return aeronResources.onDispose();
+          });
     }
   }
 }
