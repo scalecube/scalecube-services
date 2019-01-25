7471d5f83cc9e6198127099b3f5ecaf90c25e4db
ronen <ronen.hamias@gmail.com>
2019-01-25 14:48:48 +0200

    style

--- services/src/main/java/io/scalecube/services/ServiceCall.java
+++ services/src/main/java/io/scalecube/services/ServiceCall.java
@@ -1,6 +1,7 @@
 package io.scalecube.services;
 
 import static java.util.Objects.requireNonNull;
+
 import io.scalecube.services.api.ErrorData;
 import io.scalecube.services.api.ServiceMessage;
 import io.scalecube.services.exceptions.DefaultErrorMapper;
