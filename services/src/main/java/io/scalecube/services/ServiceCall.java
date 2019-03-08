74d70be89aadc23882f6b67b9f153dbf7a01f765
ronen <ronen.hamias@gmail.com>
2019-03-08 13:53:34 +0200

    check style

--- services/src/main/java/io/scalecube/services/ServiceCall.java
+++ services/src/main/java/io/scalecube/services/ServiceCall.java
@@ -46,12 +46,12 @@ public class ServiceCall {
   /**
    * Creates new {@link ServiceCall}'s definition.
    *
-   * @param transport - transport to be used by {@link ServiceCall} that is created form this
-   *     {@link Call}
+   * @param transport - transport to be used by {@link ServiceCall} that is created form this {@link
+   *     Call}
    * @param methodRegistry - methodRegistry to be used by {@link ServiceCall} that is created form
    *     this {@link Call}
-   * @param serviceRegistry - serviceRegistry to be used by {@link ServiceCall} that is created
-   *     form this {@link Call}
+   * @param serviceRegistry - serviceRegistry to be used by {@link ServiceCall} that is created form
+   *     this {@link Call}
    */
   public ServiceCall(
       ClientTransport transport,
@@ -61,7 +61,7 @@ public class ServiceCall {
     this.serviceRegistry = serviceRegistry;
     this.methodRegistry = methodRegistry;
   }
-  
+
   private ServiceCall(ServiceCall other) {
     this.transport = other.transport;
     this.methodRegistry = other.methodRegistry;
@@ -70,24 +70,42 @@ public class ServiceCall {
     this.errorMapper = other.errorMapper;
   }
 
+  /**
+   * Creates new {@link ServiceCall}'s definition with a given router.
+   *
+   * @param routerType given class of the router.
+   * @return new {@link ServiceCall} instance.
+   */
   public ServiceCall router(Class<? extends Router> routerType) {
     ServiceCall target = new ServiceCall(this);
     target.router = Routers.getRouter(routerType);
     return target;
   }
 
+  /**
+   * Creates new {@link ServiceCall}'s definition with a given router.
+   *
+   * @param router given.
+   * @return new {@link ServiceCall} instance.
+   */
   public ServiceCall router(Router router) {
     ServiceCall target = new ServiceCall(this);
     target.router = router;
     return target;
   }
 
+  /**
+   * Creates new {@link ServiceCall}'s definition with a given error mapper.
+   *
+   * @param errorMapper given.
+   * @return new {@link ServiceCall} instance.
+   */
   public ServiceCall errorMapper(ServiceClientErrorMapper errorMapper) {
     ServiceCall target = new ServiceCall(this);
     target.errorMapper = errorMapper;
     return target;
   }
-  
+
   /**
    * Issues fire-and-forget request.
    *
