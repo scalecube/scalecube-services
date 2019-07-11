98eb543efb827ec0afa068c24e5b2216696574b4
Dmytro Lazebnyi <d.lazebny@gmail.com>
2019-07-11 09:46:17 +0300

    Add `ForbiddenException`

---
+++ services-api/src/main/java/io/scalecube/services/exceptions/ForbiddenException.java
@@ -0,0 +1,14 @@
+package io.scalecube.services.exceptions;
+
+public class ForbiddenException extends ServiceException {
+
+  public static final int ERROR_TYPE = 403;
+
+  public ForbiddenException(int errorCode, String message) {
+    super(errorCode, message);
+  }
+
+  public ForbiddenException(String message) {
+    super(ERROR_TYPE, message);
+  }
+}
--- services/src/test/java/io/scalecube/services/sut/security/SecuredServiceImpl.java
+++ services/src/test/java/io/scalecube/services/sut/security/SecuredServiceImpl.java
@@ -1,6 +1,6 @@
 package io.scalecube.services.sut.security;
 
-import io.scalecube.services.exceptions.UnauthorizedException;
+import io.scalecube.services.exceptions.ForbiddenException;
 import reactor.core.publisher.Mono;
 
 public class SecuredServiceImpl implements SecuredService {
@@ -12,19 +12,19 @@ public class SecuredServiceImpl implements SecuredService {
 
   @Override
   public Mono<String> helloWithPrincipal(UserProfile user) {
-    authorize(user);
+    checkPrincipal(user);
     return Mono.just("Hello, " + user.name());
   }
 
   @Override
   public Mono<String> helloWithRequestAndPrincipal(String name, UserProfile user) {
-    authorize(user);
+    checkPrincipal(user);
     return Mono.just("Hello, " + name + " and " + user.name());
   }
 
-  private void authorize(UserProfile user) {
+  private void checkPrincipal(UserProfile user) {
     if (!user.role().equals("ADMIN")) {
-      throw new UnauthorizedException("Unathorized");
+      throw new ForbiddenException("Forbidden");
     }
   }
 }
