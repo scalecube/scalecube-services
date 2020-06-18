b929b8310b057c0e0ae8e85e0395863e4cd79cbc
Artem Vysochyn <artem.vysochyn@gmail.com>
2020-06-18 19:04:49 +0300

    Fixed discovery contexts

--- services-api/src/main/java/io/scalecube/services/discovery/api/ServiceDiscoveryContext.java
+++ services-api/src/main/java/io/scalecube/services/discovery/api/ServiceDiscoveryContext.java
@@ -1,36 +1,84 @@
 package io.scalecube.services.discovery.api;
 
 import io.scalecube.net.Address;
+import io.scalecube.services.discovery.api.ServiceDiscoveryEvent.Type;
+import io.scalecube.services.registry.api.ServiceRegistry;
 import java.util.Objects;
 import java.util.StringJoiner;
 import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Scheduler;
 
 public final class ServiceDiscoveryContext {
 
   private final String id;
   private final Address address;
   private final ServiceDiscovery discovery;
+  private final ServiceRegistry serviceRegistry;
+  private final Scheduler scheduler;
 
   private ServiceDiscoveryContext(Builder builder) {
-    this.id = Objects.requireNonNull(builder.id, "discoveryContext.id");
-    this.address = Objects.requireNonNull(builder.address, "discoveryContext.address");
-    this.discovery = Objects.requireNonNull(builder.discovery, "discoveryContext.discovery");
+    this.id = Objects.requireNonNull(builder.id, "id");
+    this.address = Objects.requireNonNull(builder.address, "address");
+    this.discovery = Objects.requireNonNull(builder.discovery, "discovery");
+    this.serviceRegistry = Objects.requireNonNull(builder.serviceRegistry, "serviceRegistry");
+    this.scheduler = Objects.requireNonNull(builder.scheduler, "scheduler");
   }
 
   public static Builder builder() {
     return new Builder();
   }
 
+  /**
+   * Returns copy builder.
+   *
+   * @param other instance to copy from
+   * @return new builder instance
+   */
+  public static Builder from(ServiceDiscoveryContext other) {
+    return new Builder()
+        .id(other.id)
+        .address(other.address)
+        .discovery(other.discovery)
+        .serviceRegistry(other.serviceRegistry)
+        .scheduler(other.scheduler);
+  }
+
+  /**
+   * Returns service discovery id.
+   *
+   * @return service discovery id
+   */
   public String id() {
     return id;
   }
 
+  /**
+   * Returns service discovery address. If service discovery instance is not yet started, then this
+   * method will return {@link Address#NULL_ADDRESS}.
+   *
+   * @see ServiceDiscovery#start()
+   * @see ServiceDiscovery#listen()
+   * @return service discovery address
+   */
   public Address address() {
     return address;
   }
 
+  /**
+   * Returns stream of service discovery events. Can be called before or after {@link
+   * ServiceDiscovery#start()}. If it's called before then new events will be streamed, if it's
+   * called after then {@link ServiceRegistry#listServiceEndpoints()} will be turned to service
+   * discovery events of type {@link Type#ENDPOINT_ADDED}, and concateneted with a stream of live
+   * events.
+   *
+   * @return stream of service discovery events
+   */
   public Flux<ServiceDiscoveryEvent> listen() {
-    return discovery.listen();
+    return Flux.fromStream(serviceRegistry.listServiceEndpoints().stream())
+        .map(ServiceDiscoveryEvent::newEndpointAdded)
+        .concatWith(discovery.listen())
+        .subscribeOn(scheduler)
+        .publishOn(scheduler);
   }
 
   @Override
@@ -39,6 +87,8 @@ public final class ServiceDiscoveryContext {
         .add("id='" + id + "'")
         .add("address=" + address)
         .add("discovery=" + discovery)
+        .add("serviceRegistry=" + serviceRegistry)
+        .add("scheduler=" + scheduler)
         .toString();
   }
 
@@ -47,6 +97,8 @@ public final class ServiceDiscoveryContext {
     private String id;
     private Address address;
     private ServiceDiscovery discovery;
+    private ServiceRegistry serviceRegistry;
+    private Scheduler scheduler;
 
     private Builder() {}
 
@@ -62,6 +114,16 @@ public final class ServiceDiscoveryContext {
 
     public Builder discovery(ServiceDiscovery discovery) {
       this.discovery = discovery;
+      return this;
+    }
+
+    public Builder serviceRegistry(ServiceRegistry serviceRegistry) {
+      this.serviceRegistry = serviceRegistry;
+      return this;
+    }
+
+    public Builder scheduler(Scheduler scheduler) {
+      this.scheduler = scheduler;
       return this;
     }
 
