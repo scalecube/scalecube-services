9debadb43aa948250116325522dcee73d8bbc904
Joseph Tarbit <joetarbit@gmail.com>
2021-03-03 11:43:58 +0000

    Bump RSocket to 1.1.0

--- services-transport-parent/services-transport-rsocket/src/main/java/io/scalecube/services/transport/rsocket/DelegatedLoopResources.java
+++ services-transport-parent/services-transport-rsocket/src/main/java/io/scalecube/services/transport/rsocket/DelegatedLoopResources.java
@@ -1,21 +1,12 @@
 package io.scalecube.services.transport.rsocket;
 
-import io.netty.channel.Channel;
+import java.util.concurrent.atomic.AtomicBoolean;
 import io.netty.channel.EventLoopGroup;
-import io.netty.channel.ServerChannel;
 import io.netty.channel.epoll.Epoll;
-import io.netty.channel.epoll.EpollDatagramChannel;
 import io.netty.channel.epoll.EpollEventLoopGroup;
-import io.netty.channel.epoll.EpollServerSocketChannel;
-import io.netty.channel.epoll.EpollSocketChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.DatagramChannel;
-import io.netty.channel.socket.nio.NioDatagramChannel;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.util.concurrent.DefaultThreadFactory;
 import io.netty.util.concurrent.Future;
-import java.util.concurrent.atomic.AtomicBoolean;
 import reactor.core.publisher.Mono;
 import reactor.netty.FutureMono;
 import reactor.netty.resources.LoopResources;
@@ -77,26 +68,6 @@ public class DelegatedLoopResources implements LoopResources {
   @Override
   public EventLoopGroup onServerSelect(boolean useNative) {
     return bossGroup;
-  }
-
-  @Override
-  public Class<? extends Channel> onChannel(EventLoopGroup group) {
-    return Epoll.isAvailable() ? EpollSocketChannel.class : NioSocketChannel.class;
-  }
-
-  @Override
-  public Class<? extends DatagramChannel> onDatagramChannel(EventLoopGroup group) {
-    return Epoll.isAvailable() ? EpollDatagramChannel.class : NioDatagramChannel.class;
-  }
-
-  @Override
-  public Class<? extends ServerChannel> onServerChannel(EventLoopGroup group) {
-    return Epoll.isAvailable() ? EpollServerSocketChannel.class : NioServerSocketChannel.class;
-  }
-
-  @Override
-  public boolean preferNative() {
-    return Epoll.isAvailable();
   }
 
   @Override
