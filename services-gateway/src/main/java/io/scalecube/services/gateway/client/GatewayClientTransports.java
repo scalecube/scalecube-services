47012f04b9b6ed734ec7d2357c043e30315d3126
Artem Vysochyn <artem.vysochyn@gmail.com>
2024-09-29 22:18:16 +0300

    WIP

--- services-gateway/src/main/java/io/scalecube/services/gateway/client/GatewayClientTransports.java
+++ services-gateway/src/main/java/io/scalecube/services/gateway/client/GatewayClientTransports.java
@@ -10,13 +10,8 @@ import java.util.function.Function;
 
 public class GatewayClientTransports {
 
-  private static final String CONTENT_TYPE = "application/json";
-
   public static final WebsocketGatewayClientCodec WEBSOCKET_CLIENT_CODEC =
       new WebsocketGatewayClientCodec();
-
-  public static final HttpGatewayClientCodec HTTP_CLIENT_CODEC =
-      new HttpGatewayClientCodec(DataCodec.getInstance(CONTENT_TYPE));
 
   private GatewayClientTransports() {
     // utils
@@ -31,18 +26,6 @@ public class GatewayClientTransports {
   public static ClientTransport websocketGatewayClientTransport(GatewayClientSettings cs) {
     final Function<GatewayClientSettings, GatewayClient> function =
         settings -> new WebsocketGatewayClient(settings, WEBSOCKET_CLIENT_CODEC);
-    return new GatewayClientTransport(function.apply(cs));
-  }
-
-  /**
-   * ClientTransport that is capable of communicating with Gateway over http.
-   *
-   * @param cs client settings for gateway client transport
-   * @return client transport
-   */
-  public static ClientTransport httpGatewayClientTransport(GatewayClientSettings cs) {
-    final Function<GatewayClientSettings, GatewayClient> function =
-        settings -> new HttpGatewayClient(settings, HTTP_CLIENT_CODEC);
     return new GatewayClientTransport(function.apply(cs));
   }
 }
