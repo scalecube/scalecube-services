9532032507b6a4f21520e9735302ec16e9bd674a
Artem Vysochyn <artem.vysochyn@gmail.com>
2024-09-30 10:01:20 +0300

    WIP

--- services-gateway/src/test/java/io/scalecube/services/gateway/websocket/WebsocketLocalWithAuthExtension.java
+++ services-gateway/src/test/java/io/scalecube/services/gateway/websocket/WebsocketLocalWithAuthExtension.java
@@ -1,27 +1,27 @@
-package io.scalecube.services.gateway.websocket;
-
-import io.scalecube.services.ServiceInfo;
-import io.scalecube.services.gateway.AbstractLocalGatewayExtension;
-import io.scalecube.services.gateway.AuthRegistry;
-import io.scalecube.services.gateway.GatewaySessionHandlerImpl;
-import io.scalecube.services.gateway.client.GatewayClientTransports;
-
-public class WebsocketLocalWithAuthExtension extends AbstractLocalGatewayExtension {
-
-  private static final String GATEWAY_ALIAS_NAME = "ws";
-
-  WebsocketLocalWithAuthExtension(Object serviceInstance, AuthRegistry authReg) {
-    this(ServiceInfo.fromServiceInstance(serviceInstance).build(), authReg);
-  }
-
-  WebsocketLocalWithAuthExtension(ServiceInfo serviceInfo, AuthRegistry authReg) {
-    super(
-        serviceInfo,
-        opts ->
-            new WebsocketGateway.Builder()
-                .options(opts.id(GATEWAY_ALIAS_NAME))
-                .gatewayHandler(new GatewaySessionHandlerImpl(authReg))
-                .build(),
-        GatewayClientTransports::websocketGatewayClientTransport);
-  }
-}
+//package io.scalecube.services.gateway.websocket;
+//
+//import io.scalecube.services.ServiceInfo;
+//import io.scalecube.services.gateway.AbstractLocalGatewayExtension;
+//import io.scalecube.services.gateway.AuthRegistry;
+//import io.scalecube.services.gateway.GatewaySessionHandlerImpl;
+//import io.scalecube.services.gateway.client.GatewayClientTransports;
+//
+//public class WebsocketLocalWithAuthExtension extends AbstractLocalGatewayExtension {
+//
+//  private static final String GATEWAY_ALIAS_NAME = "ws";
+//
+//  WebsocketLocalWithAuthExtension(Object serviceInstance, AuthRegistry authReg) {
+//    this(ServiceInfo.fromServiceInstance(serviceInstance).build(), authReg);
+//  }
+//
+//  WebsocketLocalWithAuthExtension(ServiceInfo serviceInfo, AuthRegistry authReg) {
+//    super(
+//        serviceInfo,
+//        opts ->
+//            new WebsocketGateway.Builder()
+//                .options(opts.id(GATEWAY_ALIAS_NAME))
+//                .gatewayHandler(new GatewaySessionHandlerImpl(authReg))
+//                .build(),
+//        GatewayClientTransports::websocketGatewayClientTransport);
+//  }
+//}
