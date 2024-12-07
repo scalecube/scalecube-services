d1b3159085374d85fdbb3eeef052c5c6f3fa02e7
Artem Vysochyn <artem.vysochyn@gmail.com>
2024-12-07 18:01:44 +0200

    WIP

--- services/src/main/java/io/scalecube/services/registry/ServiceRegistryImpl.java
+++ services/src/main/java/io/scalecube/services/registry/ServiceRegistryImpl.java
@@ -16,10 +16,8 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import org.jctools.maps.NonBlockingHashMap;
 import reactor.core.scheduler.Scheduler;
@@ -34,9 +32,13 @@ public class ServiceRegistryImpl implements ServiceRegistry {
   private final Map<String, ServiceEndpoint> serviceEndpoints = new NonBlockingHashMap<>();
   private final Map<String, List<ServiceReference>> serviceReferencesByQualifier =
       new NonBlockingHashMap<>();
+  private final Map<String, ServiceMethodInvoker> methodInvokerByQualifier =
+      new NonBlockingHashMap<>();
+  private final Map<Pattern, List<ServiceReference>> serviceReferencesByPattern =
+      new NonBlockingHashMap<>();
+  private final Map<Pattern, ServiceMethodInvoker> methodInvokerByPattern =
+      new NonBlockingHashMap<>();
   private final List<ServiceInfo> serviceInfos = new CopyOnWriteArrayList<>();
-  private final ConcurrentMap<String, ServiceMethodInvoker> methodInvokers =
-      new ConcurrentHashMap<>();
 
   public ServiceRegistryImpl(Map<String, Scheduler> schedulers) {
     this.schedulers = schedulers;
@@ -69,12 +71,13 @@ public class ServiceRegistryImpl implements ServiceRegistry {
 
   @Override
   public boolean registerService(ServiceEndpoint serviceEndpoint) {
-    boolean success = serviceEndpoints.putIfAbsent(serviceEndpoint.id(), serviceEndpoint) == null;
-    if (success) {
+    boolean putIfAbsent =
+        serviceEndpoints.putIfAbsent(serviceEndpoint.id(), serviceEndpoint) == null;
+    if (putIfAbsent) {
       LOGGER.log(Level.DEBUG, "ServiceEndpoint registered: {0}", serviceEndpoint);
-      serviceEndpoint.serviceReferences().forEach(this::populateServiceReferences);
+      serviceEndpoint.serviceReferences().forEach(this::addServiceReference);
     }
-    return success;
+    return putIfAbsent;
   }
 
   @Override
@@ -83,13 +86,10 @@ public class ServiceRegistryImpl implements ServiceRegistry {
     if (serviceEndpoint != null) {
       LOGGER.log(Level.DEBUG, "ServiceEndpoint unregistered: {0}", serviceEndpoint);
 
-      List<ServiceReference> serviceReferencesOfEndpoint =
-          serviceReferencesByQualifier.values().stream()
-              .flatMap(Collection::stream)
-              .filter(sr -> sr.endpointId().equals(endpointId))
-              .toList();
-
-      serviceReferencesOfEndpoint.forEach(this::cleanServiceReferences);
+      serviceReferencesByQualifier.values().stream()
+          .flatMap(Collection::stream)
+          .filter(sr -> sr.endpointId().equals(endpointId))
+          .forEach(this::removeServiceReference);
     }
     return serviceEndpoint;
   }
@@ -147,12 +147,12 @@ public class ServiceRegistryImpl implements ServiceRegistry {
                                   serviceInfo.logger(),
                                   serviceInfo.level());
 
-                          methodInvokers.put(methodInfo.qualifier(), methodInvoker);
+                          methodInvokerByQualifier.put(methodInfo.qualifier(), methodInvoker);
                         }));
   }
 
   private void checkMethodInvokerIsNotPresent(MethodInfo methodInfo) {
-    if (methodInvokers.containsKey(methodInfo.qualifier())) {
+    if (methodInvokerByQualifier.containsKey(methodInfo.qualifier())) {
       LOGGER.log(Level.ERROR, "MethodInvoker already exists, methodInfo: {0}", methodInfo);
       throw new IllegalStateException("MethodInvoker already exists");
     }
@@ -160,7 +160,7 @@ public class ServiceRegistryImpl implements ServiceRegistry {
 
   @Override
   public ServiceMethodInvoker getInvoker(String qualifier) {
-    return methodInvokers.get(Objects.requireNonNull(qualifier, "[getInvoker] qualifier"));
+    return methodInvokerByQualifier.get(qualifier);
   }
 
   @Override
@@ -168,13 +168,13 @@ public class ServiceRegistryImpl implements ServiceRegistry {
     return serviceInfos;
   }
 
-  private void populateServiceReferences(ServiceReference sr) {
+  private void addServiceReference(ServiceReference sr) {
     serviceReferencesByQualifier
         .computeIfAbsent(sr.qualifier(), key -> new CopyOnWriteArrayList<>())
         .add(sr);
   }
 
-  private void cleanServiceReferences(ServiceReference sr) {
+  private void removeServiceReference(ServiceReference sr) {
     serviceReferencesByQualifier.compute(
         sr.qualifier(),
         (key, list) -> {
@@ -182,7 +182,7 @@ public class ServiceRegistryImpl implements ServiceRegistry {
             return null;
           }
           list.remove(sr);
-          return !list.isEmpty() ? list : null;
+          return list.isEmpty() ? null : list;
         });
   }
 }
