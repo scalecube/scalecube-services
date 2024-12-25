9cb12faefa19a8cad4f729b6426a795599000a83
Artem Vysochyn <artem.vysochyn@gmail.com>
2024-12-25 22:45:26 +0200

    WIP

--- services-gateway/src/main/java/io/scalecube/services/gateway/client/http/HttpGatewayClientTransport.java
+++ services-gateway/src/main/java/io/scalecube/services/gateway/client/http/HttpGatewayClientTransport.java
@@ -48,13 +48,13 @@ public final class HttpGatewayClientTransport implements ClientChannel, ClientTr
   private final AtomicReference<HttpClient> httpClientReference = new AtomicReference<>();
 
   private HttpGatewayClientTransport(Builder builder) {
-    this.clientCodec = builder.clientCodec;
-    this.operator = builder.operator;
-    this.loopResources =
+    clientCodec = builder.clientCodec;
+    operator = builder.operator;
+    loopResources =
         builder.loopResources == null
             ? LoopResources.create("http-gateway-client", 1, true)
             : builder.loopResources;
-    this.ownsLoopResources = builder.loopResources == null;
+    ownsLoopResources = builder.loopResources == null;
   }
 
   @Override
--- services-gateway/src/main/java/io/scalecube/services/gateway/client/websocket/WebsocketGatewayClientTransport.java
+++ services-gateway/src/main/java/io/scalecube/services/gateway/client/websocket/WebsocketGatewayClientTransport.java
@@ -49,14 +49,14 @@ public final class WebsocketGatewayClientTransport implements ClientChannel, Cli
       new AtomicReference<>();
 
   private WebsocketGatewayClientTransport(Builder builder) {
-    this.clientCodec = builder.clientCodec;
-    this.keepAliveInterval = builder.keepAliveInterval;
-    this.operator = builder.operator;
-    this.loopResources =
+    clientCodec = builder.clientCodec;
+    keepAliveInterval = builder.keepAliveInterval;
+    operator = builder.operator;
+    loopResources =
         builder.loopResources == null
             ? LoopResources.create("websocket-gateway-client", 1, true)
             : builder.loopResources;
-    this.ownsLoopResources = builder.loopResources == null;
+    ownsLoopResources = builder.loopResources == null;
   }
 
   @Override
