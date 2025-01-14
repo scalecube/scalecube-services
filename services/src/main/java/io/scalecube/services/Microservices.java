41981af081913e11251dc0039715a31a4ae786f7
Artem Vysochyn <artem.vysochyn@gmail.com>
2025-01-14 15:10:39 +0200

    WIP

--- services/src/main/java/io/scalecube/services/Microservices.java
+++ services/src/main/java/io/scalecube/services/Microservices.java
@@ -1,5 +1,6 @@
 package io.scalecube.services;
 
+import static io.scalecube.services.ServiceScanner.scanServiceInfo;
 import static reactor.core.publisher.Sinks.EmitFailureHandler.busyLooping;
 
 import io.scalecube.services.auth.Authenticator;
@@ -202,9 +203,7 @@ public class Microservices implements AutoCloseable {
         context.serviceProviders.stream()
             .flatMap(serviceProvider -> serviceProvider.provide(serviceCall).stream())
             .peek(this::registerService)
-            .peek(
-                serviceInfo ->
-                    builder.appendServiceRegistrations(ServiceScanner.scanServiceInfo(serviceInfo)))
+            .peek(serviceInfo -> builder.appendServiceRegistrations(scanServiceInfo(serviceInfo)))
             .map(ServiceInfo::serviceInstance)
             .collect(Collectors.toList());
 
