1d416e99fd9102b48df27e66ccc9e3863ab6e984
Artem Vysochyn <artem.vysochyn@gmail.com>
2025-01-17 10:16:36 +0200

    WIP

--- services-gateway/src/main/java/io/scalecube/services/gateway/websocket/HeartbeatService.java
+++ services-gateway/src/main/java/io/scalecube/services/gateway/websocket/HeartbeatService.java
@@ -7,7 +7,7 @@ import reactor.core.publisher.Mono;
 @Service(HeartbeatService.NAMESPACE)
 public interface HeartbeatService {
 
-  String NAMESPACE = "v1/scalecube.websocket";
+  String NAMESPACE = "v1/scalecube.websocket.heartbeat";
 
   @ServiceMethod
   Mono<Long> ping(long value);
