22c3c93f5e87d79f5a46907fd0f84a396e45a8ea
Artem Vysochyn <artem.vysochyn@gmail.com>
2025-01-22 14:24:20 +0200

    Added more doc for HeartbeatService, fixed typo in FileService doc

--- services-gateway/src/main/java/io/scalecube/services/gateway/websocket/HeartbeatService.java
+++ services-gateway/src/main/java/io/scalecube/services/gateway/websocket/HeartbeatService.java
@@ -4,10 +4,14 @@ import io.scalecube.services.annotations.Service;
 import io.scalecube.services.annotations.ServiceMethod;
 import reactor.core.publisher.Mono;
 
+/**
+ * Service interface for handling custom ping/pong service messages for websocket - service is
+ * echoing back ping message to the client. Used (optionally) as part of {@link WebsocketGateway}.
+ */
 @Service(HeartbeatService.NAMESPACE)
 public interface HeartbeatService {
 
-  String NAMESPACE = "v1/scalecube.websocket.heartbeat";
+  String NAMESPACE = "v1/scalecube.websocket";
 
   @ServiceMethod
   Mono<Long> ping(long value);
