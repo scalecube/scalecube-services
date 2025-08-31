23e3b5aa438ff1d28878ae8b5b0e201d1930e2d5
artem-v <artem@exberry.io>
2025-08-31 17:41:34 +0300

    Small cleanup in FileStreamer

--- services/src/main/java/io/scalecube/services/files/FileStreamer.java
+++ services/src/main/java/io/scalecube/services/files/FileStreamer.java
@@ -1,6 +1,5 @@
 package io.scalecube.services.files;
 
-import io.scalecube.services.annotations.RestMethod;
 import io.scalecube.services.annotations.Service;
 import io.scalecube.services.annotations.ServiceMethod;
 import io.scalecube.services.annotations.Tag;
@@ -17,7 +16,6 @@ public interface FileStreamer {
   String NAMESPACE = "v1/endpoints";
 
   @Tag(key = "Content-Type", value = "application/file")
-  @RestMethod("GET")
   @ServiceMethod("${microservices:id}/files/:name")
   Flux<byte[]> streamFile();
 }
